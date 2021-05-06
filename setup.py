723e651f3103ae6fb3e5f7e427c6c82cb774bbfb
Stefano Carrazza <stefano.carrazza@cern.ch>
2021-05-06 12:23:13 +0200

    updating setup file

---
+++ setup.py
@@ -0,0 +1,48 @@
+# Installation script for python
+from setuptools import setup, find_packages
+import subprocess
+import os
+import re
+import sys
+
+PACKAGE = "qiboicarusq"
+
+
+# Returns the version
+def get_version():
+    """ Gets the version from the package's __init__ file
+    if there is some problem, let it happily fail """
+    VERSIONFILE = os.path.join("src", PACKAGE, "__init__.py")
+    initfile_lines = open(VERSIONFILE, "rt").readlines()
+    VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+    for line in initfile_lines:
+        mo = re.search(VSRE, line, re.M)
+        if mo:
+            return mo.group(1)
+
+
+# load long description from README
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+
+setup(
+    name=PACKAGE,
+    version=get_version(),
+    description="Quantum hardware backend for IcarusQ experiment",
+    author="The Qibo team",
+    author_email="",
+    url="https://github.com/Quantum-TII/qiboicarusq",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: Physics",
+    ],
+    install_requires=['numpy'],
+    python_requires=">=3.6.0",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+)
