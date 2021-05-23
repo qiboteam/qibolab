e6e2c60aa8d93f20527d744fc004129733ca9344
Stavros Efthymiou <35475381+stavros11@users.noreply.github.com>
2021-05-23 22:30:42 +0400

    Add tomography tests

--- src/qiboicarusq/tests/test_tomography.py
+++ src/qiboicarusq/tests/test_tomography.py
@@ -49,4 +49,69 @@ def test_cholesky_reconstruct():
     np.testing.assert_allclose(c.reconstruct(), target_matrix, atol=1e-7)
 
 
-# TODO: Test tomography class (perhaps using the example?)
+def test_tomography_init():
+    amplitudes = np.random.random(16)
+    state = np.random.random(4)
+    gates = np.random.random((16, 4, 4))
+    tom = Tomography(amplitudes, state, gates)
+    np.testing.assert_allclose(tom.amplitudes, amplitudes)
+    np.testing.assert_allclose(tom.state, state)
+    np.testing.assert_allclose(tom.gates, gates)
+
+
+def test_tomography_find_beta():
+    amplitudes = np.random.random(16)
+    state = np.array([1, 2, 3, 4])
+    tom = Tomography(amplitudes, state)
+    target_beta = [2.5, -1, -0.5, 0]
+    np.testing.assert_allclose(tom.find_beta(state), target_beta)
+
+
+def test_tomography_default_gates():
+    import os
+    amplitudes = np.random.random(16)
+    state = np.array([1, 2, 3, 4])
+    tom = Tomography(amplitudes, state)
+    target_path = os.path.join(os.getcwd(), "results")
+    if not os.path.exists(target_path):
+        os.mkdir("results")
+    target_path = os.path.join(os.getcwd(), "default_gates.npy")
+    if os.path.exists(target_path):
+        target_gates = np.load(target_path)
+        np.testing.assert_allclose(tom.gates, target_gates)
+    else:
+        np.save(target_path, tom.gates)
+
+
+def test_tomography_linear():
+    import os
+    amplitudes = np.arange(16)
+    amplitudes = amplitudes / amplitudes.sum()
+    state = np.array([0.48721439, 0.61111949, 0.44811308, 0.05143444])
+    tom = Tomography(amplitudes, state)
+    target_path = os.path.join(os.getcwd(), "results", "linear_estimation.npy")
+    if os.path.exists(target_path):
+        target_linear = np.load(target_path)
+        np.testing.assert_allclose(tom.linear, target_linear)
+    else:
+        np.save(target_path, tom.linear)
+
+
+def test_tomography_fit():
+    import os
+    amplitudes = np.arange(16)
+    amplitudes = amplitudes / amplitudes.sum()
+    state = np.array([0.48721439, 0.61111949, 0.44811308, 0.05143444])
+    tom = Tomography(amplitudes, state)
+    with pytest.raises(ValueError):
+        tom.fit
+
+    tom.minimize()
+    assert tom.success
+
+    target_path = os.path.join(os.getcwd(), "results", "mlefit_estimation.npy")
+    if os.path.exists(target_path):
+        target_fit = np.load(target_path)
+        np.testing.assert_allclose(tom.fit, target_fit)
+    else:
+        np.save(target_path, tom.fit)
