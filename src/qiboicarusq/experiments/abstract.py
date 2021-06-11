e012db6a735b82d22db45f5f62c9adccfc223af0
Paul <paultan_1@hotmail.com>
2021-06-11 12:24:12 +0800

    Add parameter abstraction

--- src/qiboicarusq/experiments/abstract.py
+++ src/qiboicarusq/experiments/abstract.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from qibo.config import raise_error
+from typing import Any
 
 
 class AbstractExperiment(ABC):
@@ -34,3 +35,55 @@ class AbstractExperiment(ABC):
     @abstractmethod
     def download(self): # pragma: no cover
         raise_error(NotImplementedError)
+
+# With reference from qcodes.instruments.parameter
+class BaseParameter:
+    def __init__(self, name, default=None, vals=None, val_mapping=None, get_wrapper=None, validator=None):
+        self.name = name
+        self.vals = vals
+        self.val_mapping = val_mapping
+        self.value = default
+        self.get_wrapper = get_wrapper
+        self.validator = validator
+    
+    def get(self):
+        if self.get_wrapper is None:
+            return self.value
+        else:
+            return self.get_wrapper(self.value)
+
+    def set(self, value):
+        if self.validator is not None:
+            if not self.validator(value, self.vals):
+                raise RuntimeError("Invalid Value", value)
+                
+        self.value = value
+
+    def __call__(self, *args: Any, **kwds: Any) -> Any:
+        if len(args) == 0:
+            return self.get()
+
+        else:
+            self.set(*args, **kwds)
+            return None
+
+
+def BoundsValidator(value, bounds):
+    lbound, ubound = bounds
+    return value >= lbound and value <= ubound
+
+def EnumValidator(value, enum):
+    return value in enum
+
+class ParameterList:
+    def __init__(self):
+        self.parameters = {}
+
+    def add_parameter(self, name: str, default=None, vals=None, val_mapping=None, get_wrapper=None, validator=None):
+        self.parameters[name] = BaseParameter(name, default, vals, val_mapping, get_wrapper, validator)
+
+    def __getitem__(self, key: str):
+        return self.parameters[key]
+
+    def __getattr__(self, key: str) -> Any:
+        return self.parameters[key]
