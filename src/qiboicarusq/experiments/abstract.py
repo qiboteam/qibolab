ee973fbb71c91805bd779ea37416685f4dd1d93a
sorewachigauyo <paultan_1@hotmail.com>
2021-06-23 17:29:17 +0800

    Add class documentation

--- src/qiboicarusq/experiments/abstract.py
+++ src/qiboicarusq/experiments/abstract.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 from qibo.config import raise_error
-from typing import Any, Union
+from typing import Any, Union, List, Tuple
 
 
 class AbstractExperiment(ABC):
@@ -38,7 +38,18 @@ class AbstractExperiment(ABC):
 
 # With reference from qcodes.instruments.parameter
 class BaseParameter:
-    def __init__(self, name, default=None, vals=None, val_mapping=None, get_wrapper=None, validator=None):
+    def __init__(self, name: str, default=None, vals=None, val_mapping=None, get_wrapper=None, validator=None):
+        """An adjustable parameter to be used in the experiment.
+
+        Args:
+        name: Parameter name
+        default: Parameter default value
+        vals: To be used to validate new values for the parameter
+        val_mapping: Iterable to be used by get_wrapper when get is called
+        get_wrapper: Function to format parameter value when used
+        validator: Function that validates new parameter value against vals
+        @see BoundsValidator, EnumValidator for examples
+        """
         self.name = name
         self.vals = vals
         self.val_mapping = val_mapping
@@ -47,12 +58,20 @@ class BaseParameter:
         self.validator = validator
     
     def get(self):
+        """Fetches the parameter value.
+
+        if get_wrapper is available, return the value returned by the get_wrapped function
+        """
         if self.get_wrapper is None:
             return self.value
         else:
-            return self.get_wrapper(self.value)
+            return self.get_wrapper(self.value, self.val_mapping)
 
     def set(self, value):
+        """Sets a new value for the parameter.
+
+        if validator is set, first validate the new value and raise an error is value is invalid.
+        """
         if self.validator is not None:
             if not self.validator(value, self.vals):
                 raise RuntimeError("Invalid Value", value)
@@ -75,9 +94,14 @@ def BoundsValidator(value, bounds):
 def EnumValidator(value, enum):
     return value in enum
 
-class ParameterList(dict):
-
+class ParameterList(dict[str, BaseParameter]):
+    """Dictionary class that holds BaseParameters.
+    """
     def add_parameter(self, name: str, default=None, vals=None, val_mapping=None, get_wrapper=None, validator=None):
+        """Adds a new parameter to the dictionary
+        
+        @see BaseParameter for arguments
+        """
         self[name] = BaseParameter(name, default, vals, val_mapping, get_wrapper, validator)
 
     def __getattr__(self, key: str) -> Any:
@@ -85,12 +109,21 @@ class ParameterList(dict):
 
 
 class GateSet(dict):
+    """Dictionary containing the PulseSequence objects for each gate.
+    """
 
     from qibo import gates
     two_qubit_gates = [gates.CNOT]
 
 
     def set(self, gate, pulse_sequence: list):
+        """Sets a qibo gate into the register with its corresponding pulse sequence.
+        If a two qubit gate is set, the id of the control qubit is also set for reference.
+
+        Args:
+        gate: Qibo gate object
+        pulse_sequence: List of PulseSequence objects that represent the gate
+        """
         gate_name = gate.name
 
         if self.is_two_qubit_gate(gate):
@@ -98,14 +131,23 @@ class GateSet(dict):
 
         self[gate_name] = pulse_sequence
 
-    def set_from_dict(self, obj):
+    def set_from_dict(self, obj: dict):
         for key, value in obj.items():
             self[key] = value
         
     def _is_two_qubit_gate(self, gate):
         return any(isinstance(gate, x) for x in self.two_qubit_gates)
 
-    def get(self, gate):
+    def get(self, gate) -> List[Any]:
+        """Fetches the pulse representation of a qibo gate
+        Note: For parameterized gates like RX, RY or RZ, this function does not set the parameter.
+
+        Args:
+        gate: Qibo gate object
+
+        Returns:
+        List of PulseSequence objects
+        """
         gate_name = gate.name
 
         if self.is_two_qubit_gate(gate):
@@ -115,6 +157,8 @@ class GateSet(dict):
 
 
 class Qubit(ParameterList):
+    """Qubit representation with parameters for control and readout
+    """
     def __init__(self, id: int = None, qubit_frequency: float = 0, qubit_frequency_bounds: tuple = (0, 0), connected_qubits: list = [],
                  drive_channel: Union[tuple, int] = None, resonator_frequency: float = 0, resonator_frequency_bounds: list = (0, 0),
                  flux_channel: int = None, readout_channel: Union[tuple, int] = None, initial_gates: dict = {},
@@ -141,5 +185,7 @@ class Qubit(ParameterList):
         self["gates"] = GateSet()
         self.gates.set_from_dict(initial_gates)
 
-    def edges(self):
+    def edges(self) -> List[Tuple]:
+        """Fetches a list of edge tuples representing qubit connectivity.
+        """
         return [(self.id, q) for q in self.connected_qubits]
