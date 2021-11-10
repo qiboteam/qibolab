0fe541308e7ff612a69f897e0f1bf2addaeca754
Stavros Efthymiou <35475381+stavros11@users.noreply.github.com>
2021-11-10 22:39:28 +0400

    Implement QibolabBackend.circuit_class

--- src/qibolab/backend.py
+++ src/qibolab/backend.py
@@ -17,6 +17,16 @@ class QibolabBackend(NumpyBackend): # pragma: no cover
         self.hardware_gates = qibolab.gates
         self.hardware_circuit = qibolab.circuit.HardwareCircuit
 
+    def circuit_class(self, accelerators=None, density_matrix=False):
+        if accelerators is not None:
+            raise_error(NotImplementedError, "Hardware backend does not support "
+                                             "multi-GPU configuration.")
+        if density_matrix:
+            raise_error(NotImplementedError, "Hardware backend does not support "
+                                             "density matrix simulation.")
+        from qibolab.circuit import HardwareCircuit
+        return HardwareCircuit
+
     def create_einsum_cache(self, qubits, nqubits, ncontrol=None): # pragma: no cover
         raise_error(NotImplementedError, "`create_einsum_cache` method is "
                                          "not required for hardware backends.")
