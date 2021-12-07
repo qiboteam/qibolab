848ebe62868f3084c8d0324ccaa5937a548c92b5
Marco Lazzarin <48728634+mlazzarin@users.noreply.github.com>
2021-12-07 10:16:19 +0400

    Add print statements to diagnostics launch script

--- examples/tii_single_qubit/main.py
+++ examples/tii_single_qubit/main.py
@@ -11,9 +11,16 @@ if __name__ == "__main__":
     with open("diagnostics_settings.json", "r") as file:
         settings = json.load(file)
 
+    print("\nRun resonator spectroscopy.\n")
     resonator_freq, _ = run_resonator_spectroscopy(**settings["resonator_spectroscopy"])
+    print("\nRun qubit spectroscopy.\n")
     qubit_freq, _ = run_qubit_spectroscopy(**settings["qubit_spectroscopy"])
+    print("\nRun Rabi pulse length.\n")
     run_rabi_pulse_length(resonator_freq, qubit_freq)
+    print("\nRun Rabi pulse gain.\n")
     run_rabi_pulse_gain(resonator_freq, qubit_freq)
+    print("\nRun Rabi pulse length and gain.\n")
     run_rabi_pulse_length_and_gain(resonator_freq, qubit_freq)
+    print("\nRun Rabi pulse length and amplitude.\n")
     run_rabi_pulse_length_and_amplitude(resonator_freq, qubit_freq)
+    print("\nDiagnostics completed.\n")
