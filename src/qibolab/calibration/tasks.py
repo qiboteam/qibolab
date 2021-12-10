4ae3c0363302c015e5b51aa6ef5324e7d0e318ce
Stavros Efthymiou <35475381+stavros11@users.noreply.github.com>
2021-12-10 16:45:10 +0400

    Fix calibration pylint

--- src/qibolab/calibration/tasks.py
+++ src/qibolab/calibration/tasks.py
@@ -2,7 +2,9 @@ from typing import Tuple
 import numpy as np
 from qibolab import experiment, scheduler
 from qibolab.circuit import PulseSequence
-from qibolab.pulses import BasicPulse, Rectangular, IQReadoutPulse
+from qibolab.pulses import Pulse, IQReadoutPulse
+from qibolab.pulse_shapes import Rectangular
+
 
 def parse_iq(raw_signals, if_frequency):
     final = experiment.readout_params.ADC_sample_size() / experiment.readout_params.ADC_sampling_rate()
@@ -36,7 +38,7 @@ def PulseSpectroscopy(scan_start: float, scan_stop: float, scan_step: float, qub
     sequence = []
     sweep = np.arange(scan_start, scan_stop, scan_step)
     for freq in sweep:
-        drive = BasicPulse(qubit_channel, readout_start - pulse_time, pulse_time, 0.75 / 2, freq, 0, Rectangular())
+        drive = Pulse(qubit_channel, readout_start - pulse_time, pulse_time, 0.75 / 2, freq, 0, Rectangular())
         sequence.append(PulseSequence([drive, readout], duration=duration))
 
     job = scheduler.execute_batch_sequence(sequence, nshots)
@@ -60,7 +62,7 @@ def RabiSpectroscopy(scan_start: float, scan_stop: float, scan_step: float, qubi
     sequence = []
     sweep = np.arange(scan_start, scan_stop, scan_step)
     for pulse_time in sweep:
-        drive = BasicPulse(qubit_channel, readout_start - pulse_time, pulse_time, 0.75 / 2, qubit_frequency, 0, Rectangular())
+        drive = Pulse(qubit_channel, readout_start - pulse_time, pulse_time, 0.75 / 2, qubit_frequency, 0, Rectangular())
         sequence.append(PulseSequence([drive, readout]))
 
     job = scheduler.execute_batch_sequence(sequence, nshots)
@@ -73,7 +75,7 @@ def RamseyInferometry(scan_start: float, scan_stop: float, scan_step: float, qub
     In this task, the qubit is driven with two pi-half pulses. The seperation between the two pulses is dependent on the scan parameters.
     The drive frequency is usually detuned from the qubit resonant frequency to determine the frequency.
     The T2 time can be extracted from the decay of the oscillation.
- 
+
     Returns an array of i, q pairs
     """
     readout_start = 0
@@ -84,9 +86,9 @@ def RamseyInferometry(scan_start: float, scan_stop: float, scan_step: float, qub
 
     sequence = []
     sweep = np.arange(scan_start, scan_stop, scan_step)
-    second_pulse = BasicPulse(qubit_channel, readout_start - pi_half, pi_half, 0.75 / 2, qubit_frequency, 0, Rectangular())
+    second_pulse = Pulse(qubit_channel, readout_start - pi_half, pi_half, 0.75 / 2, qubit_frequency, 0, Rectangular())
     for tau in sweep:
-        first_pulse = BasicPulse(qubit_channel, readout_start - pi_half - tau - pi_half, pi_half, 0.75 / 2, qubit_frequency, 0, Rectangular())
+        first_pulse = Pulse(qubit_channel, readout_start - pi_half - tau - pi_half, pi_half, 0.75 / 2, qubit_frequency, 0, Rectangular())
         sequence.append(PulseSequence([first_pulse, second_pulse, readout]))
 
     job = scheduler.execute_batch_sequence(sequence, nshots)
@@ -98,7 +100,7 @@ def T1(scan_start: float, scan_stop: float, scan_step: float, qubit_channel: int
     """T1 task.
     In this task, the qubit is driven to the excited state by the pi-pulse. Then, a delay tau, which is determined by the scan parameters, is applied before readout.
     The T1 time is determined by the decay of the excited state to the ground state.
- 
+
     Returns an array of i, q pairs
     """
     readout_start = 0
@@ -111,7 +113,7 @@ def T1(scan_start: float, scan_stop: float, scan_step: float, qubit_channel: int
     sweep = np.arange(scan_start, scan_stop, scan_step)
     for tau in sweep:
         if tau >= 0:
-            drive = BasicPulse(qubit_channel, readout_start - tau - pi_pulse, pi_pulse, 0.75 / 2, qubit_frequency, 0, Rectangular())
+            drive = Pulse(qubit_channel, readout_start - tau - pi_pulse, pi_pulse, 0.75 / 2, qubit_frequency, 0, Rectangular())
             sequence.append(PulseSequence([drive, readout]))
         else:
             sequence.append(PulseSequence([readout]))
@@ -126,7 +128,7 @@ def Spinecho(scan_start: float, scan_stop: float, scan_step: float, qubit_channe
     In this task, the qubit is driven with a pi-half pulse, a pi-pulse and a pi-half pulse. The seperation tau between the three pulses is dependent on the scan parameters.
     Some spinecho routines fix the first seperation and vary the second seperation. Here, both are varied by the same amount.
     At long tau, the system decays to the mixed state.
- 
+
     Returns an array of i, q pairs
     """
     readout_start = 0
@@ -137,13 +139,13 @@ def Spinecho(scan_start: float, scan_stop: float, scan_step: float, qubit_channe
 
     sequence = []
     sweep = np.arange(scan_start, scan_stop, scan_step)
-    second_half_pulse = BasicPulse(qubit_channel, readout_start - pi_half, pi_half, 0.75 / 2, qubit_frequency, 0, Rectangular())
+    second_half_pulse = Pulse(qubit_channel, readout_start - pi_half, pi_half, 0.75 / 2, qubit_frequency, 0, Rectangular())
     for tau in sweep:
         tau_half = tau / 2
         start =  readout_start - pi_half - tau_half - pi_pulse
-        middle_pulse = BasicPulse(qubit_channel, start, pi_pulse, 0.75 / 2, qubit_frequency, 0, Rectangular())
+        middle_pulse = Pulse(qubit_channel, start, pi_pulse, 0.75 / 2, qubit_frequency, 0, Rectangular())
         start = start - tau_half - pi_half
-        first_half_pulse = BasicPulse(qubit_channel, start, pi_half, 0.75 / 2, qubit_frequency, 0, Rectangular())
+        first_half_pulse = Pulse(qubit_channel, start, pi_half, 0.75 / 2, qubit_frequency, 0, Rectangular())
         sequence.append(PulseSequence([first_half_pulse, middle_pulse, second_half_pulse, readout]))
 
     job = scheduler.execute_batch_sequence(sequence, nshots)
