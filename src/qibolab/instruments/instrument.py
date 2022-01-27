57a7702649b0428d81af0a8c94956c3726f13e95
Joel Pérez Díaz <joel.perez@qilimanjaro.tech>
2022-01-27 12:46:20 +0100

    Created base Instrument class and InstrumentException

---
+++ src/qibolab/instruments/instrument.py
@@ -0,0 +1,41 @@
+from abc import ABC, abstractmethod
+
+class Instrument(ABC):
+    """
+    Parent class for all the instruments
+    """
+    def __init__(self):
+        self._connected = False
+        # Implement signature as InstrumentClassName@ip_address
+        self._signature = None
+        self._driver = None
+        self._connected = False
+    
+    @abstractmethod
+    def connect(self):
+        """
+        Establish connection with the instrument.
+        Initialize self._driver variable
+        """
+        raise NotImplementedError
+
+    @property
+    def signature(self):
+        return self._signature
+
+    @abstractmethod
+    def close(self):
+        """
+        Close connection with the instrument.
+        Set instrument values to idle values if required.
+        """
+        raise NotImplementedError
+
+
+class InstrumentException(Exception):
+    def __init__(self, instrument: Instrument, message: str):
+        self.instrument = instrument
+        header = f"InstrumentException with {self.instrument.signature}"
+        full_msg = header + ": " + message
+        super().__init__(full_msg)
+        self.instrument = instrument
