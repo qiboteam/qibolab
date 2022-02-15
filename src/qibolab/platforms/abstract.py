c45e568d0f5312d8840f0738a24ecb923a7f5054
Stefano Carrazza <stefano.carrazza@cern.ch>
2022-02-15 16:59:34 +0100

    adding platform switcher (function version)

---
+++ src/qibolab/platforms/abstract.py
@@ -0,0 +1,150 @@
+from abc import ABC, abstractmethod
+import yaml
+from qibo.config import raise_error, log
+
+
+class AbstractPlatform(ABC):
+    """Abstract platform for controlling quantum devices.
+
+    Args:
+        name (str): name of the platform.
+        runcard (str): path to the yaml file containing the platform setup.
+    """
+
+    def __init__(self, name, runcard):
+        log.info(f"Loading platform {name}")
+        log.info(f"Loading runcard {runcard}")
+        self.name = name
+
+        # Load calibration settings
+        with open(runcard, "r") as file:
+            self._settings = yaml.safe_load(file)
+
+        # Define references to instruments
+        self.is_connected = False
+        self.connect()
+
+    def _check_connected(self):
+        if not self.is_connected:
+            raise_error(RuntimeError, "Cannot access instrument because it is not connected.")
+
+    @property
+    def data_folder(self):
+        return self._settings.get("settings").get("data_folder")
+
+    @property
+    def hardware_avg(self):
+        return self._settings.get("settings").get("hardware_avg")
+
+    @property
+    def sampling_rate(self):
+        return self._settings.get("settings").get("sampling_rate")
+
+    @property
+    def software_averages(self):
+        return self._settings.get("settings").get("software_averages")
+
+    @software_averages.setter
+    def software_averages(self, x):
+        self._settings["settings"]["software_averages"] = x
+
+    @property
+    def repetition_duration(self):
+        return self._settings.get("settings").get("repetition_duration")
+
+    @property
+    def resonator_frequency(self):
+        return self._settings.get("settings").get("resonator_freq")
+
+    @property
+    def qubit_frequency(self):
+        return self._settings.get("settings").get("qubit_freq")
+
+    @property
+    def pi_pulse_gain(self):
+        return self._settings.get("settings").get("pi_pulse_gain")
+
+    @property
+    def pi_pulse_amplitude(self):
+        return self._settings.get("settings").get("pi_pulse_amplitude")
+
+    @property
+    def pi_pulse_duration(self):
+        return self._settings.get("settings").get("pi_pulse_duration")
+
+    @property
+    def pi_pulse_frequency(self):
+        return self._settings.get("settings").get("pi_pulse_frequency")
+
+    @property
+    def readout_pulse(self):
+        return self._settings.get("settings").get("readout_pulse")
+
+    @property
+    def max_readout_voltage(self):
+        return self._settings.get("settings").get("resonator_spectroscopy_max_ro_voltage")
+
+    @property
+    def min_readout_voltage(self):
+        return self._settings.get("settings").get("rabi_oscillations_pi_pulse_min_voltage")
+
+    @property
+    def delay_between_pulses(self):
+        return self._settings.get("settings").get("delay_between_pulses")
+
+    @property
+    def delay_before_readout(self):
+        return self._settings.get("settings").get("delay_before_readout")
+
+    def run_calibration(self, runcard):
+        """Executes calibration routines and updates the settings json."""
+        # TODO: Implement calibration routines and update ``self._settings``.
+
+        # update instruments with new calibration settings
+        self.setup()
+        # save new calibration settings to json
+        with open(runcard, "w") as file:
+            yaml.dump(self._settings, file)
+
+    def __call__(self, sequence, nshots=None):
+        return self.execute(sequence, nshots)
+
+    @abstractmethod
+    def connect(self):
+        """Connects to lab instruments using the details specified in the calibration settings."""
+        raise_error(NotImplementedError)
+
+    @abstractmethod
+    def setup(self):
+        """Configures instruments using the loaded calibration settings."""
+        raise_error(NotImplementedError)
+
+    @abstractmethod
+    def start(self):
+        """Turns on the local oscillators."""
+        raise_error(NotImplementedError)
+
+    @abstractmethod
+    def stop(self):
+        """Turns off all the lab instruments."""
+        raise_error(NotImplementedError)
+
+    @abstractmethod
+    def disconnect(self):
+        """Disconnects from the lab instruments."""
+        raise_error(NotImplementedError)
+
+    @abstractmethod
+    def execute(self, sequence, nshots=None):
+        """Executes a pulse sequence.
+
+        Args:
+            sequence (:class:`qibolab.pulses.PulseSequence`): Pulse sequence to execute.
+            nshots (int): Number of shots to sample from the experiment.
+                If ``None`` the default value provided as hardware_avg in the
+                calibration json will be used.
+
+        Returns:
+            Readout results acquired by after execution.
+        """
+        raise_error(NotImplementedError)
--- src/qibolab/platforms/icplatform.py
+++ src/qibolab/platforms/icplatform.py
@@ -1,35 +1,13 @@
-import pathlib
-import yaml
 from qibo.config import raise_error, log
+from qibolab.platforms.abstract import AbstractPlatform
 
-class ICPlatform:
-    """Platform for controlling quantum devices.
 
-    The path of the calibration json can be provided using the
-    ``"CALIBRATION_PATH"`` environment variable.
-    If no path is provided the default path (``platforms/tiiq_settings.json``)
-    will be used.
+class ICPlatform(AbstractPlatform):
+    """Platform for controlling quantum devices with IC."""
 
-    Args:
-        name (str): name of the platform stored in a yaml file in qibolab/platforms.
-    """
-
-    def __init__(self, name):
-        log.info(f"Loading platform {name}.")
-        self.name = name
-        # Load calibration settings
-        self.calibration_path = pathlib.Path(__file__).parent / f"{name}.yml"
-        with open(self.calibration_path, "r") as file:
-            self._settings = yaml.safe_load(file)
-
-        # Define references to instruments
-        self.is_connected = False
+    def __init__(self, name, runcard):
         self._ic = None
-        # instruments are connected in :meth:`qibolab.platform.Platform.start`
-
-    def _check_connected(self):
-        if not self.is_connected:
-            raise_error(RuntimeError, "Cannot access instrument because it is not connected.")
+        super().__init__(name, runcard)
 
     def connect(self):
         """Connects to lab instruments using the details specified in the calibration settings."""
@@ -47,8 +25,8 @@ class ICPlatform:
                 self.is_connected = True
             except Exception as exception:
                 raise_error(RuntimeError, "Cannot establish connection to "
-                                         f"{self.name} instruments. "
-                                         f"Error captured: '{exception}'")
+                            f"{self.name} instruments. "
+                            f"Error captured: '{exception}'")
         self.setup()
 
     def setup(self):
@@ -58,10 +36,10 @@ class ICPlatform:
             for name, params in instruments.items():
                 setup_params = params.get("setup_parameters")
                 self.ic.setup_instrument(name, setup_params)
-    
+
     def start(self):
         """Turns on the local oscillators.
-        
+
         At this point, the pulse sequence have not been uploaded to the DACs, so they will not be started yet.
         """
         self.connect()
@@ -91,10 +69,11 @@ class ICPlatform:
             after execution.
         """
         if not self.is_connected:
-            raise_error(RuntimeError, "Execution failed because instruments are not connected.")
+            raise_error(
+                RuntimeError, "Execution failed because instruments are not connected.")
         if nshots is None:
             nshots = self.hardware_avg
-        
+
         sobj = {}
         robj = {}
         from qibolab.pulses import ReadoutPulse
