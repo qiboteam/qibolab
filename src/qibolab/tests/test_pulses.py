e8075bab8404f1f135a3a59f97d557777632cd2b
stavros11 <35475381+stavros11@users.noreply.github.com>
2022-03-16 17:10:21 +0400

    Add pulse tests

--- src/qibolab/tests/test_pulses.py
+++ src/qibolab/tests/test_pulses.py
@@ -1,19 +1,69 @@
-# pylint: skip-file
-
+"""Tests ``pulses.py`` and ``pulse_shapes.py``."""
 import pytest
 import numpy as np
-from qibolab import pulses
-from qibolab.pulse_shapes import Rectangular, Gaussian, Drag, SWIPHT
-from qibolab.circuit import PulseSequence
+
+
+def test_rectangular_shape():
+    from qibolab.pulse_shapes import Rectangular
+    rect = Rectangular()
+    assert rect.name == "rectangular"
+    np.testing.assert_allclose(rect.envelope(0.0, 0.2, 10.0, 4.5), 4.5 * np.ones(10))
+
+
+def test_gaussian_shape():
+    from qibolab.pulse_shapes import Gaussian
+    gauss = Gaussian(1.5)
+    assert gauss.name == "gaussian"
+    assert gauss.sigma == 1.5
+    assert repr(gauss) == "gaussian(1.5)"
+    envelope = gauss.envelope(0.0, 0.0, 10.0, 4.5)
+    assert envelope.shape == (10,)
+
+
+def test_drag_shape():
+    from qibolab.pulse_shapes import Drag
+    drag = Drag(1.5, 2.5)
+    assert drag.name == "drag"
+    assert drag.sigma == 1.5
+    assert drag.beta == 2.5
+    assert repr(drag) == "drag(1.5, 2.5)"
+    target_envelop = 4.4108940298803985 + 1.470298009960133j
+    time = np.array([1.0])
+    assert drag.envelope(time, 0.2, 2.2, 4.5) == target_envelop
+
+
+def test_swipht_shape():
+    from qibolab.pulse_shapes import SWIPHT
+    swipht = SWIPHT(2.2)
+    assert swipht.name == "SWIPHT"
+    assert swipht.g == 2.2
+    assert repr(swipht) == "SWIPHT(2.2)"
+    target_envelop = 4.4108940298803985
+    time = np.array([1.0])
+    assert swipht.envelope(time, 0.2, 2.2, 4.5) == 4.5
+
+
+def test_pulse():
+    from qibolab.pulses import Pulse
+    from qibolab.pulse_shapes import Gaussian
+    pulse = Pulse(0.0, 8.0, 0.8, 40.0, 0.7, Gaussian(1.0))
+    target = "P(qcm, 0.0, 8.0, 0.8, 40.0, 0.7, gaussian(1.0))"
+    assert pulse.serial() == target
+    assert repr(pulse) == target
+    assert pulse.compile().shape == (8,)
+
+
+def test_readout_pulse():
+    from qibolab.pulses import ReadoutPulse
+    from qibolab.pulse_shapes import Rectangular
+    pulse = ReadoutPulse(0.0, 8.0, 0.8, 40.0, 0.7, Rectangular())
+    target = "P(qrm, 0.0, 8.0, 0.8, 40.0, 0.7, rectangular)"
+    assert pulse.serial() == target
+    assert repr(pulse) == target
+    np.testing.assert_allclose(pulse.compile(), 0.8 * np.ones(8))
 
 
 @pytest.mark.skip
-def test_basic_pulse():
-    basic = pulses.Pulse(0.5, 1.5, 0.8, 40.00, 0.7, "Rectangular")
-    target_repr = "P(0, 0.5, 1.5, 0.8, 40.0, 0.7, Rectangular)"
-    assert repr(basic) == target_repr
-
-
 def test_multifrequency_pulse():
     members = [
         pulses.Pulse(0.5, 1.5, 0.8, 40.00, 0.7, "Rectangular", channel=0),
@@ -27,81 +77,8 @@ def test_multifrequency_pulse():
     assert repr(multi) == target_repr
 
 
+@pytest.mark.skip
 def test_file_pulse():
     filep = pulses.FilePulse(0, 1.0, "testfile")
     target_repr = "F(0, 1.0, testfile)"
     assert repr(filep) == target_repr
-
-
-@pytest.mark.skip
-def test_rectangular_shape():
-    rect = Rectangular()
-    assert rect.name == "rectangular"
-    assert rect.envelope(1.0, 0.2, 2.2, 4.5) == 4.5
-
-
-@pytest.mark.skip
-def test_gaussian_shape():
-    gauss = Gaussian(1.5)
-    assert gauss.name == "gaussian"
-    assert gauss.sigma == 1.5
-    assert repr(gauss) == "(gaussian, 1.5)"
-    target_envelop = 4.4108940298803985
-    time = np.array([1.0])
-    assert gauss.envelope(time, 0.2, 2.2, 4.5) == target_envelop
-
-
-def test_drag_shape():
-    drag = Drag(1.5, 2.5)
-    assert drag.name == "drag"
-    assert drag.sigma == 1.5
-    assert drag.beta == 2.5
-    assert repr(drag) == "(drag, 1.5, 2.5)"
-    target_envelop = 4.4108940298803985 + 1.470298009960133j
-    time = np.array([1.0])
-    assert drag.envelope(time, 0.2, 2.2, 4.5) == target_envelop
-
-
-def test_swipht_shape():
-    swipht = SWIPHT(2.2)
-    assert swipht.name == "SWIPHT"
-    assert swipht.g == 2.2
-    assert repr(swipht) == "(SWIPHT, 2.2)"
-    target_envelop = 4.4108940298803985
-    time = np.array([1.0])
-    assert swipht.envelope(time, 0.2, 2.2, 4.5) == 4.5
-
-
-# TODO: Fix these tests so that waveform is not zero
-@pytest.mark.skip("Pulse compile was changed after implementing TIIq.")
-def test_basic_pulse_compile():
-    seq = PulseSequence([])
-    waveform = np.zeros((seq.nchannels, seq.sample_size))
-    basic = pulses.Pulse(0.5, 1.5, 0.8, 40.00, 0.7, Gaussian(1.0))
-    waveform = basic.compile(waveform, seq)
-    target_waveform = np.zeros_like(waveform)
-    np.testing.assert_allclose(waveform, target_waveform)
-
-
-@pytest.mark.skip("Pulse compile was changed after implementing TIIq.")
-def test_multifrequency_pulse_compile():
-    seq = PulseSequence([])
-    waveform = np.zeros((seq.nchannels, seq.sample_size), dtype="complex128")
-    members = [
-        pulses.Pulse(0.5, 1.5, 0.8, 40.00, 0.7, Gaussian(1.0)),
-        pulses.Pulse(0.5, 1.5, 0.8, 40.00, 0.7, Drag(1.0, 1.5))
-        ]
-    multi = pulses.MultifrequencyPulse(members)
-    waveform = multi.compile(waveform, seq)
-    target_waveform = np.zeros_like(waveform)
-    np.testing.assert_allclose(waveform, target_waveform)
-
-
-@pytest.mark.skip("Skipping this test because `sequence.file_dir` is not available")
-def test_file_pulse_compile():
-    seq = PulseSequence([])
-    waveform = np.zeros((seq.nchannels, seq.sample_size))
-    filep = pulses.FilePulse(0, 1.0, "file")
-    waveform = filep.compile(waveform, seq)
-    target_waveform = np.zeros_like(waveform)
-    np.testing.assert_allclose(waveform, target_waveform)
