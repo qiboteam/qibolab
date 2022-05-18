7726cad3bc411cb5f3ece53f128fea5872536bd3
aorgazf <55031026+aorgazf@users.noreply.github.com>
2022-05-18 21:43:21 +0400

    initial

---
+++ src/qibolab/paths.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+qibolab_folder = Path(__file__).parent
+src_folder = qibolab_folder.parent
+project_folder = src_folder.parent
+examples_folder = project_folder / 'examples'
