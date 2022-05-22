8e9234e09af4f6ce3a9f1304d4385f8da277f233
aorgazf <55031026+aorgazf@users.noreply.github.com>
2022-05-22 19:13:16 +0400

    test_platform

--- src/qibolab/tests/test_platform.py
+++ src/qibolab/tests/test_platform.py
@@ -7,6 +7,10 @@ def test_platform():
     platform = Platform("tiiq")
     assert isinstance(platform, QBloxPlatform)
 
+    from qibolab.platforms.multiqubit import MultiqubitPlatform
+    platform = Platform("multiqubit")
+    assert isinstance(platform, MultiqubitPlatform)
+
     from qibolab.platforms.icplatform import ICPlatform
     platform = Platform("icarusq")
     assert isinstance(platform, ICPlatform)
