efb253cf362c1891d5d344257d9fb62c4772eb3d
aorgazf <55031026+aorgazf@users.noreply.github.com>
2022-06-02 09:17:24 +0400

    fix test_platform

--- src/qibolab/tests/test_platform.py
+++ src/qibolab/tests/test_platform.py
@@ -2,7 +2,7 @@ import pytest
 from qibolab import Platform
 
 
-def test_platform():
+def test_platform_multiqubit():
     from qibolab.platforms.multiqubit import MultiqubitPlatform
     platform = Platform("multiqubit")
     assert isinstance(platform, MultiqubitPlatform)
@@ -11,9 +11,12 @@ def test_platform():
     platform = Platform("qili")
     assert isinstance(platform, MultiqubitPlatform)
 
+@pytest.mark.xfail # Loading the platform requires access to one of the instrument's driver dll
+def test_platform_icarusq():
     from qibolab.platforms.icplatform import ICPlatform
     platform = Platform("icarusq")
     assert isinstance(platform, ICPlatform)
 
+def test_platform():
     with pytest.raises(RuntimeError):
         platform = Platform("nonexistent")
