90e30b6b2ce69812dc5cd3921587614b727379b6
aorgazf <55031026+aorgazf@users.noreply.github.com>
2022-06-29 15:37:50 +0400

    Revert "connection to scope2"
    This reverts commit c9d343fbe5b0e7f38f8898389c0ef1cfeb55c12c.

--- src/qibolab/instruments/teledyne_scope.py
+++
@@ -1,5 +0,0 @@
-import win32com.client #imports the pywin32 library
-scope=win32com.client.Dispatch("LeCroy.ActiveDSOCtrl.1")  #creates instance of the ActiveDSO control
-scope.MakeConnection("IP:127.0.0.1") #Connects to the oscilloscope.  Substitute your IP address
-scope.WriteString("C1:VDIV .02",1) #Remote Command to set C1 volt/div setting to 20 mV.
-scope.Disconnect() #Disconnects from the oscilloscope
