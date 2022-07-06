10bd0cd820e7eee8a3bf2eae661fbaed4f0d339f
Prerna Baranwal <prernabaranwal2190@gmail.com>
2022-07-06 11:28:40 +0800

    Updated test.dds

--- src/qibolab/tests/test-dds.py
+++ src/qibolab/tests/test-dds.py
@@ -1,5 +1,5 @@
 import numpy as np
-import evalcontrol
-evaluation_board = evalcontrol.AD9959()
+import qibolab.instruments
+evaluation_board = qibolab.instruments.AD9959()
 evaluation_board.set_clock_multiplier(15)
 evaluation_board.set_frequency(200e6,channel=[0])
