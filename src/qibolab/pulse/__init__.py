539b9a1aa00b7d35d49745d6bef5925e892036e3
JoelPerezDiaz <joel.perez@qilimanjaro.tech>
2022-07-06 13:16:31 +0200

    Complete package init files

--- src/qibolab/pulse/__init__.py
+++ src/qibolab/pulse/__init__.py
@@ -1,2 +1,6 @@
+"""__init__.py"""
 from .pulse import Pulse
+from .pulse_sequence import PulseSequence
+from .pulse_sequences import PulseSequences
+from .pulse_shape import Drag, Gaussian, PulseShape, Rectangular
 from .readout_pulse import ReadoutPulse
--- src/qibolab/pulse/pulse_shape/__init__.py
+++ src/qibolab/pulse/pulse_shape/__init__.py
@@ -1,3 +1,5 @@
+"""__init__.py"""
 from .drag import Drag
 from .gaussian import Gaussian
-from . rectangular import Rectangular
+from .pulse_shape import PulseShape
+from .rectangular import Rectangular
