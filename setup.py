d172706aa01a937b3554e2d7d147cc761f77a80f
Stavros <35475381+stavros11@users.noreply.github.com>
2022-07-13 17:26:44 +0400

    Update setup.py and rules

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
         "Topic :: Scientific/Engineering :: Physics",
     ],
     install_requires=[
-        "qibo",     # 0.1.7
+        "qibo>=0.1.8",
         "qcodes",   # 0.34.1
         "lmfit",    # 1.0.3
     ],
