e39d69181b26dff21ed6e30e91e379e86d01bbba
aorgazf <55031026+aorgazf@users.noreply.github.com>
2022-07-18 15:08:49 +0400

    initial

--- src/qibolab/instruments/__init__.py
+++ src/qibolab/instruments/__init__.py
@@ -1,4 +1,4 @@
-from qibolab.instruments.qblox import PulsarQRM, PulsarQCM, ClusterQRM, ClusterQCM
+from qibolab.instruments.qblox import ClusterQRM_RF, ClusterQCM_RF
 from qibolab.instruments.rohde_schwarz import SGS100A
 from qibolab.instruments.icarusq import TektronixAWG5204, AlazarADC, MCAttenuator, QuicSyn
 from qibolab.instruments.icarusqfpga import IcarusQFPGA, PulseBlaster
