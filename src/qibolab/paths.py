7d5c326b0a588ebf916031e0ce41e19d138531ce
aorgazf <55031026+aorgazf@users.noreply.github.com>
2022-07-19 12:39:32 +0400

    add script_folder to paths.py

--- src/qibolab/paths.py
+++ src/qibolab/paths.py
@@ -1,6 +1,8 @@
 from pathlib import Path
+import os
 
 qibolab_folder = Path(__file__).parent
 src_folder = qibolab_folder.parent
 project_folder = src_folder.parent
 examples_folder = project_folder / 'examples'
+script_folder = Path(os.path.abspath(''))
