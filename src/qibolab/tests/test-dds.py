07e0a36442a13fa6f2b231dbe2c67c9c004d6840
Prerna Baranwal <prernabaranwal2190@gmail.com>
2022-07-27 11:24:22 +0800

    Up[date test-dds.py

--- src/qibolab/tests/test-dds.py
+++ src/qibolab/tests/test-dds.py
@@ -1,15 +1,11 @@
-# from __future__ import absolute_import
-# import numpy as np
-# from qibolab.instruments.abstract import AbstractInstrument, InstrumentException
-# from qibolab.instruments.icarusqfpga import AbstractInstrument
-# from qibolab.instruments.ad9959 import AD9959
-
 import yaml
 import pytest
 import numpy as np 
 from qibolab.paths import qibolab_folder
 from qibolab.instruments.abstract import AbstractInstrument, InstrumentException
+from qibolab.platforms.cold_ion_qubit import cold_ion
 from qibolab.instruments.dds_ad9959 import AD9959
+
 
 
 evaluation_board = AD9959()
