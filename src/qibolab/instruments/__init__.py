b0c82326b997034c0ad78be3675b5e45d632fc29
Prerna Baranwal <prernabaranwal2190@gmail.com>
2022-07-27 11:32:36 +0800

    Update __init__.py

--- src/qibolab/instruments/__init__.py
+++ src/qibolab/instruments/__init__.py
@@ -2,4 +2,4 @@ from qibolab.instruments.qblox import PulsarQRM, PulsarQCM, ClusterQRM, ClusterQ
 from qibolab.instruments.rohde_schwarz import SGS100A
 from qibolab.instruments.icarusq import TektronixAWG5204, AlazarADC, MCAttenuator, QuicSyn
 from qibolab.instruments.icarusqfpga import IcarusQFPGA, PulseBlaster
-from dds_ad9959 import AD9959
+from qibolab.instruments.dds_ad9959 import AD9959
