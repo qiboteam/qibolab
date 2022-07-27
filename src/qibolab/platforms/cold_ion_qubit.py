79f5de7649b002f6ba797f2779a477af7307e4c0
Prerna Baranwal <prernabaranwal2190@gmail.com>
2022-07-27 11:20:01 +0800

    Update cold_ion_qubit.py

--- src/qibolab/platforms/cold_ion_qubit.py
+++ src/qibolab/platforms/cold_ion_qubit.py
@@ -1,10 +1,8 @@
 from qibo.config import raise_error
 from qibolab.platforms.abstract import AbstractPlatform
-
-
+# additional contents to be added laetr on
 class cold_ion(AbstractPlatform):
         def __init__(self, name, runcard):
-         super().__init__(name, runcard)
- 
-         pid = self.settings.get("pid")
-         vid = self.settings.get("vid")
+                super().__init__(name, runcard)
+                pid = self.settings.get("pid")
+                vid = self.settings.get("vid")
