9f17407096032083d313233193cebe44679e2efc
andrea-pasquale <andreapasquale97@gmail.com>
2022-08-05 17:49:39 +0400

    Remove instruments import from __init__

--- src/qibolab/instruments/__init__.py
+++
@@ -1,4 +0,0 @@
-from qibolab.instruments.qblox import ClusterQRM_RF, ClusterQCM_RF
-from qibolab.instruments.rohde_schwarz import SGS100A
-from qibolab.instruments.icarusq import TektronixAWG5204, AlazarADC, MCAttenuator, QuicSyn
-from qibolab.instruments.icarusqfpga import IcarusQFPGA, PulseBlaster
