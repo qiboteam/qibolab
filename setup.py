ce228c2b2693d0189b46d32e8533fc5a7241c10d
David Fuentes Ruiz <trapeya25@gmail.com>
2022-08-15 12:19:18 +0200

    minor fix in setup version

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ setup(
             "IPython"],
         # TII system dependencies
         "tiiq": [
-            "qblox-instruments=0.6.1",    # 0.6.1
+            "qblox-instruments==0.6.1",    # 0.6.1
             "quantify-core",        # 0.6.0
             "dash",                 # Live plotting
         ]
