9187e01e3d4c04069fa5fb688e55847c61028733
Prerna Baranwal <prernabaranwal2190@gmail.com>
2022-08-16 16:45:49 +0800

    update cold_ion_qubit.py

--- src/qibolab/platforms/cold_ion_qubit.py
+++ src/qibolab/platforms/cold_ion_qubit.py
@@ -1,7 +1,15 @@
-from qibo.config import raise_error
+from qibo.config import log, raise_error
 from qibolab.platforms.abstract import AbstractPlatform
 # additional contents to be added later on
 # abstract paltform will be implemented later on
 class cold_ion(AbstractPlatform):
+        def _init__(self,name,runcard):
+                log.info(f"Loading platform {name} from runcard {runcard}")
+                self.name = name
+                self.runcard = runcard
+                self.is_connected = False
+                            
         def run_calibration(self):
-                raise_error(NotImplementedError)
+                return self.name
+#                raise_error(NotImplementedError)
+        
