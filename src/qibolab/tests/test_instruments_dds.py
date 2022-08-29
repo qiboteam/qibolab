96a7b5c77f6c1462b564237c6f3ccc3554dadd22
Prerna Baranwal <prernabaranwal2190@gmail.com>
2022-08-29 13:27:55 +0800

    update test_instruments

--- src/qibolab/tests/test_instruments_dds.py
+++ src/qibolab/tests/test_instruments_dds.py
@@ -8,7 +8,7 @@ from qibolab.instruments.dds_ad9959 import AD9959
 from qibolab.paths import qibolab_folder
 from qibolab.platforms.cold_ion_qubit import cold_ion
 
-evaluation_board = AD9959()
-evaluation_board.set_clock_multiplier(20)
-evaluation_board.set_frequency(20e6, channel=[0])
-evaluation_board.set_frequency(200e6, channel=[0])
+# evaluation_board = AD9959()
+# evaluation_board.set_clock_multiplier(20)
+# evaluation_board.set_frequency(20e6, channel=[0])
+# evaluation_board.set_frequency(200e6, channel=[0])
