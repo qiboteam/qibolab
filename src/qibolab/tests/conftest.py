015814095c7c68ec8fdf5fea68349c0cf9e11dd2
Stavros Efthymiou <35475381+stavros11@users.noreply.github.com>
2022-11-08 14:07:10 +0400

    Select tests using marker

--- src/qibolab/tests/conftest.py
+++ src/qibolab/tests/conftest.py
@@ -5,8 +5,6 @@ from qibolab.platform import Platform
 
 def pytest_addoption(parser):
     parser.addoption("--platforms", type=str, action="store", default="tii5q", help="qpu platforms to test on")
-    parser.addoption("--skip-qpu", action="store_true", help="skip tests that require qpu")
-    parser.addoption("--skip-no-qpu", action="store_true", help="skip tests that do not require qpu")
 
 
 def pytest_configure(config):
@@ -15,10 +13,6 @@ def pytest_configure(config):
 
 def pytest_runtest_setup(item):
     marked_qpu = "qpu" in {mark.name for mark in item.iter_markers()}
-    if item.config.getoption("--skip-qpu") and marked_qpu:
-        pytest.skip("Skipping test that requires qpu.")
-    elif item.config.getoption("--skip-no-qpu") and not marked_qpu:
-        pytest.skip("Skipping test that does not require qpu.")
 
 
 def pytest_generate_tests(metafunc):
