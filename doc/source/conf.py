a9c735eddcb4e1e42df909d75a6b7234d6e92af8
MatteoRobbiati <robbiati.matteo@gmail.com>
2022-11-11 10:08:54 +0100

    fixed package name and extension error

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -13,25 +13,23 @@
 import os
 import sys
 
-from recommonmark.transform import AutoStructify
+sys.path.insert(0, os.path.abspath("."))
 
-sys.path.insert(0, os.path.abspath(".."))
-import qibo
+import qibolab
 
 # -- Project information -----------------------------------------------------
 
-project = "Qibo"
-copyright = "2020-2022 by the Qibo team"
+project = "qibolab"
+copyright = "2021, The Qibo team"
 author = "The Qibo team"
 
-# The full version, including alpha/beta/rc tags
-# release = qibo.__version__
+release = qibolab.__version__
 
 
 # -- General configuration ---------------------------------------------------
-#
+
 # https://stackoverflow.com/questions/56336234/build-fail-sphinx-error-contents-rst-not-found
-master_doc = "index"
+# master_doc = "index"
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -43,22 +41,10 @@ extensions = [
     "sphinx.ext.napoleon",
     "sphinx.ext.intersphinx",
     "recommonmark",
-    "nbsphinx",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
-
-# Markdown configuration
-
-# The suffix(es) of source filenames.
-# You can specify multiple suffix as a list of string:
-#
-source_suffix = {".rst": "restructuredtext", ".txt": "markdown", ".md": "markdown"}
-
-autosectionlabel_prefix_document = True
-# Allow to embed rst syntax in  markdown files.
-enable_eval_rst = True
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -87,26 +73,8 @@ html_theme_options = {
 html_static_path = ["_static"]
 
 
-# -- Intersphinx  -------------------------------------------------------------
-
-intersphinx_mapping = {"python": ("https://docs.python.org/3", None)}
-
-
-# -- Doctest ------------------------------------------------------------------
-#
-
-doctest_path = [os.path.abspath("../examples")]
-
-# -- Autodoc ------------------------------------------------------------------
-#
-autodoc_member_order = "bysource"
-
-# Adapted this from
-# https://github.com/readthedocs/recommonmark/blob/ddd56e7717e9745f11300059e4268e204138a6b1/docs/conf.py
-# app setup hook
 def setup(app):
-    app.add_config_value("recommonmark_config", {"enable_eval_rst": True}, True)
-    app.add_transform(AutoStructify)
+    """Include custom style to change colors"""
     app.add_css_file("css/style.css")
 
 
