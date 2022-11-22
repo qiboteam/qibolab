e5b40942212f9f2d19dd65344cc7530b0ca653dc
Javier Serrano <javier.serrano@tii.ae>
2022-11-22 15:31:11 +0400

    Driver implementation with Json

---
+++ src/qibolab/platforms/rfsoc_old.py
@@ -0,0 +1,77 @@
+import yaml
+from qibo.config import log
+import numpy as np
+from qibolab.platforms.abstract import AbstractPlatform
+
+
+class RFSocPlatform(AbstractPlatform):
+    def __init__(self, name, runcard):
+        log.info(f"Loading platform {name} from runcard {runcard}")
+        self.name = name
+        self.runcard = runcard
+        self.is_connected = False
+        # Load platform settings
+        with open(runcard) as file:
+            self.settings = yaml.safe_load(file)
+
+        self.nqubits = self.settings.get("nqubits")
+        if self.nqubits == 1:
+            self.resonator_type = "3D"
+        else:
+            self.resonator_type = "2D"
+
+#        self.soc = QickSoc()
+        self.cfg = dict(self.settings.get("config"))
+        # self.cfg["sigma"] = self.soc.us2cycles(0.025, gen_ch=qubit_channel)
+        self.host = self.cfg["ip_address"]
+        self.port = self.cfg["ip_port"]
+    def reload_settings(self):
+        raise NotImplementedError
+
+    def run_calibration(self, show_plots=False):  # pragma: no cover
+        raise NotImplementedError
+
+    def connect(self):
+        raise NotImplementedError
+
+    def setup(self):
+        raise NotImplementedError
+
+    def start(self):
+        raise NotImplementedError
+
+    def stop(self):
+        raise NotImplementedError
+
+    def disconnect(self):
+        raise NotImplementedError
+
+    def execute_pulse_sequence(self, sequence, var1,  nshots=None):
+        import socket
+        import struct
+        data = struct.pack('BB', 3, var1)
+    # Create a socket (SOCK_STREAM means a TCP socket)
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            # Connect to server and send data
+            sock.connect((self.host, self.port))
+            sock.sendall(bytes(data))
+        #    sock.sendall(bytes(data , "utf-8"))
+
+            # Receive data from the server and shut down
+            received = sock.recv(1024)
+
+            avg = struct.unpack("ffff",received)
+#            avgiRe = struct.unpack("f",sock.recv(4)[0])
+#            avgiIm = struct.unpack("f",sock.recv(4)[0])           
+            avgi = np.array([avg[0], avg[1]])
+            avgq = np.array([avg[2], avg[3]])
+        return avgi, avgq
+    
+
+        """
+        program = Program(self.soc, self.cfg, sequence)
+        # TODO: Pass optional values: ``threshold``
+        avgi, avgq = program.acquire(self.soc, load_pulses=True, progress=False, debug=False)
+        return avgi, avgq
+        """
+
