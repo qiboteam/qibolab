5326c82263fcc5a8ff38415abf8a57c079e6661e
Javier Serrano <javier.serrano@tii.ae>
2022-11-24 15:33:23 +0400

    Driver implemented in the platform

--- src/qibolab/platforms/rfsoc.py
+++ src/qibolab/platforms/rfsoc.py
@@ -2,7 +2,16 @@ import yaml
 from qibo.config import log
 import numpy as np
 from qibolab.platforms.abstract import AbstractPlatform
-
+from qibolab.pulses import (
+    Drag,
+    Gaussian,
+    Pulse,
+    PulseSequence,
+    ReadoutPulse,
+    Rectangular,
+)
+import socket
+import json
 
 class RFSocPlatform(AbstractPlatform):
     def __init__(self, name, runcard):
@@ -14,28 +23,44 @@ class RFSocPlatform(AbstractPlatform):
         with open(runcard) as file:
             self.settings = yaml.safe_load(file)
 
-        self.nqubits = self.settings.get("nqubits")
-        if self.nqubits == 1:
-            self.resonator_type = "3D"
-        else:
-            self.resonator_type = "2D"
 
-#        self.cfg = dict(self.settings.get("config"))
-        self.cfg = dict(self.settings.get("instruments"))
-        self.host = self.cfg["tii_rfsoc4x2"]["settings"]["ip_address"]
-        self.port = self.cfg["tii_rfsoc4x2"]["settings"]["ip_port"]
-        print("Configuracion: ", self.cfg)
+        self.cfg = self.settings["instruments"]["tii_rfsoc4x2"]["settings"]
+
+        self.host = self.cfg["ip_address"]
+        self.port = self.cfg["ip_port"]
+
+     # Create a socket (SOCK_STREAM means a TCP socket) and send configuration
+        jsonDic = self.cfg
+        jsonDic["opCode"] = "configuration"
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            # Connect to server and send data
+            sock.connect((self.host, self.port))
+            sock.sendall(json.dumps(jsonDic).encode())
+        sock.close()        
+
     def reload_settings(self):
         raise NotImplementedError
 
     def run_calibration(self, show_plots=False):  # pragma: no cover
         raise NotImplementedError
 
-    def connect(self):
+    def connect(self):      
         raise NotImplementedError
 
-    def setup(self):
-        raise NotImplementedError
+    def setup(self, rabi_length):
+        self.experiment = self.settings["instruments"]["tii_rfsoc4x2"]["experiment"]
+        self.experiment["rabi_length"] = rabi_length
+
+        jsonDic = self.experiment 
+        jsonDic["opCode"] = "setup"
+        # Create a socket (SOCK_STREAM means a TCP socket)
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            # Connect to server and send data
+            sock.connect((self.host, self.port))
+            sock.sendall(json.dumps(jsonDic).encode())
+
+        sock.close()
+        return  
 
     def start(self):
         raise NotImplementedError
@@ -46,35 +71,56 @@ class RFSocPlatform(AbstractPlatform):
     def disconnect(self):
         raise NotImplementedError
 
-    def _generate_json_packet(self, var1):
-        # Generate the json packet to send over TCP
-        return {
-            "opcode": 3, 
-            "length": var1
-            }
-        
 
-    def execute_pulse_sequence(self, sequence, rabi_length,  nshots=None):
-        import socket
-        import json
 
-        self.cfg["tii_rfsoc4x2"]["settings"]["rabi_length"] = rabi_length
+    def execute_pulse_sequence(self, sequence,   nshots=None):
+        ps: PulseShape
 
-        jsonDic = self.cfg 
+        jsonDic = {}
+        i = 0
+        for pulse in sequence:
+            ps = pulse.shape
+            if type(ps) is Drag:
+                shape = "Drag"
+                rel_sigma = ps.rel_sigma
+                beta = ps.beta
+            elif type(ps) is Gaussian:
+                shape = "Gaussian"
+                rel_sigma = ps.rel_sigma
+                beta = 0
+            elif type(ps) is Rectangular:
+                shape = "Rectangular"
+                rel_sigma = 0
+                beta = 0
 
-    # Create a socket (SOCK_STREAM means a TCP socket)
+            pulseDic = {"start": pulse.start, 
+                        "duration": pulse.duration,
+                        "amplitude": pulse.amplitude,
+                        "frequency": pulse.frequency,
+                        "relative_phase": pulse.relative_phase,
+                        "shape": shape,
+                        "rel_sigma": rel_sigma,
+                        "beta": beta,
+                        "channel": pulse.channel,
+#                        "type": pulse.type,
+                        "qubit": pulse.qubit
+                        }
+            jsonDic['pulse'+str(i)] = pulseDic
+            i = i+1
+
+        jsonDic["opCode"] = "execute"
+        # Create a socket (SOCK_STREAM means a TCP socket)
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
             # Connect to server and send data
             sock.connect((self.host, self.port))
             sock.sendall(json.dumps(jsonDic).encode())
-            print("datos enviados")
             # Receive data from the server and shut down
-            received = sock.recv(1024)
+            received = sock.recv(256)
             avg = json.loads(received.decode('utf-8'))          
             avgi = np.array([avg["avgiRe"], avg["avgiIm"]])
             avgq = np.array([avg["avgqRe"], avg["avgqIm"]])
         sock.close()
 
         return avgi, avgq
-    
+  
 
