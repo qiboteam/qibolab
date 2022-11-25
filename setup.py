701b06d64512833be458b6aa51acaa9816b06908
Edoardo-Pedicillo <edoardo.pedicillo@gmail.com>
2022-11-25 15:30:18 +0400

    change example

--- setup.py
+++ setup.py
@@ -53,6 +53,7 @@ setup(
             "sphinx_markdown_tables",
             "nbsphinx",
             "IPython",
+            "sphinx_copybutton",
         ],
         # TII system dependencies
         "tiiq": [
