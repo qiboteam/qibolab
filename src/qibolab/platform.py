ed810eac6608feb837300812c1fbe3837592f186
maxime.hantute@live.fr <maxime.hantute@live.fr>
2022-11-25 18:16:54 +0400

    to david

--- src/qibolab/platform.py
+++ src/qibolab/platform.py
@@ -12,7 +12,8 @@ def Platform(name, runcard=None):
         from qibolab.paths import qibolab_folder
 
         runcard = qibolab_folder / "runcards" / f"{name}.yml"
-    if name == "tii1q" or name == "tii5q" or name == "qili":
+
+    if name == "tii1q" or name == "qw5q_gold" or name == "qili":
         from qibolab.platforms.multiqubit import MultiqubitPlatform as Device
     elif name == "icarusq":
         from qibolab.platforms.icplatform import ICPlatform as Device
