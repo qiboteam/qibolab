176bb4077e6c031d160529deab9621128d52c347
simone bordoni <simone.bordoni@uniroma1.it>
2022-12-06 18:44:58 +0400

    fix transpilers_transpile

--- src/qibolab/transpilers/transpile.py
+++ src/qibolab/transpilers/transpile.py
@@ -1,17 +1,16 @@
 from qibo import gates
-from qibo.config import log
+from qibo.config import log, raise_error
 
 from qibolab.transpilers.connectivity import fix_connectivity
 from qibolab.transpilers.native import NativeGates
 
 
-def transpile(circuit, fuse_one_qubit=False):
+def transpile(circuit, two_qubit_native="optimized", fuse_one_qubit=False):
     """Implements full transpilation pipeline.
 
     Args:
         circuit (qibo.models.Circuit): Circuit model to transpile.
-        fuse_one_qubit (bool): If ``True`` it fuses adjacent one-qubit gates to reduce
-            circuit depth.
+        two_qubit_native: two qubit native gate ("CZ", "iSWAP" or "optimized")
 
     Returns:
         new (qibo.models.Circuit): New circuit that can be executed on tii5q platform.
@@ -31,7 +30,7 @@ def transpile(circuit, fuse_one_qubit=False):
     new, hardware_qubits = fix_connectivity(circuit)
 
     # two-qubit gates to native
-    native_gates = NativeGates()
+    native_gates = NativeGates(two_qubit_native=two_qubit_native)
     new = native_gates.translate_circuit(new, translate_single_qubit=False)
 
     # Optional: fuse one-qubit gates to reduce circuit depth
@@ -44,17 +43,17 @@ def transpile(circuit, fuse_one_qubit=False):
     return new, hardware_qubits
 
 
-def can_execute(circuit, verbose=True):
+def can_execute(circuit, two_qubit_native="optimized"):
     """Checks if a circuit can be executed on tii5q.
 
     Args:
         circuit (qibo.models.Circuit): Circuit model to check.
-        verbose (bool): If ``True`` it prints debugging log messages.
+        two_qubit_native: two qubit native gate ("CZ", "iSWAP" or "optimized")
 
     Returns ``True`` if the following conditions are satisfied:
         - Circuit does not contain more than two-qubit gates.
         - All one-qubit gates are I, Z, RZ or U3.
-        - All two-qubit gates are CZ.
+        - All two-qubit gates are CZ or iSWAP based on two_qubit_native.
         - All two-qubit gates have qubit 0 as target or control.
     otherwise returns ``False``.
     """
@@ -66,14 +65,25 @@ def can_execute(circuit, verbose=True):
 
         if len(gate.qubits) == 1:
             if not isinstance(gate, (gates.I, gates.Z, gates.RZ, gates.U3)):
-                if verbose:
-                    vlog(f"{gate.name} is not native gate.")
+                log.info(f"{gate.name} is not a single qubit native gate.")
                 return False
 
         elif len(gate.qubits) == 2:
-            if not isinstance(gate, gates.CZ):
-                vlog(f"{gate.name} is not native gate.")
-                return False
+            if two_qubit_native == "CZ":
+                if not isinstance(gate, gates.CZ):
+                    log.info(f"{gate.name} is not a two qubit native gate.")
+                    return False
+            # TODO: all gates mapped to iSWAP
+            elif two_qubit_native == "iSWAP":
+                if not isinstance(gate, (gates.CZ, gates.iSWAP)):
+                    log.info(f"{gate.name} is not a two qubit native gate.")
+                    return False
+            elif two_qubit_native == "optimized":
+                if not isinstance(gate, (gates.CZ, gates.iSWAP)):
+                    log.info(f"{gate.name} is not a two qubit native gate.")
+                    return False
+            else:
+                raise_error(NotImplementedError, "Use CZ, iSWAP or optimized for two_qubit_native")
             if 0 not in gate.qubits:
                 vlog("Circuit does not respect connectivity. " f"{gate.name} acts on {gate.qubits}.")
                 return False
