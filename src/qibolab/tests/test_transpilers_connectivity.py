770cace2f95871c5070f5228d0f56943cd3c86b0
simone bordoni <simone.bordoni@uniroma1.it>
2022-12-16 16:36:24 +0400

    fix tests

--- src/qibolab/tests/test_transpilers_connectivity.py
+++ src/qibolab/tests/test_transpilers_connectivity.py
@@ -64,7 +64,7 @@ def test_fix_connectivity_unitaries(run_number, nqubits, unitary_dim, depth):
     """Checks that the transpiled circuit can be executed and is equivalent to original
     when using unitaries."""
 
-    from qibolab.tests.test_transpilers_decompositions import random_unitary
+    from qibolab.tests.test_transpilers_unitary_decompositions import random_unitary
 
     original = Circuit(nqubits)
     pairs = list(itertools.combinations(range(nqubits), unitary_dim))
