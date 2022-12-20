2e113586474021c05e8f754383d835accbfb1d03
Stavros Efthymiou <35475381+stavros11@users.noreply.github.com>
2022-12-20 18:34:36 +0400

    Accept only one platform for each test run

--- src/qibolab/tests/conftest.py
+++ src/qibolab/tests/conftest.py
@@ -4,7 +4,7 @@ from qibolab.platform import Platform
 
 
 def pytest_addoption(parser):
-    parser.addoption("--platforms", type=str, action="store", default=None, help="qpu platforms to test on")
+    parser.addoption("--platform", type=str, action="store", default=None, help="qpu platform to test")
 
 
 def pytest_configure(config):
@@ -12,13 +12,12 @@ def pytest_configure(config):
 
 
 def pytest_generate_tests(metafunc):
-    platforms = metafunc.config.option.platforms
-    platforms = [] if platforms is None else platforms.split(",")
+    platform_name = metafunc.config.option.platform
 
     if "platform_name" in metafunc.fixturenames:
         if "qubit" in metafunc.fixturenames:
             # TODO: Do backend initialization here instead of every test (currently does not work)
-            qubits = [(n, q) for n in platforms for q in range(Platform(n).nqubits)]
+            qubits = [(platform_name, q) for q in range(Platform(platform_name).nqubits)]
             metafunc.parametrize("platform_name,qubit", qubits)
         else:
-            metafunc.parametrize("platform_name", platforms)
+            metafunc.parametrize("platform_name", [platform_name])
