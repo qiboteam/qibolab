335c56cccc98aa3ac34709572c77e80dfa5d1902
Stavros Efthymiou <35475381+stavros11@users.noreply.github.com>
2022-12-20 18:45:45 +0400

    Fix multiqubit platform tests

--- tests/test_platforms_multiqubit.py
+++ tests/test_platforms_multiqubit.py
@@ -1,5 +1,4 @@
 import os
-import pathlib
 import shutil
 
 import numpy as np
@@ -16,11 +15,9 @@ from qibolab.pulses import PulseSequence
 qubit = 0
 nshots = 1024
 
-HERE = pathlib.Path(__file__).parent
-
 
 def copy_runcard(platform_name):
-    test_runcard = HERE / "test_platforms_multiqubit.yml"
+    test_runcard = qibolab_folder / "tests" / "test_platforms_multiqubit.yml"
     if not os.path.exists(test_runcard):
         original_runcard = qibolab_folder / "runcards" / f"{platform_name}.yml"
         shutil.copyfile(str(original_runcard), test_runcard)
@@ -128,9 +125,7 @@ def test_multiqubitplatform_execute_multiple_drive_pulses_one_readout(platform):
 
 
 @pytest.mark.qpu
-def test_multiqubitplatform_execute_multiple_drive_pulses_one_readout_no_spacing(
-    platform,
-):
+def test_multiqubitplatform_execute_multiple_drive_pulses_one_readout_no_spacing(platform):
     # Multiple qubit drive pulses and one readout pulse with no spacing between them
     sequence = PulseSequence()
     sequence.add(platform.create_qubit_drive_pulse(qubit, start=0, duration=200))
@@ -141,9 +136,7 @@ def test_multiqubitplatform_execute_multiple_drive_pulses_one_readout_no_spacing
 
 
 @pytest.mark.qpu
-def test_multiqubitplatform_execute_multiple_overlaping_drive_pulses_one_readout(
-    platform,
-):
+def test_multiqubitplatform_execute_multiple_overlaping_drive_pulses_one_readout(platform):
     # Multiple overlapping qubit drive pulses and one readout pulse
     sequence = PulseSequence()
     sequence.add(platform.create_qubit_drive_pulse(qubit, start=0, duration=200))
