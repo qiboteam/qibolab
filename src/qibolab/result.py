cd91c37919500d0ac585292d03010a11fd0a0812
andrea-pasquale <andreapasquale97@gmail.com>
2023-01-31 11:17:33 +0400

    Implement 2d sweeper for dummy

--- src/qibolab/result.py
+++ src/qibolab/result.py
@@ -4,81 +4,60 @@ from typing import Optional
 
 import numpy as np
 import numpy.typing as npt
-
-ExecRes = np.dtype([("i", np.float64), ("q", np.float64)])
-
-
-@dataclass
-class AveragedResults:
-    """Data structure containing averages of ``ExecutionResults``."""
-
-    i: npt.NDArray[np.float64]
-    q: npt.NDArray[np.float64]
-
-    def __add__(self, data):
-        i = np.append(self.i, data.i)
-        q = np.append(self.q, data.q)
-
-        new_execution_results = self.__class__(i, q)
-
-        return new_execution_results
-
-    def to_dict(self):
-        """Serialize output in dict"""
-
-        return {
-            "MSR[V]": np.sqrt(self.i**2 + self.q**2),
-            "i[V]": self.i,
-            "q[V]": self.q,
-            "phase[rad]": np.angle(self.i + 1.0j * self.q),
-        }
-
-    def __len__(self):
-        return len(self.i)
+from scipy import signal
 
 
 @dataclass
 class ExecutionResults:
     """Data structure to deal with the output of :func:`qibolab.platforms.abstract.AbstractPlatform.execute_pulse_sequence`"""
 
-    array: npt.NDArray[ExecRes]
+    _i: npt.NDArray[np.float64]
+    _q: npt.NDArray[np.float64]
     shots: Optional[npt.NDArray[np.uint32]] = None
 
     @classmethod
     def from_components(cls, is_, qs_, shots=None):
-        ar = np.empty(is_.shape, dtype=ExecRes)
-        ar["i"] = is_
-        ar["q"] = qs_
-        ar = np.rec.array(ar)
-        return cls(ar, shots)
+        return cls(is_, qs_, shots)
+
+    @property
+    def in_progress(self):
+        """Placeholder for when we implement live fetching of data from instruments."""
+        return False
 
     @property
     def i(self):
-        return self.array.i
+        return self._i
+
+    @i.setter
+    def i(self, value):
+        self._i = value
 
     @property
     def q(self):
-        return self.array.q
+        return self._q
+
+    @q.setter
+    def q(self, value):
+        self._q = value
 
     def __add__(self, data):
-        i = np.append(self.i, data.i, axis=0)
-        q = np.append(self.q, data.q, axis=0)
+        i = np.append(self.i, data.i)
+        q = np.append(self.q, data.q)
 
         new_execution_results = self.__class__.from_components(i, q)
 
         return new_execution_results
 
     @cached_property
-    def measurement(self):
-        """Resonator signal voltage mesurement (MSR) in volts."""
+    def msr(self):
+        """Computes msr value."""
         return np.sqrt(self.i**2 + self.q**2)
 
     @cached_property
     def phase(self):
         """Computes phase value."""
         phase = np.angle(self.i + 1.0j * self.q)
-        return phase
-        # return signal.detrend(np.unwrap(phase))
+        return signal.detrend(np.unwrap(phase))
 
     @cached_property
     def ground_state_probability(self):
@@ -96,10 +75,6 @@ class ExecutionResults:
             return {"probability": 1 - self.ground_state_probability}
         elif state == 0:
             return {"probability": self.ground_state_probability}
-
-    def compute_average(self):
-        """Perform average over i and q"""
-        return AveragedResults(self.array.i.mean(), self.array.q.mean())
 
     def to_dict(self, average=False):
         """Serialize output in dict.
