62d41f4a818bab8315b8a4ebfe57118b464fac48
andrea-pasquale <andreapasquale97@gmail.com>
2023-01-31 11:17:33 +0400

    Implement 2d sweeper for dummy

--- src/qibolab/result.py
+++ src/qibolab/result.py
@@ -1,53 +1,68 @@
 from dataclasses import dataclass
 from functools import cached_property
+from typing import Optional
 
 import numpy as np
 import numpy.typing as npt
-
-ExecRes = np.dtype([("i", np.float64), ("q", np.float64), ("shots", np.uint32)])
+from scipy import signal
 
 
 @dataclass
 class ExecutionResults:
     """Data structure to deal with the output of execute_pulse_sequence"""
 
-    array: npt.NDArray[ExecRes]
+    _i: npt.NDArray[np.float64]
+    _q: npt.NDArray[np.float64]
+    shots: Optional[npt.NDArray[np.uint32]] = None
 
     @classmethod
-    def from_components(cls, is_, qs, shots):
-        ar = np.empty(len(is_), dtype=ExecRes)
-        ar["i"] = is_
-        ar["q"] = qs
-        ar["shots"] = shots
-        ar = np.rec.array(ar)
-        return cls(ar)
+    def from_components(cls, is_, qs_, shots=None):
+        return cls(is_, qs_, shots)
+
+    @property
+    def in_progress(self):
+        """Placeholder for when we implement live fetching of data from instruments."""
+        return False
 
     @property
     def i(self):
-        return self.array.i
+        return self._i
+
+    @i.setter
+    def i(self, value):
+        self._i = value
 
     @property
     def q(self):
-        return self.array.q
+        return self._q
 
-    @property
-    def shots(self):
-        return self.array.shots
+    @q.setter
+    def q(self, value):
+        self._q = value
+
+    def __add__(self, data):
+        i = np.append(self.i, data.i)
+        q = np.append(self.q, data.q)
+
+        new_execution_results = self.__class__.from_components(i, q)
+
+        return new_execution_results
 
     @cached_property
     def msr(self):
         """Computes msr value."""
-        return np.sqrt(self.array.i**2 + self.array.q**2)
+        return np.sqrt(self.i**2 + self.q**2)
 
     @cached_property
     def phase(self):
         """Computes phase value."""
-        return np.angle(self.array.i + 1.0j * self.array.q)
+        phase = np.angle(self.i + 1.0j * self.q)
+        return signal.detrend(np.unwrap(phase))
 
     @cached_property
     def ground_state_probability(self):
         """Computes ground state probability"""
-        return 1 - self.array.shots.mean()
+        return 1 - np.mean(self.shots)
 
     def to_dict_probability(self, state=1):
         """Serialize probabilities in dict.
@@ -61,7 +76,7 @@ class ExecutionResults:
         elif state == 0:
             return {"probability": self.ground_state_probability}
 
-    def to_dict(self, average=True):
+    def to_dict(self, average=False):
         """Serialize output in dict.
         Args:
             average (bool): If `True` returns a dictionary of the form
