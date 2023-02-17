a9bbeac9bfa680bff1d0c63c0c631e82999fde52
Stavros Efthymiou <35475381+stavros11@users.noreply.github.com>
2023-02-17 18:10:51 +0400

    Rename script and add comment

--- qputests.sh
+++ qputests.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Script for running the selfhosted tests on QPUs directly from GitHub
+# Tests need to be copied to /tmp/ because coverage does not work with NFS
+
 cp -r tests /tmp/
 cd /tmp/tests
 pytest --cov=qibolab --cov-report=xml -m qpu --platforms $PLATFORM
