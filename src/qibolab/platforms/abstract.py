5f9fb0c96773f3924bd0f48a4544894492e37aac
andrea-pasquale <andreapasquale97@gmail.com>
2023-02-28 17:07:38 +0400

    Fix resonator type

--- src/qibolab/platforms/abstract.py
+++ src/qibolab/platforms/abstract.py
@@ -124,7 +124,11 @@ class AbstractPlatform(ABC):
             settings = self.settings = yaml.safe_load(file)
 
         self.nqubits = settings["nqubits"]
-        self.resonator_type = settings["resonator_type"]
+        if "resonator_type" in self.settings:
+            self.resonator_type = self.settings["resonator_type"]
+        else:
+            self.resonator_type = "3D" if self.nqubits == 1 else "2D"
+
         self.topology = settings["topology"]
 
         self.relaxation_time = settings["settings"]["repetition_duration"]
