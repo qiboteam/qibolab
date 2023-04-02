fix addition of AverageResults and ExecutionResults

src/qibolab/result.py
@@ -61,8 +61,12 @@ class ExecutionResults:
         return self.array.q
 
     def __add__(self, data):
-        i = np.append(self.i, data.i, axis=0)
-        q = np.append(self.q, data.q, axis=0)
+        if isinstance(data, AveragedResults):
+            i = np.append(self.i, [data.i], axis=0)
+            q = np.append(self.q, [data.q], axis=0)
+        else:
+            i = np.append(self.i, data.i, axis=0)
+            q = np.append(self.q, data.q, axis=0)
 
         new_execution_results = self.__class__.from_components(i, q)
 