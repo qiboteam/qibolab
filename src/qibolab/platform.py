9a39d10756bdca7b50624c78be1eae1d5ebdcbae
maxime.hantute@live.fr <maxime.hantute@live.fr>
2023-04-18 11:11:33 +0400

    LO fixed

--- src/qibolab/platform.py
+++ src/qibolab/platform.py
@@ -56,25 +56,25 @@ def create_tii_qw25q(runcard, simulation_duration=None, address=None, cloud=Fals
             "A": ["L2-1_a", "L2-1_b"],
             "B": ["L2-2_a", "L2-2_b"],
             "C": ["L2-3_a", "L2-3_b"],
-            "D": ["L2-4_a", "L2-4_b"],
+            # "D": ["L2-4_a", "L2-4_b"],
         },
         "readout": {
             "A": ["L3-26_a", "L3-26_b"],
             "B": ["L3-27_a", "L3-27_b"],
             "C": ["L3-18_a", "L3-18_b"],
-            "D": ["L3-30_a", "L3-30_b"],
+            # "D": ["L3-30_a", "L3-30_b"],
         },
         "drive": {
             "A": [f"L3-{i}" for i in range(1, 7)],
             "B": [f"L3-{i}" for i in range(7, 10)] + ["L3-19", "L4-22"],
             "C": [f"L4-{i}" for i in range(23, 28)],
-            "D": [f"L4-{i}" for i in range(28, 31)],
+            # "D": [f"L4-{i}" for i in range(28, 31)],
         },
         "flux": {
             "A": [f"L1-{i}" for i in range(5, 10)] + ["L1-4"],
             "B": [f"L1-{i}" for i in range(11, 16)],
             "C": [f"L1-{i}" for i in range(16, 21)],
-            "D": [f"L1-{i}" for i in range(21, 26)],
+            # "D": [f"L1-{i}" for i in range(21, 26)],
         },
     }
 
@@ -82,7 +82,7 @@ def create_tii_qw25q(runcard, simulation_duration=None, address=None, cloud=Fals
         "A": [1, 2, 3],
         "B": [4, 5],
         "C": [6, 7],
-        "D": [8, 9],
+        # "D": [8, 9],
     }
 
     # Create channels
@@ -153,10 +153,10 @@ def create_tii_qw25q(runcard, simulation_duration=None, address=None, cloud=Fals
         LO_RS_SGS100A(f"LO_0{i}", f"192.168.0.3{i}") for i in [1, 3, 4, 5, 6, 9]
     ]
     drive_local_oscillators = {
-        "A": ["LO_05"] + 2 * ["LO_01"] + 2 * ["LO_01"] + ["era_01"],
+        "A": ["LO_05"] + 2 * ["LO_01"] + ["LO_05"] + ["LO_01"] + ["era_01"],
         "B": ["era_02"] + 4 * ["LO_06"],
         "C": [f"era_0{i}" for i in range(3, 8)],
-        "D": ["era_08"] + 2 * ["LO_01"],
+        # "D": ["era_08"] + 2 * ["LO_01"],
     }
     # Configure local oscillator's frequency and power
     for lo in local_oscillators:
@@ -170,7 +170,7 @@ def create_tii_qw25q(runcard, simulation_duration=None, address=None, cloud=Fals
             lo.frequency = 7.7e9
             lo.power = 23
         elif lo.name == "LO_05":
-            lo.frequency = 5.4e9
+            lo.frequency = 5.37e9
             lo.power = 18
         elif lo.name == "LO_06":
             lo.frequency = 6.2e9
@@ -191,7 +191,7 @@ def create_tii_qw25q(runcard, simulation_duration=None, address=None, cloud=Fals
                 channels[wiring["feedback"][feedline][1]].local_oscillator = lo
         else:
             for feedline in drive_local_oscillators:
-                for i, name in enumerate(drive_local_oscillators[feedline]):
+                for i, name in enumerate(drive_local_oscillators[feedline]):                    
                     if lo.name == name:
                         channels[wiring["drive"][feedline][i]].local_oscillator = lo
 
@@ -213,10 +213,10 @@ def create_tii_qw25q(runcard, simulation_duration=None, address=None, cloud=Fals
                     if "era" in qubits[q].drive.local_oscillator.name:
                         qubits[q].drive.local_oscillator.frequency = qubits[q].drive_frequency + 200e6
 
-    for q in ["A1", "A2", "A4", "B1", "B2", "B3", "C1", "C4", "D1", "D2"]:  # Qubits with LO around 7e9
+    for q in ["A1", "A2", "A4", "B1", "B2", "B3", "C1", "C4"]:  # Qubits with LO around 7e9
         qubits[q].readout = channels[wiring["readout"][q[0]][0]]
         qubits[q].feedback = channels[wiring["feedback"][q[0]][0]]
-    for q in ["A3", "A5", "A6", "B4", "B5", "C2", "C3", "C5", "D3", "D4", "D5"]:  # Qubits with LO around 7.5e9
+    for q in ["A3", "A5", "A6", "B4", "B5", "C2", "C3", "C5"]:  # Qubits with LO around 7.5e9
         qubits[q].readout = channels[wiring["readout"][q[0]][1]]
         qubits[q].feedback = channels[wiring["feedback"][q[0]][1]]
 
@@ -534,8 +534,6 @@ def Platform(name, runcard=None, design=None):
         return create_tii_qw25q(runcard)
     elif name == "qw25q_A":
         return create_tii_qw25q_A(runcard)
-    elif name == "qw25q_B":
-        return create_tii_qw25q_B(runcard)
     else:
         from qibolab.platforms.multiqubit import MultiqubitPlatform as Device
 
