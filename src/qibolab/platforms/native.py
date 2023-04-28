688f3504d56f81040b09139a2f36e10be67de84c
Stavros Efthymiou <35475381+stavros11@users.noreply.github.com>
2023-04-28 15:49:34 +0400

    Use Enum for pulse type and remove NativeFluxPulse

--- src/qibolab/platforms/native.py
+++ src/qibolab/platforms/native.py
@@ -15,15 +15,11 @@ class NativePulse:
     duration: int
     amplitude: float
     shape: str
-    type: str
+    pulse_type: PulseType
     qubit: "Qubit"
     frequency: int = 0
     relative_start: int = 0
     """Relative start is relevant for two-qubit gate operations which correspond to a pulse sequence."""
-
-    def __post_init__(self):
-        if self.type != "ro" and self.type != "qd":
-            raise_error(TypeError, f"NativePulse was initialized with incorrect type {self.type}.")
 
     @classmethod
     def from_dict(cls, name, **kwargs):
@@ -35,8 +31,9 @@ class NativePulse:
                 implementing the gate.
         """
         field_names = {field.name for field in fields(cls)}
-        kwargs = {k: v for k, v in kwargs.items() if k in field_names}
-        return cls(name, **kwargs)
+        new_kwargs = {k: v for k, v in kwargs.items() if k in field_names}
+        new_kwargs["pulse_type"] = PulseType(kwargs["type"])
+        return cls(name, **new_kwargs)
 
     def pulse(self, start, relative_phase=0.0):
         """Construct the :class:`qibolab.pulses.Pulse` object implementing the gate.
@@ -49,48 +46,29 @@ class NativePulse:
             A :class:`qibolab.pulses.DrivePulse` or :class:`qibolab.pulses.DrivePulse`
             or :class:`qibolab.pulses.FluxPulse` with the pulse parameters of the gate.
         """
-        pulse_type = PulseType(self.type).name
-        pulse_cls = PulseConstructor[pulse_type].value
-        channel = getattr(self.qubit, pulse_type.lower()).name
-        return pulse_cls(
-            start + self.relative_start,
-            self.duration,
-            self.amplitude,
-            self.frequency,
-            relative_phase,
-            self.shape,
-            channel,
-            qubit=self.qubit.name,
-        )
+        if self.pulse_type is PulseType.FLUX:
+            return FluxPulse(
+                start + self.relative_start,
+                self.duration,
+                self.amplitude,
+                self.shape,
+                channel=self.qubit.flux.name,
+                qubit=self.qubit.name,
+            )
 
-
-@dataclass
-class NativeFluxPulse(NativePulse):
-    """Container with parameters required to create a :class:`qibolab.pulses.FluxPulse` object."""
-
-    def __post_init__(self):
-        if self.type != "qf":
-            raise_error(TypeError, f"NativeFluxPulse was initialized with incorrect type {self.type}.")
-
-    def pulse(self, start, relative_phase=0.0):
-        """Construct the :class:`qibolab.pulses.Pulse` object implementing the gate.
-
-        Args:
-            start (int): Start time of the pulse in the sequence.
-            relative_phase (float): Relative phase of the pulse.
-
-        Returns:
-            A :class:`qibolab.pulses.DrivePulse` or :class:`qibolab.pulses.DrivePulse`
-            or :class:`qibolab.pulses.FluxPulse` with the pulse parameters of the gate.
-        """
-        return FluxPulse(
-            start + self.relative_start,
-            self.duration,
-            self.amplitude,
-            self.shape,
-            channel=self.qubit.flux.name,
-            qubit=self.qubit.name,
-        )
+        else:
+            pulse_cls = PulseConstructor[self.pulse_type.name].value
+            channel = getattr(self.qubit, self.pulse_type.name.lower()).name
+            return pulse_cls(
+                start + self.relative_start,
+                self.duration,
+                self.amplitude,
+                self.frequency,
+                relative_phase,
+                self.shape,
+                channel,
+                qubit=self.qubit.name,
+            )
 
 
 @dataclass
@@ -99,7 +77,6 @@ class VirtualPulse:
 
     phase: float
     qubit: "Qubit"
-    type: str = "virtual_z"
 
 
 @dataclass
@@ -137,15 +114,8 @@ class NativeSequence:
             if pulse_type == "virtual_z":
                 phase = kwargs["phase"]
                 pulses.append(VirtualPulse(phase, qubit))
-            elif pulse_type == "qf":
-                name = f"{name}{i}"
-                pulses.append(NativeFluxPulse.from_dict(name, **kwargs, qubit=qubit))
-            elif pulse_type == "qd":
-                name = f"{name}{i}"
-                pulses.append(NativePulse.from_dict(name, **kwargs, qubit=qubit))
             else:
-                raise_error(TypeError, f"Incorrect type {pulse_type} was given in NativeSequence.")
-
+                pulses.append(NativePulse.from_dict(f"{name}{i}", **kwargs, qubit=qubit))
         return cls(name, pulses)
 
     def sequence(self, start=0):
@@ -181,8 +151,7 @@ class NativeSingleQubitGates:
             native_gates (dict): Dictionary with native gate pulse parameters as loaded
                 from the runcard.
         """
-        mz = NativePulse.from_dict("MZ", **native_gates["MZ"], qubit=qubit)
-        rx = NativePulse.from_dict("RX", **native_gates["RX"], qubit=qubit)
-        rx90 = NativePulse.from_dict("RX90", **native_gates["RX"], qubit=qubit)
-        rx90.amplitude /= 2.0
-        return cls(mz, rx, rx90)
+        pulses = {n: NativePulse.from_dict(n, **native_gates[n], qubit=qubit) for n in ["MZ", "RX"]}
+        pulses["RX90"] = NativePulse.from_dict("RX90", **native_gates["RX"], qubit=qubit)
+        pulses["RX90"].amplitude /= 2.0
+        return cls(**pulses)
