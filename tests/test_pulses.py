9a301873f33f2a07d8f7b7cf4f033791b6a351bd
Jadwiga Wilkens <wilkensjadwiga@gmail.com>
2023-05-02 14:01:09 +0400

    removed unused import

--- tests/test_pulses.py
+++ tests/test_pulses.py
@@ -23,7 +23,7 @@ from qibolab.pulses import (
     Waveform,
     eCap,
 )
-from qibolab.symbolic import floatSymbolicExpression as se_float
+
 from qibolab.symbolic import intSymbolicExpression as se_int
 
 HERE = pathlib.Path(__file__).parent
