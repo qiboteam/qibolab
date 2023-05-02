e8c673a25a406404f1f5809cb3212388dd218fdb
aorgazf <55031026+aorgazf@users.noreply.github.com>
2023-05-02 09:22:35 +0400

    some proposed changes to execution results

--- src/qibolab/result.py
+++ src/qibolab/result.py
@@ -1,101 +1,92 @@
-from dataclasses import dataclass
 from functools import cached_property
 from typing import Optional
 
 import numpy as np
 import numpy.typing as npt
 
-ExecRes = np.dtype([("i", np.float64), ("q", np.float64)])
+iq_record_dtype = np.dtype([("i", np.float64), ("q", np.float64)])
 
 
-@dataclass
 class ExecutionResults:
     """Data structure to deal with the output of :func:`qibolab.platforms.abstract.AbstractPlatform.execute_pulse_sequence`"""
 
-    array: npt.NDArray[ExecRes]
-    shots: Optional[npt.NDArray[np.uint32]] = None
+    def __init__(self, i:np.ndarray | list, q:np.ndarray | list, state:np.ndarray | list = None):
+        if not isinstance(i, np.ndarray):
+            i = np.array(i)
+        if not isinstance(q, np.ndarray):
+            q = np.array(q)
+        if i.shape != q.shape:
+            raise ValueError("is_ and qs_ must have the same size")
 
-    @classmethod
-    def from_components(cls, is_, qs_, shots=None):
-        ar = np.empty(is_.shape, dtype=ExecRes)
-        ar["i"] = is_
-        ar["q"] = qs_
-        ar = np.rec.array(ar)
-        return cls(ar, shots)
+        self.voltage: npt.NDArray[iq_record_dtype] = np.recarray(i.shape, dtype=iq_record_dtype)
+        self.voltage["i"] = i
+        self.voltage["q"] = q
+
+        if state and not isinstance(state, np.ndarray):
+            state = np.array(state)
+        self.state: Optional[npt.NDArray[np.uint32]] = state
 
     @property
     def i(self):
-        return self.array.i
+        return self.voltage.i
 
     @property
     def q(self):
-        return self.array.q
-
-    def __add__(self, data):
-        assert len(data.i.shape) == len(data.q.shape)
-        if data.shots is not None:
-            assert len(data.i.shape) == len(data.shots.shape)
-        # concatenate on first dimension; if a scalar is passed, just append it
-        axis = 0 if len(data.i.shape) > 0 else None
-        i = np.append(self.i, data.i, axis=axis)
-        q = np.append(self.q, data.q, axis=axis)
-        if data.shots is not None:
-            shots = np.append(self.shots, data.shots, axis=axis)
-        else:
-            shots = None
-
-        new_execution_results = self.__class__.from_components(i, q, shots)
-
-        return new_execution_results
+        return self.voltage.q
 
     @cached_property
-    def measurement(self):
-        """Resonator signal voltage mesurement (MSR) in volts."""
+    def magnitude(self):
+        """Signal magnitude in volts."""
         return np.sqrt(self.i**2 + self.q**2)
 
     @cached_property
     def phase(self):
-        """Computes phase value."""
+        """Signal phase in radiants."""
         phase = np.angle(self.i + 1.0j * self.q)
         return phase
-        # return signal.detrend(np.unwrap(phase))
 
     @cached_property
-    def ground_state_probability(self):
-        """Computes ground state probability"""
-        return 1 - np.mean(self.shots)
+    def state_0_probability(self):
+        return self.probability(0) if self.state else None
+            
+    @cached_property
+    def state_1_probability(self):
+        return self.probability(1) if self.state else None
 
-    def raw_probability(self, state=1):
-        """Serialize probabilities in dict.
-        Args:
-            state (int): if 0 stores the probabilities of finding
-                        the ground state. If 1 stores the
-                        probabilities of finding the excited state.
-        """
-        if state == 1:
-            return {"probability": 1 - self.ground_state_probability}
-        elif state == 0:
-            return {"probability": self.ground_state_probability}
+    def probability(self, state=0):
+        """Returns the statistical frequency of the specified state (0 or 1)."""
+        return np.count_nonzero(self.state == state)/len(self.state)
 
-    @property
-    def average(self):
-        """Perform average over i and q"""
-        return AveragedResults.from_components(np.mean(self.i), np.mean(self.q))
-
-    @property
-    def raw(self):
-        """Serialize output in dict."""
-
-        return {
-            "MSR[V]": self.measurement,
-            "i[V]": self.i,
-            "q[V]": self.q,
-            "phase[rad]": self.phase,
-        }
+    def __add__(self, data):# __add__(self, data:ExecutionResults) -> ExecutionResults
+        axis = 0 if len(data.i.shape) > 0 else None
+        i = np.append(self.i, data.i, axis=axis)
+        q = np.append(self.q, data.q, axis=axis)
+        if data.state:
+            state = np.append(self.state, data.state, axis=axis)
+        else:
+            state = None
+        return ExecutionResults(i, q, state)
 
     def __len__(self):
         return len(self.i)
 
+    def serialize(self):
+        """Serialize as a dictionary."""
+        serialized_dict = {
+            "magnitude[V]": self.magnitude,
+            "i[V]": self.i,
+            "q[V]": self.q,
+            "phase[rad]": self.phase,
+        }
+        if self.state:
+            serialized_dict["state"] = self.state_1_probability
+        return serialized_dict
+
+    @property
+    def average(self):
+        """Perform average over i and q"""
+        return AveragedResults(np.mean(self.i), np.mean(self.q))
+    
 
 class AveragedResults(ExecutionResults):
     """Data structure containing averages of ``ExecutionResults``."""
