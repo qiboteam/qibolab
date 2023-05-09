af666fb5e20ea8fd376db1ab9d39bfa8a7fdbf90
rodolfocarobene <rodolfo.carobene@gmail.com>
2023-05-09 16:30:37 +0400

    added todo

--- src/qibolab/instruments/rfsoc.py
+++ src/qibolab/instruments/rfsoc.py
@@ -495,7 +495,6 @@ class RFSoC(AbstractInstrument):
                                 sequence[sweeper.indexes[jdx]].amplitude = values[jdx][idx]
                     else:  # TODO elif
                         for kdx, jdx in enumerate(sweeper.indexes):
-                            print(f"setting {values[kdx][idx]} to qubit {jdx}")
                             qubits[jdx].flux.bias = values[kdx][idx]
 
                     res = self.recursive_python_sweep(qubits, sequence, original_ro, *sweepers[1:], average=average)
@@ -707,7 +706,7 @@ class TII_ZCU111(RFSoC):  # Containes the main settings:
         self.cfg = QickProgramConfig(
             sampling_rate=6_000_000_000,
             mixer_freq=0,
-            LO_freq=7_000_000_000,
+            LO_freq=7_000_000_000,  # TODO this is not needed anymore, info in channels
             LO_power=10.0,
             adc_sampling_frequency=6_000_000_000,  # 3_072_000_000,
         )
