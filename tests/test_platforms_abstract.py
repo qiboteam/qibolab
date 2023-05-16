9bcd0526cba4f0cfcc41f355cdc1f000a29eba7b
Edoardo-Pedicillo <edoardo.pedicillo@gmail.com>
2023-05-16 15:11:30 +0400

    add platform setup

--- tests/test_platforms_abstract.py
+++ tests/test_platforms_abstract.py
@@ -163,6 +163,7 @@ def test_add_measurement_to_sequence(platform_name):
 
 def test_update(platform_name):
     platform = Platform(platform_name)
+    platform.setup()
     readout_frequencies = np.ones(platform.nqubits)
     updates = {"t2": {platform.qubits[i].name: readout_frequencies[i] for i in range(platform.nqubits)}}
     platform.update(updates)
