8a9ee70032979ad459ec5a94627f44d0453fdbef
Jacfomg <juan.a.c.f.99@gmail.com>
2023-05-31 11:29:26 +0400

    fix flux

--- src/qibolab/instruments/zhinst.py
+++ src/qibolab/instruments/zhinst.py
@@ -699,7 +699,7 @@ class Zurich(AbstractInstrument):
                     if isinstance(pulse, ZhSweeperLine):
                         self.play_sweep(exp, qubit, pulse, section="flux")
                     elif isinstance(pulse, ZhSweeper):
-                        self.play_sweep(exp, qubit, pulse, section="drive")
+                        self.play_sweep(exp, qubit, pulse, section="flux")
                     elif isinstance(pulse, ZhPulse):
                         exp.play(signal=f"flux{q}", pulse=pulse.zhpulse)
                     i += 1
