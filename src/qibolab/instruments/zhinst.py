92fa2d0cce0d42442e6bdd84d3e658cc00b2bfbc
Jacfomg <juan.a.c.f.99@gmail.com>
2023-11-01 16:38:00 +0400

    bias sweep issue

--- src/qibolab/instruments/zhinst.py
+++ src/qibolab/instruments/zhinst.py
@@ -232,8 +232,8 @@ class ZhSweeperLine:
         if sweeper.parameter is Parameter.bias:
             if isinstance(qubit, Qubit):
                 pulse = FluxPulse(
-                    start=sequence.start,
-                    duration=sequence.duration,
+                    start=0,
+                    duration=sequence.duration+sequence.start,
                     amplitude=1,
                     shape="Rectangular",
                     channel=qubit.flux.name,
@@ -242,9 +242,11 @@ class ZhSweeperLine:
                 self.signal = f"flux{qubit.name}"
             if isinstance(qubit, Coupler):
                 # TODO: For the couplers specs is enough, may need changing for 2q coupler routines ?
+                
+                print(sequence.duration+sequence.start)
                 pulse = CouplerFluxPulse(
-                    start=sequence.start,
-                    duration=sequence.duration,
+                    start=0,
+                    duration=sequence.duration+sequence.start,
                     amplitude=1,
                     shape="Rectangular",
                     channel=qubit.flux.name,
@@ -948,6 +950,17 @@ class Zurich(Controller):
                 for pulse, q, iq_angle in zip(pulses, qubits, iq_angles):
                     pulse.zhpulse.uid += str(i)
 
+                    if play_after is None:
+                        exp.delay(
+                                    signal=f"measure{q}",
+                                    time=self.sequence_qibo.start * NANO_TO_SECONDS,
+                                )
+                        exp.delay(
+                                    signal=f"acquire{q}",
+                                    time=self.sequence_qibo.start * NANO_TO_SECONDS,
+                                )
+                
+                    
                     # Integration weights definition or load from the chip folder
                     weights_file = (
                         INSTRUMENTS_DATA_FOLDER / f"{self.chip}/weights/integration_weights_optimization_qubit_{q}.npy"
