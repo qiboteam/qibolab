21fe7aca24d72a0640438a6289fa963b1045a7f5
Alessandro Candido <candido.ale@gmail.com>
2023-12-12 20:37:07 +0100

    Add Nix files

---
+++ flake.nix
@@ -0,0 +1,58 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
+    devenv.url = "github:cachix/devenv";
+    nixpkgs-python.url = "github:cachix/nixpkgs-python";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    devenv,
+    systems,
+    ...
+  } @ inputs: let
+    forEachSystem = nixpkgs.lib.genAttrs (import systems);
+  in {
+    packages = forEachSystem (system: {
+      default =
+        nixpkgs.legacyPackages.${system}.poetry2nix.mkPoetryApplication
+        {
+          projectDir = self;
+          preferWheels = true;
+        };
+    });
+
+    devShells =
+      forEachSystem
+      (system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        default = devenv.lib.mkShell {
+          inherit inputs pkgs;
+          modules = [
+            {
+              packages = with pkgs; [pre-commit poethepoet];
+
+              languages.python = {
+                enable = true;
+                poetry = {
+                  enable = true;
+                  install.enable = true;
+                  install.groups = ["tests"];
+                  install.allExtras = true;
+                };
+                version = "3.11";
+              };
+            }
+          ];
+        };
+      });
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+}
