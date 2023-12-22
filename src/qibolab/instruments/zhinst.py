b66e1b1a961dc09d80d9f8373b6f35b31ec8dde9
GabrielePalazzo <73099233+GabrielePalazzo@users.noreply.github.com>
2023-12-22 13:11:59 +0400

    Fixed nt sweep on bias

--- src/qibolab/instruments/zhinst.py
+++ src/qibolab/instruments/zhinst.py
@@ -1178,15 +1178,16 @@ class Zurich(Controller):
         rearranging_axes = np.zeros(2, dtype=int)
         if len(sweepers) == 2:
             if sweepers[1].parameter is Parameter.frequency:
-                if (sweepers[0].parameter is Parameter.bias) or (
-                    not sweepers[0].parameter is Parameter.amplitude
-                    and sweepers[0].pulses[0].type is not PulseType.READOUT
-                ):
-                    rearranging_axes[:] = [sweepers.index(sweepers[1]), 0]
-                    sweeper_changed = sweepers[1]
-                    sweepers.remove(sweeper_changed)
-                    sweepers.insert(0, sweeper_changed)
-                    log.warning("Sweepers were reordered")
+                if not sweepers[0].pulses is None:
+                    if (sweepers[0].parameter is Parameter.bias) or (
+                        not sweepers[0].parameter is Parameter.amplitude
+                        and sweepers[0].pulses[0].type is not PulseType.READOUT
+                    ):
+                        rearranging_axes[:] = [sweepers.index(sweepers[1]), 0]
+                        sweeper_changed = sweepers[1]
+                        sweepers.remove(sweeper_changed)
+                        sweepers.insert(0, sweeper_changed)
+                        log.warning("Sweepers were reordered")
         return rearranging_axes, sweepers
 
     def offsets_off(self):
@@ -1332,7 +1333,9 @@ class Zurich(Controller):
                     ).zhsweeper
                     zhsweeper.uid = "bias"  # f"bias{i}"
                     path = "DEV8660"
-                    parameter = zhsweeper
+                    from copy import deepcopy  # if really needed, it needs to be moved on top of the module
+                    parameter = deepcopy(zhsweeper)
+                    parameter.values += qubit.flux.offset
                     device_path = f"{path}/sigouts/0/offset"
 
         elif sweeper.parameter is Parameter.amplitude:
