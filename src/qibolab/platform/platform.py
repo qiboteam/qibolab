17c05fa23bc56010284716bdab614ff854d42c39
Alessandro Candido <candido.ale@gmail.com>
2024-01-18 14:29:54 +0100

    Fix backends test, remove explicit copy methods
    To copy, both shallow and deep, just use the dedicated standard library module

--- src/qibolab/platform/platform.py
+++ src/qibolab/platform/platform.py
@@ -1,5 +1,5 @@
 """A platform for executing quantum algorithms."""
-
+import copy
 from collections import defaultdict
 from dataclasses import dataclass, field, fields
 from typing import Dict, List, Optional, Tuple
