d9bd2a033f215951116e1449afaed1d213bd874e
Alessandro Candido <candido.ale@gmail.com>
2024-01-18 18:18:40 +0100

    Start rearranging pulses into a subpackage

--- src/qibolab/pulses/sequence.py
+++ src/qibolab/pulses/sequence.py
@@ -1,8 +1,5 @@
 """PulseSequence class."""
-
-from collections import defaultdict
-
-from .pulse import PulseType
+import numpy as np
 
 
 class PulseSequence(list):
@@ -94,21 +91,27 @@ class PulseSequence(list):
         return new_pc
 
     @property
-    def pulses_per_channel(self):
-        """Return a dictionary with the sequence per channel."""
-        sequences = defaultdict(type(self))
+    def finish(self) -> int:
+        """The time when the last pulse of the sequence finishes."""
+        t: int = 0
         for pulse in self:
-            sequences[pulse.channel].append(pulse)
-        return sequences
+            if pulse.finish > t:
+                t = pulse.finish
+        return t
+
+    @property
+    def start(self) -> int:
+        """The start time of the first pulse of the sequence."""
+        t = self.finish
+        for pulse in self:
+            if pulse.start < t:
+                t = pulse.start
+        return t
 
     @property
     def duration(self) -> int:
-        """The time when the last pulse of the sequence finishes."""
-        channel_pulses = self.pulses_per_channel
-        if len(channel_pulses) == 1:
-            pulses = next(iter(channel_pulses.values()))
-            return sum(pulse.duration for pulse in pulses)
-        return max(sequence.duration for sequence in channel_pulses.values())
+        """Duration of the sequence calculated as its finish - start times."""
+        return self.finish - self.start
 
     @property
     def channels(self) -> list:
@@ -129,6 +132,25 @@ class PulseSequence(list):
                 qubits.append(pulse.qubit)
         qubits.sort()
         return qubits
+
+    def get_pulse_overlaps(self):  # -> dict((int,int): PulseSequence):
+        """Return a dictionary of slices of time (tuples with start and finish
+        times) where pulses overlap."""
+        times = []
+        for pulse in self:
+            if not pulse.start in times:
+                times.append(pulse.start)
+            if not pulse.finish in times:
+                times.append(pulse.finish)
+        times.sort()
+
+        overlaps = {}
+        for n in range(len(times) - 1):
+            overlaps[(times[n], times[n + 1])] = PulseSequence()
+            for pulse in self:
+                if (pulse.start <= times[n]) & (pulse.finish >= times[n + 1]):
+                    overlaps[(times[n], times[n + 1])] += [pulse]
+        return overlaps
 
     def separate_overlapping_pulses(self):  # -> dict((int,int): PulseSequence):
         """Separate a sequence of overlapping pulses into a list of non-
@@ -155,3 +177,84 @@ class PulseSequence(list):
             if not stored:
                 separated_pulses.append(PulseSequence([new_pulse]))
         return separated_pulses
+
+    # TODO: Implement separate_different_frequency_pulses()
+
+    @property
+    def pulses_overlap(self) -> bool:
+        """Whether any of the pulses in the sequence overlap."""
+        overlap = False
+        for pc in self.get_pulse_overlaps().values():
+            if len(pc) > 1:
+                overlap = True
+                break
+        return overlap
+
+    def plot(self, savefig_filename=None, sampling_rate=SAMPLING_RATE):
+        """Plot the sequence of pulses.
+
+        Args:
+            savefig_filename (str): a file path. If provided the plot is save to a file.
+        """
+        if len(self) > 0:
+            import matplotlib.pyplot as plt
+            from matplotlib import gridspec
+
+            fig = plt.figure(figsize=(14, 2 * len(self)), dpi=200)
+            gs = gridspec.GridSpec(ncols=1, nrows=len(self))
+            vertical_lines = []
+            for pulse in self:
+                vertical_lines.append(pulse.start)
+                vertical_lines.append(pulse.finish)
+
+            n = -1
+            for qubit in self.qubits:
+                qubit_pulses = self.get_qubit_pulses(qubit)
+                for channel in qubit_pulses.channels:
+                    n += 1
+                    channel_pulses = qubit_pulses.get_channel_pulses(channel)
+                    ax = plt.subplot(gs[n])
+                    ax.axis([0, self.finish, -1, 1])
+                    for pulse in channel_pulses:
+                        num_samples = len(
+                            pulse.shape.modulated_waveform_i(sampling_rate)
+                        )
+                        time = pulse.start + np.arange(num_samples) / sampling_rate
+                        ax.plot(
+                            time,
+                            pulse.shape.modulated_waveform_q(sampling_rate).data,
+                            c="lightgrey",
+                        )
+                        ax.plot(
+                            time,
+                            pulse.shape.modulated_waveform_i(sampling_rate).data,
+                            c=f"C{str(n)}",
+                        )
+                        ax.plot(
+                            time,
+                            pulse.shape.envelope_waveform_i(sampling_rate).data,
+                            c=f"C{str(n)}",
+                        )
+                        ax.plot(
+                            time,
+                            -pulse.shape.envelope_waveform_i(sampling_rate).data,
+                            c=f"C{str(n)}",
+                        )
+                        # TODO: if they overlap use different shades
+                        ax.axhline(0, c="dimgrey")
+                        ax.set_ylabel(f"qubit {qubit} \n channel {channel}")
+                        for vl in vertical_lines:
+                            ax.axvline(vl, c="slategrey", linestyle="--")
+                        ax.axis([0, self.finish, -1, 1])
+                        ax.grid(
+                            visible=True,
+                            which="both",
+                            axis="both",
+                            color="#CCCCCC",
+                            linestyle="-",
+                        )
+            if savefig_filename:
+                plt.savefig(savefig_filename)
+            else:
+                plt.show()
+            plt.close()
