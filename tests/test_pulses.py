d1baf5c2e6e161fe2e49e6567ea8b69858ffad6f
Alessandro Candido <candido.ale@gmail.com>
2024-01-18 17:50:55 +0100

    Pass explicitly the if to modulated waveform tests

--- tests/test_pulses.py
+++ tests/test_pulses.py
@@ -638,10 +638,10 @@ def test_pulseshape_rectangular():
     np.testing.assert_allclose(pulse.shape.envelope_waveform_i(sampling_rate).data, i)
     np.testing.assert_allclose(pulse.shape.envelope_waveform_q(sampling_rate).data, q)
     np.testing.assert_allclose(
-        pulse.shape.modulated_waveform_i(sampling_rate).data, mod_i
+        pulse.shape.modulated_waveform_i(_if, sampling_rate).data, mod_i
     )
     np.testing.assert_allclose(
-        pulse.shape.modulated_waveform_q(sampling_rate).data, mod_q
+        pulse.shape.modulated_waveform_q(_if, sampling_rate).data, mod_q
     )
 
 
@@ -689,10 +689,10 @@ def test_pulseshape_gaussian():
     np.testing.assert_allclose(pulse.shape.envelope_waveform_i(sampling_rate).data, i)
     np.testing.assert_allclose(pulse.shape.envelope_waveform_q(sampling_rate).data, q)
     np.testing.assert_allclose(
-        pulse.shape.modulated_waveform_i(sampling_rate).data, mod_i
+        pulse.shape.modulated_waveform_i(_if, sampling_rate).data, mod_i
     )
     np.testing.assert_allclose(
-        pulse.shape.modulated_waveform_q(sampling_rate).data, mod_q
+        pulse.shape.modulated_waveform_q(_if, sampling_rate).data, mod_q
     )
 
 
@@ -746,10 +746,10 @@ def test_pulseshape_drag():
     np.testing.assert_allclose(pulse.shape.envelope_waveform_i(sampling_rate).data, i)
     np.testing.assert_allclose(pulse.shape.envelope_waveform_q(sampling_rate).data, q)
     np.testing.assert_allclose(
-        pulse.shape.modulated_waveform_i(sampling_rate).data, mod_i
+        pulse.shape.modulated_waveform_i(_if, sampling_rate).data, mod_i
     )
     np.testing.assert_allclose(
-        pulse.shape.modulated_waveform_q(sampling_rate).data, mod_q
+        pulse.shape.modulated_waveform_q(_if, sampling_rate).data, mod_q
     )
 
 
