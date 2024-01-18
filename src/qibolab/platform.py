407a66f6651a40e3dc467b1d8c45c8259a256269
Alessandro Candido <candido.ale@gmail.com>
2024-01-18 14:17:45 +0100

    Replace usage of pulse subclasses (import related)
    Minimal replacement, such that pytest can at least report

--- src/qibolab/platform.py
+++ src/qibolab/platform.py
@@ -10,7 +10,7 @@ from qibo.config import log, raise_error
 from qibolab.couplers import Coupler
 from qibolab.execution_parameters import ExecutionParameters
 from qibolab.instruments.abstract import Controller, Instrument, InstrumentId
-from qibolab.pulses import PulseSequence, ReadoutPulse
+from qibolab.pulses import PulseSequence, PulseType
 from qibolab.qubits import Qubit, QubitId, QubitPair, QubitPairId
 from qibolab.sweeper import Sweeper
 
@@ -47,7 +47,7 @@ def unroll_sequences(
             new_pulse = pulse.copy()
             new_pulse.start += start
             total_sequence.append(new_pulse)
-            if isinstance(pulse, ReadoutPulse):
+            if pulse.type is PulseType.READOUT:
                 readout_map[pulse.id].append(new_pulse.id)
         start = total_sequence.finish + relaxation_time
     return total_sequence, readout_map
