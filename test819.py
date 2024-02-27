430f0fbda3a39a9fe6c2378a92487949956eb4f8
Alessandro Candido <candido.ale@gmail.com>
2024-02-27 19:42:56 +0100

    test: Obtain qasm programs from the QRM and QCM_BB modules

--- test819.py
+++ test819.py
@@ -7,24 +7,25 @@ from qibolab.execution_parameters import (
     AveragingMode,
     ExecutionParameters,
 )
+from qibolab.instruments.qblox.controller import QbloxController
+from qibolab.platform import NS_TO_SEC
 from qibolab.pulses import PulseSequence
 from qibolab.sweeper import Parameter, Sweeper, SweeperType
 
 GlobalBackend.set_backend("qibolab", "spinq10q")
 platform: Platform = GlobalBackend().platform
+controller: QbloxController = platform.instruments["qblox_controller"]
 
 sequence = PulseSequence()
 ro_pulses = {}
-qubit_frequency = {}
-bare_resonator_frequency = {}
 
 qid = 0
 qubit = platform.qubits[qid]
-qubit_frequency[qid] = qubit.drive_frequency
-bare_resonator_frequency[qid] = qubit.bare_resonator_frequency
+qubits = {qid: qubit}
 
-ro_pulses[qid] = platform.create_qubit_readout_pulse(qid, start=0)
-sequence.add(ro_pulses[qid])
+ro_pulse = platform.create_qubit_readout_pulse(qid, start=0)
+ro_pulse.frequency = int(2e9)
+sequence.add(ro_pulse)
 
 freq_width = 2e7
 freq_step = 2e5
@@ -33,19 +34,19 @@ bias_step = 0.01
 nshots = 1024
 relaxation_time = 5000
 
+navgs = nshots
+repetition_duration = sequence.finish + relaxation_time
+
 # define the parameters to sweep and their range:
 delta_frequency_range = np.arange(-freq_width // 2, freq_width // 2, freq_step)
 freq_sweeper = Sweeper(
-    Parameter.frequency,
-    delta_frequency_range,
-    [ro_pulses[qid]],
-    type=SweeperType.OFFSET,
+    Parameter.frequency, delta_frequency_range, [ro_pulse], type=SweeperType.OFFSET
 )
 
 delta_bias_range = np.arange(-bias_width / 2, bias_width / 2, bias_step)
-bias_sweepers = [
-    Sweeper(Parameter.bias, delta_bias_range, qubits=[qubit], type=SweeperType.OFFSET)
-]
+bias_sweeper = Sweeper(
+    Parameter.bias, delta_bias_range, qubits=[qubit], type=SweeperType.OFFSET
+)
 
 options = ExecutionParameters(
     nshots=nshots,
@@ -53,6 +54,38 @@ options = ExecutionParameters(
     acquisition_type=AcquisitionType.INTEGRATION,
     averaging_mode=AveragingMode.CYCLIC,
 )
-for bias_sweeper in bias_sweepers:
-    __import__("pdb").set_trace()
-    results = platform.sweep(sequence, options, bias_sweeper, freq_sweeper)
+time = (sequence.duration + relaxation_time) * nshots * NS_TO_SEC
+sweepers = (bias_sweeper, freq_sweeper)
+for sweep in sweepers:
+    time *= len(sweep.values)
+
+# mock
+controller.is_connected = True
+
+
+class Sequencers:
+    def __getitem__(self, index):
+        return self
+
+    def set(self, *args):
+        pass
+
+
+class Device:
+    sequencers = Sequencers()
+
+
+for mod in controller.modules.values():
+    mod.device = Device()
+    mod._device_num_sequencers = 0
+# end mock
+
+for name, mod in controller.modules.items():
+    if "qcm_rf" in name:
+        continue
+    channels = controller._set_module_channel_map(mod, qubits)
+    pulses = sequence.get_channel_pulses(*channels)
+    mod.process_pulse_sequence(
+        qubits, pulses, navgs, nshots, repetition_duration, sweepers
+    )
+    print(mod._sequencers["o1"][0].program)
