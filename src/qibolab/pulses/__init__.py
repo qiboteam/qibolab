1e6bce5a4cb8323141ad62d8990105023980feef
Alessandro Candido <candido.ale@gmail.com>
2024-02-28 20:10:24 +0100

    feat!: Export only shapes container

--- src/qibolab/pulses/__init__.py
+++ src/qibolab/pulses/__init__.py
@@ -1,16 +1,3 @@
 from .pulse import Delay, Pulse, PulseType, VirtualZ
 from .sequence import PulseSequence
-from .shape import (
-    IIR,
-    SAMPLING_RATE,
-    SNZ,
-    Custom,
-    Drag,
-    Gaussian,
-    GaussianSquare,
-    PulseShape,
-    Rectangular,
-    ShapeInitError,
-    Waveform,
-    eCap,
-)
+from .shape import *
