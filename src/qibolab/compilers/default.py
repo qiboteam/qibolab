9fc21b29d749423b47ca2020243da0522ab1dc63
Stavros Efthymiou <35475381+stavros11@users.noreply.github.com>
2024-03-04 19:16:45 +0400

    fix: compiler and tests

--- src/qibolab/compilers/default.py
+++ src/qibolab/compilers/default.py
@@ -4,6 +4,7 @@ Uses I, Z, RZ, U3, CZ, and M as the set of native gates.
 """
 
 import math
+from dataclasses import replace
 
 from qibolab.pulses import PulseSequence, VirtualZ
 from qibolab.serialize_ import replace
