f5888a3de8bf671c4a046ea7f2627a9b3bb4cccc
Gabriele <73099233+GabrielePalazzo@users.noreply.github.com>
2024-03-14 16:04:28 +0100

    refactor: yield-ing temperature values + removed threading

--- src/qibolab/instruments/bluefors.py
+++ src/qibolab/instruments/bluefors.py
@@ -1,70 +1,49 @@
-import re
+# import re
 import socket
-import threading
-from datetime import datetime
 
 from qibo.config import log
 
+# from datetime import datetime
+
 
 class TemperatureController:
-    def __init__(self, IP_address):
-        self.IP_address = IP_address
-        self._thread = threading.Thread(target=self.get_data)
-        self._thread.daemon = (
-            True  # Daemonize the thread so it terminates when the main program exits
-        )
-        self._is_running = False
-        self._property = None
-        self._thread.start()
+    """Bluefors temperature controller."""
 
-    def get_data(self):
+    def __init__(self, ip_address: str, port: int = 8888):
+        """Creation of the controller object.
 
-        # Create a TCP/IP socket
+        Args:
+            ip_address (str): IP address of the board sending cryo temperature data.
+            port (int): port of the board sending cryo temperature data.
+        """
+        self.ip_address = ip_address
+        self.port = port
+
+    def get_data(self) -> str:
+        """Connect to the socket and get temperature data.
+
+        Returns:
+            message (str): socket message in this format:
+                flange_name: {'temperature': <value(float)>, 'timestamp':<value(float)>}
+        """
         client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-        # Connect the socket to the server
-        client_socket.connect((self.IP_address, 8888))
+        client_socket.connect((self.ip_address, self.port))
         log.info("Bluefors Temperature Controller Connected")
 
-        self.data = {}
+        # This message is a string that looks like a json
+        # Shoud we convert it into a dictionary?
+        message = client_socket.recv(1024).decode()
+        return message
+
+    def read_data(self):
+        """Continously read data from the temperature controller."""
         while True:
-            try:
-                # Receive data from the server
-                message = client_socket.recv(1024).decode()
-                # print(message)
-
-                # Define regular expression pattern to match the identifier, parameters, and values
-                pattern = r"([\w-]+): {'(\w+)':([\d\.]+), '(\w+)':([\d\.]+)}"
-
-                # Find all matches using regex
-                matches = re.findall(pattern, message)
-
-                # Extract identifier, parameters, and values
-                flange, _, temperature, _, timestamp = matches[0]
-
-                # Convert timestamp to datetime object
-                date_time = datetime.fromtimestamp(float(timestamp))
-
-                # Format datetime object as a string
-                formatted_date = date_time.strftime("%Y-%m-%d %H:%M:%S")
-
-                # Create a dictionary with the extracted information
-                self.data[flange] = {
-                    "temperature": float(temperature),
-                    "timestamp": float(timestamp),
-                    "datetime": formatted_date,
-                }
-                # print(data)
-            except:
-                pass
+            yield self.get_data()
 
 
 # Example usage
 if __name__ == "__main__":
-    import time
-
-    tc = TemperatureController("192.168.0.114")
-    while True:
-        time.sleep(10)
-        print("latest data")
-        print(tc.data)
+    tc = TemperatureController("192.168.0.114", 8888)
+    temperature_values = tc.read_data()
+    for temperature_value in temperature_values:
+        print(temperature_value)
