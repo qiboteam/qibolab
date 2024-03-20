fix amplitude scaling if negative values are present

src/qibolab/instruments/zhinst/executor.py
@@ -652,8 +652,9 @@ class Zurich(Controller):
         play_parameters = {}
         for p, zhs in pulse.zhsweepers:
             if p is Parameter.amplitude:
-                pulse.zhpulse.amplitude *= max(zhs.values)
-                zhs.values /= max(zhs.values)
+                max_value = max(np.abs(zhs.values))
+                pulse.zhpulse.amplitude *= max_value
+                zhs.values /= max_value
                 play_parameters["amplitude"] = zhs
             if p is Parameter.duration:
                 play_parameters["length"] = zhs

src/qibolab/instruments/zhinst/sweep.py
@@ -4,6 +4,7 @@ from collections.abc import Iterable
 from copy import copy
 
 import laboneq.simple as lo
+import numpy as np
 
 from qibolab.pulses import Pulse, PulseType
 from qibolab.qubits import Qubit
@@ -92,13 +93,12 @@ class ProcessedSweeps:
                     pulse.type is PulseType.READOUT
                     and sweeper.parameter is Parameter.amplitude
                 ):
-                    sweep_param = lo.SweepParameter(
-                        values=sweeper.values / max(sweeper.values)
-                    )
+                    max_value = max(np.abs(sweeper.values))
+                    sweep_param = lo.SweepParameter(values=sweeper.values / max_value)
                     # FIXME: this implicitly relies on the fact that pulse is the same python object as appears in the
                     # sequence that is being executed, hence the mutation is propagated. This is bad programming and
                     # should be fixed once things become simpler
-                    pulse.amplitude *= max(sweeper.values)
+                    pulse.amplitude *= max_value
 
                     channel_sweeps.append(
                         (