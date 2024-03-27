5496e47015d4acef42ed5a458fef99b5427b11ef
Alessandro Candido <candido.ale@gmail.com>
2024-03-27 18:09:47 +0400

    test: Fix drag tests

--- src/qibolab/pulses/envelope.py
+++ src/qibolab/pulses/envelope.py
@@ -187,7 +187,7 @@ class Drag(BaseEnvelope):
         .. todo::
         """
         ts = np.arange(samples)
-        mu = samples / 2
+        mu = (samples - 1) / 2
         sigma = _samples_sigma(self.rel_sigma, samples)
         return self.beta * (-(ts - mu) / (sigma**2)) * self.i(samples)
 
