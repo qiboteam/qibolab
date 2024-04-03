8f9ee4065a9e1c0ae4f9ad9107c43adc6079ce30
Jun Yong Khoo <jyogaurora@gmail.com>
2024-04-03 23:41:04 +0800

    update dec_to_basis_string

--- src/qibolab/instruments/emulator/backends/qutip_backend.py
+++ src/qibolab/instruments/emulator/backends/qutip_backend.py
@@ -445,9 +445,7 @@ class Qutip_Simulator:
 
             full_HS_dim = np.product(self.nlevels_HS)
             for state_id in range(full_HS_dim):
-                basis_string = dec_to_basis_string(
-                    state_id, self.nlevels_HS  # , to_string=False
-                )
+                basis_string = dec_to_basis_string(state_id, self.nlevels_HS)
                 labels.append(basis_string)
                 basis_state = self.state_from_basis_vector(
                     basis_string[: self.nqubits], basis_string[self.nqubits :]
