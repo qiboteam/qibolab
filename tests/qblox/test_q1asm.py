1d927fe8e332aefcd5fc5ba5ca80b1c2d91eb5c7
Alessandro Candido <candido.ale@gmail.com>
2024-04-04 22:41:25 +0200

    feat: Add program structure

--- tests/qblox/test_q1asm.py
+++ tests/qblox/test_q1asm.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+from qibolab.instruments.neoqblox.ast_ import Program
 from qibolab.instruments.neoqblox.parse import parse
 
 PROGRAMS = Path(__file__).parent / "q1asm"
@@ -7,4 +8,5 @@ PROGRAMS = Path(__file__).parent / "q1asm"
 
 def test_q1asm():
     for program in PROGRAMS.glob("*.q1asm"):
-        parse(program.read_text())
+        prog = parse(program.read_text())
+        assert isinstance(prog, Program)
