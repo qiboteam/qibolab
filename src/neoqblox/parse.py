ec2c63f2d90f6b56ea64aa8d9d3dc497d7b221c0
Alessandro Candido <candido.ale@gmail.com>
2024-04-04 19:43:52 +0200

    feat: Generate a suitable AST for Q1ASM

--- src/neoqblox/parse.py
+++ src/neoqblox/parse.py
@@ -1,6 +1,9 @@
 from pathlib import Path
 
+import rich
 from lark import Lark
+
+from .ast_ import ToAst
 
 GRAMMAR_FILE = Path(__file__).parent / "q1asm.lark"
 GRAMMAR = GRAMMAR_FILE.read_text()
@@ -9,13 +12,7 @@ GRAMMAR = GRAMMAR_FILE.read_text()
 parser = Lark(GRAMMAR)
 
 
-def test(code):
+def parse(code):
     tree = parser.parse(code)
     print(tree.pretty())
-
-
-if __name__ == "__main__":
-    import sys
-
-    for arg in sys.argv[1:]:
-        test(Path(arg).read_text())
+    rich.print(ToAst().transform(tree).children)
