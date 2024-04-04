6cd600b4bfd30598fcbf01b864dfe12a63f61241
Alessandro Candido <candido.ale@gmail.com>
2024-04-04 13:37:42 +0200

    build: Restore flake to main

--- flake.nix
+++ flake.nix
@@ -47,9 +47,7 @@
             }: {
               packages = with pkgs; [pre-commit poethepoet jupyter];
 
-              env = {
-                QIBOLAB_PLATFORMS = (dirOf config.env.DEVENV_ROOT) + "/qibolab_platforms_qrc";
-              };
+              env.QIBOLAB_PLATFORMS = (dirOf config.env.DEVENV_ROOT) + "/qibolab_platforms_qrc";
 
               languages.python = {
                 enable = true;
