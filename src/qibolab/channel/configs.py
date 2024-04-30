33ad2adf0280bfc8af171205a8f1530794540665
Hayk Sargsyan <52532457+hay-k@users.noreply.github.com>
2024-04-30 14:25:44 +0400

    implement appending sequence to a sequnece
    fix docstring
    
    fix: Solve circular import in the channel subpackage
    
    fix: Make import relative
    
    test: Turn import time error in test time error
    
    fix: Drop ports from controller

--- src/qibolab/channel/configs.py
+++ src/qibolab/channel/configs.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Optional
+from typing import Optional, Union
 
 from qibolab.execution_parameters import AcquisitionType
 
@@ -11,6 +11,7 @@ __all__ = [
     "AcquisitionChannelConfig",
     "OscillatorConfig",
     "IQMixerConfig",
+    "ChannelConfig",
 ]
 
 
@@ -81,3 +82,6 @@ class AcquisitionChannelConfig:
 
     None if the channel does not feature a TWPA.
     """
+
+
+ChannelConfig = Union[DCChannelConfig, IQChannelConfig, AcquisitionChannelConfig]
