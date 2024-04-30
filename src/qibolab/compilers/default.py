9e366094b3311340791c68add78e46311d059ab3
Hayk Sargsyan <52532457+hay-k@users.noreply.github.com>
2024-04-30 13:05:31 +0400

    remove channel and qubit properties from pulse

--- src/qibolab/compilers/default.py
+++ src/qibolab/compilers/default.py
@@ -17,16 +17,24 @@ def identity_rule(gate, qubit):
 
 def z_rule(gate, qubit):
     """Z gate applied virtually."""
+<<<<<<< HEAD
     seq = PulseSequence()
     seq[qubit.drive.name].append(VirtualZ(phase=math.pi))
     return seq
+=======
+    return PulseSequence([VirtualZ(phase=math.pi)])
+>>>>>>> c5ef5a19 (remove channel and qubit properties from pulse)
 
 
 def rz_rule(gate, qubit):
     """RZ gate applied virtually."""
+<<<<<<< HEAD
     seq = PulseSequence()
     seq[qubit.drive.name].append(VirtualZ(phase=gate.parameters[0]))
     return seq
+=======
+    return PulseSequence([VirtualZ(phase=gate.parameters[0])])
+>>>>>>> c5ef5a19 (remove channel and qubit properties from pulse)
 
 
 def gpi2_rule(gate, qubit):
@@ -42,7 +50,32 @@ def gpi_rule(gate, qubit):
     # to the matrix representation. See
     # https://github.com/qiboteam/qibolab/pull/804#pullrequestreview-1890205509
     # for more detail.
+<<<<<<< HEAD
     return qubit.native_gates.RX.create_sequence(theta=np.pi, phi=gate.parameters[0])
+=======
+    pulse = replace(qubit.native_gates.RX, relative_phase=theta)
+    sequence = PulseSequence([pulse])
+    return sequence
+
+
+def u3_rule(gate, qubit):
+    """U3 applied as RZ-RX90-RZ-RX90-RZ."""
+    # Transform gate to U3 and add pi/2-pulses
+    theta, phi, lam = gate.parameters
+    # apply RZ(lam)
+    virtual_z_phases = {qubit.name: lam}
+    sequence = PulseSequence()
+    sequence.append(VirtualZ(phase=lam))
+    # Fetch pi/2 pulse from calibration and apply RX(pi/2)
+    sequence.append(qubit.native_gates.RX90)
+    # apply RZ(theta)
+    sequence.append(VirtualZ(phase=theta))
+    # Fetch pi/2 pulse from calibration and apply RX(-pi/2)
+    sequence.append(replace(qubit.native_gates.RX90, relative_phase=-math.pi))
+    # apply RZ(phi)
+    sequence.append(VirtualZ(phase=phi))
+    return sequence
+>>>>>>> c5ef5a19 (remove channel and qubit properties from pulse)
 
 
 def cz_rule(gate, pair):
