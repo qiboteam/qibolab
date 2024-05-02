4cfad5e7bc79ee5c00d3a9f592887861110d0ce8
Stavros Efthymiou <35475381+stavros11@users.noreply.github.com>
2024-05-02 23:19:55 +0400

    chore: remove FluxPulse object and add frequency: 0 to flux pulses in runcards

--- src/qibolab/serialize.py
+++ src/qibolab/serialize.py
@@ -182,8 +182,6 @@ def load_instrument_settings(
 
 def _dump_pulse(pulse: Pulse):
     data = pulse.model_dump()
-    if pulse.type in (PulseType.FLUX, PulseType.COUPLERFLUX):
-        del data["frequency"]
     data["type"] = data["type"].value
     if "channel" in data:
         del data["channel"]
