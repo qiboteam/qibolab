635badcb7e7a76040baa2aea3944a8e0adc8297f
Hayk Sargsyan <52532457+hay-k@users.noreply.github.com>
2024-05-14 16:42:19 +0400

    introduce small mixin classes for channel definitions

--- src/qibolab/channel/__init__.py
+++ src/qibolab/channel/__init__.py
@@ -23,6 +23,41 @@ class Channel:
     config: ChannelConfig
 
 
+@dataclass(frozen=True)
+class WithExternalLo:
+    """Mixin class to be used for instrument specific IQ channel definitions,
+    in case the instrument does not have internal up-conversion unit and relies
+    on an external local oscillator (LO)."""
+
+    lo: str
+    """The name of the external local oscillator instrument."""
+
+
+@dataclass(frozen=True)
+class WithExternalTwpaPump:
+    """Mixin class to be used for instrument specific acquisition channel
+    definitions, in case the instrument does not have built-in oscillator
+    dedicated as TWPA pump and an external TWPA pump should be used."""
+
+    twpa_pump: str
+    """The name of the oscillator instrument used as TWPA pump."""
+
+
+def external_config(channel: Channel) -> dict[str, str]:
+    """Identifies which parts of the configuration of given channel should be
+    used to configure and externally connected supplementary instrument.
+
+    Returns:
+        Dictionary mapping config attribute to instrument name.
+    """
+
+    if isinstance(channel, WithExternalLo):
+        return {"lo": channel.lo}
+    if isinstance(channel, WithExternalTwpaPump):
+        return {"twpa_pump": channel.twpa_pump}
+    return {}
+
+
 @dataclass
 class ChannelMap:
     """Collection of :class:`Channel` objects identified by name.
--- src/qibolab/instruments/zhinst/channel/__init__.py
+++ src/qibolab/instruments/zhinst/channel/__init__.py
@@ -1,19 +1,19 @@
 from dataclasses import dataclass
 
-from qibolab.channel import Channel
-from qibolab.instruments.oscillator import LocalOscillator
+from qibolab.channel import Channel, WithExternalTwpaPump
 
 from .configs import *
 
 
 @dataclass(frozen=True)
 class ZIChannel(Channel):
+    """Channel for Zurich Instruments (ZI) devices."""
 
     device: str
+    """Name of the device."""
     path: str
+    """Path of the device node."""
 
 
 @dataclass(frozen=True)
-class ZIAcquisitionChannel(ZIChannel):
-
-    twpa_pump: LocalOscillator
+class ZiAcquisitionChannel(WithExternalTwpaPump, ZIChannel): ...
