29a1f72795f10ea6ec4936a993d8e9f5b0d31312
Hayk Sargsyan <52532457+hay-k@users.noreply.github.com>
2024-05-15 13:08:57 +0400

    get rid of ZhPulse

--- src/qibolab/instruments/zhinst/pulse.py
+++ src/qibolab/instruments/zhinst/pulse.py
@@ -1,7 +1,5 @@
 """Wrapper for qibolab and laboneq pulses and sweeps."""
 
-from typing import Optional
-
 import laboneq.simple as laboneq
 import numpy as np
 from laboneq.dsl.experiment.pulse_library import (
@@ -10,7 +8,6 @@ from laboneq.dsl.experiment.pulse_library import (
 )
 
 from qibolab.pulses import Drag, Gaussian, GaussianSquare, Pulse, PulseType, Rectangular
-from qibolab.sweeper import Parameter
 
 from .util import NANO_TO_SECONDS, SAMPLING_RATE
 
@@ -67,40 +64,3 @@ def select_pulse(pulse: Pulse):
             samples=pulse.i(SAMPLING_RATE) + (1j * pulse.q(SAMPLING_RATE)),
             can_compress=True,
         )
-
-
-class ZhPulse:
-    """Wrapper data type that holds a qibolab pulse, the corresponding laboneq
-    pulse object, and any sweeps associated with this pulse."""
-
-    def __init__(self, pulse):
-        self.pulse: Pulse = pulse
-        """Qibolab pulse."""
-        self.zhpulse = select_pulse(pulse)
-        """Laboneq pulse."""
-        self.zhsweepers: list[tuple[Parameter, laboneq.SweepParameter]] = []
-        """Parameters to be swept, along with their laboneq sweep parameter
-        definitions."""
-        self.delay_sweeper: Optional[laboneq.SweepParameter] = None
-        """Laboneq sweep parameter if the delay of the pulse should be
-        swept."""
-
-    # pylint: disable=R0903,E1101
-    def add_sweeper(self, param: Parameter, sweeper: laboneq.SweepParameter):
-        """Add sweeper to list of sweepers associated with this pulse."""
-        if param in {
-            Parameter.amplitude,
-            Parameter.frequency,
-            Parameter.duration,
-            Parameter.relative_phase,
-        }:
-            self.zhsweepers.append((param, sweeper))
-        elif param is Parameter.start:
-            # TODO: Change this case to ``Delay.duration``
-            if self.delay_sweeper:
-                raise ValueError(
-                    "Cannot have multiple delay sweepers for a single pulse"
-                )
-            self.delay_sweeper = sweeper
-        else:
-            raise ValueError(f"Sweeping {param} is not supported")
