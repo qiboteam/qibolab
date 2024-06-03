a52e4bc8a6d8727beeaf0b5ca68f92db0b7c5999
Hayk Sargsyan <52532457+hay-k@users.noreply.github.com>
2024-06-03 12:23:54 +0400

    implement possibility for user to add aux IQ channels

--- src/qibolab/channel/channel.py
+++ src/qibolab/channel/channel.py
@@ -1,7 +1,13 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Optional, Union
 
-from .configs import AcquisitionConfig, DcConfig, IqConfig, OscillatorConfig
+from .configs import (
+    AcquisitionConfig,
+    AuxIqConfig,
+    DcConfig,
+    IqConfig,
+    OscillatorConfig,
+)
 
 __all__ = [
     "ChannelConfig",
@@ -13,6 +19,9 @@ __all__ = [
 
 
 ChannelConfig = Union[AcquisitionConfig, DcConfig, IqConfig]
+AuxChannelConfig = Union[AuxIqConfig]
+
+AUX_SUFFIX = "__aux"
 
 
 @dataclass(frozen=True)
@@ -22,12 +31,34 @@ class Channel:
 
     A quantum computer can be perceived as just a set of channels where
     signals can be sent to or received from. Channels are identified
-    with a unique name. The type of a channel is inferred from the type
-    of config it accepts.
+    with a unique name. The type of channel is inferred from the type of
+    config it accepts.
     """
 
     name: str
     config: ChannelConfig
+
+    _aux_channels: dict[str, AuxChannelConfig] = field(init=False, default_factory=dict)
+
+    def __post_init__(self):
+        if AUX_SUFFIX in self.name:
+            raise ValueError(f"Channel name must not contain {AUX_SUFFIX}.")
+
+    @property
+    def aux_channels(self) -> dict[str, AuxChannelConfig]:
+        return self._aux_channels.copy()
+
+    def create_aux_channel(self, config: AuxChannelConfig) -> str:
+        if isinstance(self.config, IqConfig) and not isinstance(config, AuxIqConfig):
+            raise ValueError(
+                f"Channel {self.name} with configuration of type {IqConfig} (IQ channel) cannot contain aux channel "
+                f"with configuration of type {type(config)}"
+            )
+        elif not isinstance(self.config, IqConfig):
+            raise ValueError(f"Aux channels are supported for IQ channels only.")
+        name = f"{self.name}{AUX_SUFFIX}_{len(self._aux_channels)}"
+        self._aux_channels[name] = config
+        return name
 
 
 @dataclass(frozen=True)
