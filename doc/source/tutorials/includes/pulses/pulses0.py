446e6e188d9155f9b3f7d3dcb3fefee406816af1
Roland Siegbert <roland.siegbert@phys.ethz.ch>
2024-07-02 05:48:25 +0200

    Add pulses

---
+++ doc/source/tutorials/includes/pulses/pulses0.py
@@ -0,0 +1,36 @@
+# pulses0.py
+
+from qibolab.pulses import (
+    DrivePulse,
+    ReadoutPulse,
+    PulseSequence,
+    Rectangular,
+    Gaussian,
+)
+
+# Define PulseSequence
+sequence = PulseSequence()
+
+# Add some pulses to the pulse sequence
+sequence.add(
+    DrivePulse(
+        start=0,
+        frequency=200000000,
+        amplitude=0.3,
+        duration=60,
+        relative_phase=0,
+        shape=Gaussian(5),
+        qubit=0,
+    )
+)
+sequence.add(
+    ReadoutPulse(
+        start=70,
+        frequency=20000000.0,
+        amplitude=0.5,
+        duration=3000,
+        relative_phase=0,
+        shape=Rectangular(),
+        qubit=0,
+    )
+)
---
+++ doc/source/tutorials/includes/pulses/pulses1.py
@@ -0,0 +1,16 @@
+from qibolab import create_platform
+from qibolab.execution_parameters import ExecutionParameters
+from pulses0 import sequence
+
+# Define platform and load specific runcard
+platform = create_platform("dummy")
+
+# Connects to lab instruments using the details specified in the calibration settings.
+platform.connect()
+
+# Executes a pulse sequence.
+options = ExecutionParameters(nshots=1000, relaxation_time=100)
+results = platform.execute_pulse_sequence(sequence, options=options)
+
+# Disconnect from the instruments
+platform.disconnect()
