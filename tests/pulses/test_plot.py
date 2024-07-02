d99eacb8b75ea12ae5c8bbbb1660b1f9260df6f1
Hayk Sargsyan <52532457+hay-k@users.noreply.github.com>
2024-07-02 11:10:24 +0400

    update plot tests

--- tests/pulses/test_plot.py
+++ tests/pulses/test_plot.py
@@ -30,7 +30,6 @@ def test_plot_functions():
         envelope=Rectangular(),
         relative_phase=0,
         type=PulseType.FLUX,
-        qubit=0,
     )
     p1 = Pulse(
         duration=40,
@@ -39,7 +38,6 @@ def test_plot_functions():
         envelope=Gaussian(rel_sigma=0.2),
         relative_phase=0,
         type=PulseType.DRIVE,
-        qubit=2,
     )
     p2 = Pulse(
         duration=40,
@@ -48,18 +46,13 @@ def test_plot_functions():
         envelope=Drag(rel_sigma=0.2, beta=2),
         relative_phase=0,
         type=PulseType.DRIVE,
-        qubit=200,
     )
     p3 = Pulse.flux(
         duration=40,
         amplitude=0.9,
         envelope=Iir(a=np.array([-0.5, 2]), b=np.array([1]), target=Rectangular()),
-        channel="0",
-        qubit=200,
     )
-    p4 = Pulse.flux(
-        duration=40, amplitude=0.9, envelope=Snz(t_idling=10), channel="0", qubit=200
-    )
+    p4 = Pulse.flux(duration=40, amplitude=0.9, envelope=Snz(t_idling=10))
     p5 = Pulse(
         duration=40,
         amplitude=0.9,
@@ -75,22 +68,34 @@ def test_plot_functions():
         envelope=GaussianSquare(rel_sigma=0.2, width=0.9),
         relative_phase=0,
         type=PulseType.DRIVE,
-        qubit=2,
     )
-    ps = PulseSequence([p0, p1, p2, p3, p4, p5, p6])
+    ps = PulseSequence()
+    ps.update(
+        {
+            "q0/flux": [p0],
+            "q2/drive": [p1, p6],
+            "q200/drive": [p2],
+            "q200/flux": [p3, p4],
+            "q0/drive": [p5],
+        }
+    )
     envelope = p0.envelopes(SAMPLING_RATE)
     wf = modulate(np.array(envelope), 0.0, rate=SAMPLING_RATE)
 
     plot_file = HERE / "test_plot.png"
 
-    plot.waveform(wf, plot_file)
+    plot.waveform(wf, filename=plot_file)
     assert os.path.exists(plot_file)
     os.remove(plot_file)
 
-    plot.pulse(p0, plot_file)
+    plot.pulse(p0, filename=plot_file)
     assert os.path.exists(plot_file)
     os.remove(plot_file)
 
-    plot.sequence(ps, plot_file)
+    plot.pulse(p0, freq=2e9, filename=plot_file)
+    assert os.path.exists(plot_file)
+    os.remove(plot_file)
+
+    plot.sequence(ps, {"q200/drive": 3e9}, filename=plot_file)
     assert os.path.exists(plot_file)
     os.remove(plot_file)
