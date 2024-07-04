77544f1e23df5102eca50f15b26fd3aeffb2e02d
Stavros Efthymiou <35475381+stavros11@users.noreply.github.com>
2024-07-04 16:12:17 +0400

    fix: rebasing gone wrong

--- src/qibolab/pulses/plot.py
+++ src/qibolab/pulses/plot.py
@@ -26,7 +26,7 @@ def waveform(wf: Waveform, filename=None):
         filename (str): a file path. If provided the plot is save to a file.
     """
     plt.figure(figsize=(14, 5), dpi=200)
-    plt.plot(wf.data, c="C0", linestyle="dashed")
+    plt.plot(wf, c="C0", linestyle="dashed")
     plt.xlabel("Sample Number")
     plt.ylabel("Amplitude")
     plt.grid(visible=True, which="both", axis="both", color="#888888", linestyle="-")
@@ -57,14 +57,14 @@ def pulse(pulse_: Pulse, freq: Optional[float] = None, filename: Optional[str] =
     ax1 = plt.subplot(gs[0])
     ax1.plot(
         time,
-        waveform_i.data,
+        waveform_i,
         label="envelope i",
         c="C0",
         linestyle="dashed",
     )
     ax1.plot(
         time,
-        waveform_q.data,
+        waveform_q,
         label="envelope q",
         c="C1",
         linestyle="dashed",
@@ -86,13 +86,11 @@ def pulse(pulse_: Pulse, freq: Optional[float] = None, filename: Optional[str] =
     ax1.set_ylabel("Amplitude")
 
     ax1.grid(visible=True, which="both", axis="both", color="#888888", linestyle="-")
-    start = float(pulse_.start)
-    finish = float(pulse._finish) if pulse._finish is not None else 0.0
+    start = 0
+    finish = float(pulse_.duration)
     ax1.axis((start, finish, -1.0, 1.0))
     ax1.legend()
 
-    modulated_i = pulse_.shape.modulated_waveform_i(sampling_rate).data
-    modulated_q = pulse_.shape.modulated_waveform_q(sampling_rate).data
     ax2 = plt.subplot(gs[1])
     ax2.plot(waveform_i, waveform_q, label="envelope", c="C2")
     if modulated is not None:
