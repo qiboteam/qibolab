7bbf02e6f79b880ccb2698c58f1f38190fb31f8b
Stavros Efthymiou <35475381+stavros11@users.noreply.github.com>
2024-07-13 18:50:59 +0400

    fix: imports

--- src/qibolab/instruments/qm/controller.py
+++ src/qibolab/instruments/qm/controller.py
@@ -65,7 +65,7 @@ def find_baking_pulses(sweepers):
 
 
 @dataclass
-class QMController(Controller):
+class QmController(Controller):
     """:class:`qibolab.instruments.abstract.Controller` object for controlling
     a Quantum Machines cluster.
 
