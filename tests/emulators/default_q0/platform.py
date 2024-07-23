8dceab7d43c8763c5a5adb10bf7d32c5a21a7e88
Hayk Sargsyan <52532457+hay-k@users.noreply.github.com>
2024-07-23 10:59:42 +0400

    update documentation

--- tests/emulators/default_q0/platform.py
+++ tests/emulators/default_q0/platform.py
@@ -1,9 +1,10 @@
 import pathlib
 
-from qibolab.channels import ChannelMap
+from qibolab.components import IqChannel, AcquireChannel
 from qibolab.instruments.emulator.pulse_simulator import PulseSimulator
 from qibolab.platform import Platform
 from qibolab.serialize import (
+    load_component_config,
     load_instrument_settings,
     load_qubits,
     load_runcard,
@@ -29,23 +30,16 @@ def create():
     qubits, couplers, pairs = load_qubits(runcard)
     settings = load_settings(runcard)
 
-    # Create channel object
-    channels = ChannelMap()
-    channels |= (f"readout-{q}" for q in qubits)
-    channels |= (f"drive-{q}" for q in qubits)
-
-    # map channels to qubits
+    # define channels for qubits
     for q, qubit in qubits.items():
-        qubit.readout = channels[f"readout-{q}"]
-        qubit.drive = channels[f"drive-{q}"]
-
-        channels[f"drive-{q}"].qubit = qubit
-        qubit.sweetspot = 0  # not used
+        qubit.measure = IqChannel("measure-{q}", mixer=None, lo=None, acquisition="acquire-{q}")
+        qubit.acquisition = AcquireChannel("acquire-{q}", mixer=None, lo=None, measure="measure-{q}")
 
     return Platform(
         device_name,
         qubits,
         pairs,
+        {},
         instruments,
         settings,
         resonator_type="2D",
