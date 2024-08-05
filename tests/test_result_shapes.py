6c076e36b549172a0d2229393a8d9691332651ea
Alessandro Candido <candido.ale@gmail.com>
2024-08-05 19:34:33 +0200

    test: Lift execution function to fixture, for general reuse

--- tests/test_result_shapes.py
+++ tests/test_result_shapes.py
@@ -1,53 +1,16 @@
-import numpy as np
-import numpy.typing as npt
 import pytest
 
-from qibolab import AcquisitionType, AveragingMode, ExecutionParameters
-from qibolab.platform.platform import Platform
-from qibolab.pulses import PulseSequence
-from qibolab.sweeper import Parameter, Sweeper
+from qibolab import AcquisitionType, AveragingMode
 
 NSHOTS = 50
 NSWEEP1 = 5
 NSWEEP2 = 8
 
 
-def execute(
-    platform: Platform,
-    acquisition_type: AcquisitionType,
-    averaging_mode: AveragingMode,
-    sweep=False,
-) -> npt.NDArray:
-    qubit = next(iter(platform.qubits.values()))
-
-    qd_seq = qubit.native_gates.RX.create_sequence()
-    probe_seq = qubit.native_gates.MZ.create_sequence()
-    probe_pulse = next(iter(probe_seq.values()))[0]
-    sequence = PulseSequence()
-    sequence.extend(qd_seq)
-    sequence.extend(probe_seq)
-
-    options = ExecutionParameters(
-        nshots=NSHOTS, acquisition_type=acquisition_type, averaging_mode=averaging_mode
-    )
-    if sweep:
-        amp_values = np.arange(0.01, 0.06, 0.01)
-        freq_values = np.arange(-4e6, 4e6, 1e6)
-        sweeper1 = Sweeper(Parameter.bias, amp_values, channels=[qubit.flux.name])
-        sweeper2 = Sweeper(Parameter.amplitude, freq_values, pulses=[probe_pulse])
-        results = platform.execute([sequence], options, [[sweeper1], [sweeper2]])
-    else:
-        results = platform.execute([sequence], options)
-    return results[probe_pulse.id][0]
-
-
 @pytest.mark.parametrize("sweep", [False, True])
-def test_discrimination_singleshot(connected_platform, sweep):
+def test_discrimination_singleshot(execute, sweep):
     result = execute(
-        connected_platform,
-        AcquisitionType.DISCRIMINATION,
-        AveragingMode.SINGLESHOT,
-        sweep,
+        AcquisitionType.DISCRIMINATION, AveragingMode.SINGLESHOT, sweep, NSHOTS
     )
     if sweep:
         assert result.shape == (NSHOTS, NSWEEP1, NSWEEP2)
@@ -56,9 +19,9 @@ def test_discrimination_singleshot(connected_platform, sweep):
 
 
 @pytest.mark.parametrize("sweep", [False, True])
-def test_discrimination_cyclic(connected_platform, sweep):
+def test_discrimination_cyclic(execute, sweep):
     result = execute(
-        connected_platform, AcquisitionType.DISCRIMINATION, AveragingMode.CYCLIC, sweep
+        AcquisitionType.DISCRIMINATION, AveragingMode.CYCLIC, sweep, NSHOTS
     )
     if sweep:
         assert result.shape == (NSWEEP1, NSWEEP2)
@@ -67,9 +30,9 @@ def test_discrimination_cyclic(connected_platform, sweep):
 
 
 @pytest.mark.parametrize("sweep", [False, True])
-def test_integration_singleshot(connected_platform, sweep):
+def test_integration_singleshot(execute, sweep):
     result = execute(
-        connected_platform, AcquisitionType.INTEGRATION, AveragingMode.SINGLESHOT, sweep
+        AcquisitionType.INTEGRATION, AveragingMode.SINGLESHOT, sweep, NSHOTS
     )
     if sweep:
         assert result.shape == (NSHOTS, NSWEEP1, NSWEEP2, 2)
@@ -78,10 +41,8 @@ def test_integration_singleshot(connected_platform, sweep):
 
 
 @pytest.mark.parametrize("sweep", [False, True])
-def test_integration_cyclic(connected_platform, sweep):
-    result = execute(
-        connected_platform, AcquisitionType.INTEGRATION, AveragingMode.CYCLIC, sweep
-    )
+def test_integration_cyclic(execute, sweep):
+    result = execute(AcquisitionType.INTEGRATION, AveragingMode.CYCLIC, sweep, NSHOTS)
     if sweep:
         assert result.shape == (NSWEEP1, NSWEEP2, 2)
     else:
