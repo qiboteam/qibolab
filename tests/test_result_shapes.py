b188e4d5cfd9539584232f2c2d9112534f934394
Alessandro Candido <candido.ale@gmail.com>
2024-08-06 12:47:58 +0200

    test: Generalize execute fixture to accept custom sweepers

--- tests/test_result_shapes.py
+++ tests/test_result_shapes.py
@@ -8,37 +8,38 @@ NSWEEP1 = 5
 NSWEEP2 = 8
 
 
-@pytest.mark.parametrize("sweep", [False, True])
+@pytest.fixture(params=[False, True])
+def sweep(request):
+    return None if request.param else []
+
+
 def test_discrimination_singleshot(execute, sweep):
-    result = execute(Acq.DISCRIMINATION, Av.SINGLESHOT, sweep, NSHOTS)
-    if sweep:
-        assert result.shape == (NSHOTS, NSWEEP1, NSWEEP2)
-    else:
+    result = execute(Acq.DISCRIMINATION, Av.SINGLESHOT, NSHOTS, sweep)
+    if sweep == []:
         assert result.shape == (NSHOTS,)
+    else:
+        assert result.shape == (NSHOTS, NSWEEP1, NSWEEP2)
 
 
-@pytest.mark.parametrize("sweep", [False, True])
 def test_discrimination_cyclic(execute, sweep):
-    result = execute(Acq.DISCRIMINATION, Av.CYCLIC, sweep, NSHOTS)
-    if sweep:
-        assert result.shape == (NSWEEP1, NSWEEP2)
-    else:
+    result = execute(Acq.DISCRIMINATION, Av.CYCLIC, NSHOTS, sweep)
+    if sweep == []:
         assert result.shape == tuple()
+    else:
+        assert result.shape == (NSWEEP1, NSWEEP2)
 
 
-@pytest.mark.parametrize("sweep", [False, True])
 def test_integration_singleshot(execute, sweep):
-    result = execute(Acq.INTEGRATION, Av.SINGLESHOT, sweep, NSHOTS)
-    if sweep:
-        assert result.shape == (NSHOTS, NSWEEP1, NSWEEP2, 2)
-    else:
+    result = execute(Acq.INTEGRATION, Av.SINGLESHOT, NSHOTS, sweep)
+    if sweep == []:
         assert result.shape == (NSHOTS, 2)
-
-
-@pytest.mark.parametrize("sweep", [False, True])
-def test_integration_cyclic(execute, sweep):
-    result = execute(Acq.INTEGRATION, Av.CYCLIC, sweep, NSHOTS)
-    if sweep:
-        assert result.shape == (NSWEEP1, NSWEEP2, 2)
     else:
+        assert result.shape == (NSHOTS, NSWEEP1, NSWEEP2, 2)
+
+
+def test_integration_cyclic(execute, sweep):
+    result = execute(Acq.INTEGRATION, Av.CYCLIC, NSHOTS, sweep)
+    if sweep == []:
         assert result.shape == (2,)
+    else:
+        assert result.shape == (NSWEEP1, NSWEEP2, 2)
