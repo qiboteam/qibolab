b548b615fcadf38f14cdb24db9cf5b684b3445f3
Stavros Efthymiou <35475381+stavros11@users.noreply.github.com>
2024-08-08 16:53:45 +0400

    fix: missing import

--- src/qibolab/instruments/qm/config/config.py
+++ src/qibolab/instruments/qm/config/config.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass, field
-from typing import Union
+from typing import Optional, Union
 
 from qibolab.components.configs import IqConfig, OscillatorConfig
 from qibolab.pulses import Pulse
