91bc35d602264627485ad2c43eaed6b6d37afb1c
Alessandro Candido <candido.ale@gmail.com>
2024-08-22 13:38:40 +0200

    test: Remove flux constructor tests

--- tests/pulses/test_pulse.py
+++ tests/pulses/test_pulse.py
@@ -1,18 +1,10 @@
 """Tests ``pulses.py``."""
 
 import numpy as np
-import pytest
+from pytest import approx, raises
 
 from qibolab.pulses import Acquisition, Custom, Pulse, Rectangular, VirtualZ
 from qibolab.pulses.pulse import _Readout
-
-
-def test_flux():
-    p = Pulse.flux(duration=5, amplitude=0.9, envelope=Rectangular())
-    assert p.relative_phase == 0
-
-    p1 = Pulse.flux(duration=5, amplitude=0.9, relative_phase=1, envelope=Rectangular())
-    assert p1.relative_phase == 0
 
 
 def test_virtual_z():
@@ -31,10 +23,10 @@ def test_readout():
 def test_envelope_waveform_i_q():
     d = 1000
     p = Pulse(duration=d, amplitude=1, envelope=Rectangular())
-    assert pytest.approx(p.i(1)) == np.ones(d)
-    assert pytest.approx(p.i(2)) == np.ones(2 * d)
-    assert pytest.approx(p.q(1)) == np.zeros(d)
-    assert pytest.approx(p.envelopes(1)) == np.stack([np.ones(d), np.zeros(d)])
+    assert approx(p.i(1)) == np.ones(d)
+    assert approx(p.i(2)) == np.ones(2 * d)
+    assert approx(p.q(1)) == np.zeros(d)
+    assert approx(p.envelopes(1)) == np.stack([np.ones(d), np.zeros(d)])
 
     envelope_i = np.cos(np.arange(0, 10, 0.01))
     envelope_q = np.sin(np.arange(0, 10, 0.01))
@@ -42,7 +34,7 @@ def test_envelope_waveform_i_q():
     pulse = Pulse(duration=1000, amplitude=1, relative_phase=0, envelope=Rectangular())
 
     custom_shape_pulse = custom_shape_pulse.model_copy(update={"i_": pulse.i(1)})
-    with pytest.raises(ValueError):
+    with raises(ValueError):
         custom_shape_pulse.i(samples=10)
-    with pytest.raises(ValueError):
+    with raises(ValueError):
         custom_shape_pulse.q(samples=10)
