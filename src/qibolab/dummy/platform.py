202f0dd41a975ed5def413b5becd98e700bf77fb
Alessandro Candido <candido.ale@gmail.com>
2024-08-26 16:16:13 +0200

    fix: Define channels independently from the instrument in dummy

--- src/qibolab/dummy/platform.py
+++ src/qibolab/dummy/platform.py
@@ -10,11 +10,10 @@ FOLDER = pathlib.Path(__file__).parent
 
 def create_dummy() -> Platform:
     """Create a dummy platform using the dummy instrument."""
-    # register the instruments
-    instrument = DummyInstrument(name="dummy", address="0.0.0.0")
-    pump = DummyLocalOscillator(name="twpa_pump", address="0.0.0.0")
+    pump_name = "twpa_pump"
 
     qubits = {}
+    channels = {}
     # attach the channels
     for q in range(5):
         drive, drive12, flux, probe, acquisition = (
@@ -24,9 +23,9 @@ def create_dummy() -> Platform:
             f"qubit_{q}/probe",
             f"qubit_{q}/acquisition",
         )
-        instrument.channels |= {
+        channels |= {
             probe: IqChannel(mixer=None, lo=None),
-            acquisition: AcquireChannel(twpa_pump=pump.name, probe=probe),
+            acquisition: AcquireChannel(twpa_pump=pump_name, probe=probe),
             drive: IqChannel(mixer=None, lo=None),
             drive12: IqChannel(mixer=None, lo=None),
             flux: DcChannel(),
@@ -42,8 +41,12 @@ def create_dummy() -> Platform:
     couplers = {}
     for c in (0, 1, 3, 4):
         flux = f"coupler_{c}/flux"
-        instrument.channels |= {flux: DcChannel()}
+        channels |= {flux: DcChannel()}
         couplers[c] = Qubit(flux=flux)
+
+    # register the instruments
+    instrument = DummyInstrument(name="dummy", address="0.0.0.0", channels=channels)
+    pump = DummyLocalOscillator(name=pump_name, address="0.0.0.0")
 
     return Platform.load(
         path=FOLDER, instruments=[instrument, pump], qubits=qubits, couplers=couplers
