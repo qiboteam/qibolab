fc4b66e81fc6dcad8053a7c6e9f81225b24f5012
Alessandro Candido <candido.ale@gmail.com>
2024-08-28 16:53:25 +0200

    test: Properly move unrolling test
    Slightly messed up during rebase

--- tests/test_unrolling.py
+++ tests/test_unrolling.py
@@ -109,10 +109,13 @@ def test_batch(bounds):
 
 def test_unroll_sequences(platform: Platform):
     qubit = next(iter(platform.qubits.values()))
+    assert qubit.probe is not None
     natives = platform.natives.single_qubit[0]
+    assert natives.RX is not None
+    assert natives.MZ is not None
     sequence = PulseSequence()
     sequence.concatenate(natives.RX.create_sequence())
-    sequence.append((qubit.probe.name, Delay(duration=sequence.duration)))
+    sequence.append((qubit.probe, Delay(duration=sequence.duration)))
     sequence.concatenate(natives.MZ.create_sequence())
     total_sequence, readouts = unroll_sequences(10 * [sequence], relaxation_time=10000)
     assert len(total_sequence.acquisitions) == 10
