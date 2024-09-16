aa872548e444e7bb139921fa4d8a854acbf5bd89
Alessandro Candido <candido.ale@gmail.com>
2024-09-16 16:00:46 +0200

    fix: Add instruments public subpackage

--- src/qibolab/__init__.py
+++ src/qibolab/__init__.py
@@ -1,7 +1,8 @@
-from . import _core, _version
+from . import _core, _version, instruments
 from ._core import *
 from ._version import *
 
 __all__ = []
 __all__ += _core.__all__
 __all__ += _version.__all__
+__all__ += ["instruments"]
