3c26826d62fdf388166adf3f5a352150628166ae
Alessandro Candido <candido.ale@gmail.com>
2024-09-16 14:33:25 +0200

    fix: Move qibolab content to private core subpackage
    To enforce the public API usage

--- src/qibolab/__init__.py
+++ src/qibolab/__init__.py
@@ -1,27 +1,5 @@
-from . import (
-    backends,
-    components,
-    execution_parameters,
-    platform,
-    pulses,
-    sequence,
-    sweeper,
-    version,
-)
-from .backends import *
-from .execution_parameters import *
-from .platform import *
-from .pulses import *
-from .sequence import *
-from .sweeper import *
-from .version import *
+from . import _core
+from ._core import *
 
 __all__ = []
-__all__ += backends.__all__
-__all__ += components.__all__
-__all__ += execution_parameters.__all__
-__all__ += platform.__all__
-__all__ += pulses.__all__
-__all__ += sequence.__all__
-__all__ += sweeper.__all__
-__all__ += version.__all__
+__all__ += _core.__all__
