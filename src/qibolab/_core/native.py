fix: Update name of rotation generating function

src/qibolab/_core/native.py
@@ -29,7 +29,9 @@ def _normalize_angles(theta, phi):
     return theta, phi
 
 
-def rxy(seq: PulseSequence, theta: float = np.pi, phi: float = 0.0) -> PulseSequence:
+def rotation(
+    seq: PulseSequence, theta: float = np.pi, phi: float = 0.0
+) -> PulseSequence:
     """Create a sequence for single-qubit rotation.
 
     ``theta`` will be the angle of the rotation, while ``phi`` the angle that the rotation axis forms with x axis.
@@ -77,7 +79,7 @@ class SingleQubitNatives(NativeContainer):
         ``theta`` will be the angle of the rotation, while ``phi`` the angle that the rotation axis forms with x axis.
         """
         assert self.RX is not None
-        return rxy(self.RX, theta, phi)
+        return rotation(self.RX, theta, phi)
 
 
 class TwoQubitNatives(NativeContainer):

tests/test_native.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pytest
 
-from qibolab._core.native import Native, TwoQubitNatives, rxy
+from qibolab._core.native import Native, TwoQubitNatives, rotation
 from qibolab._core.pulses import Drag, Gaussian, Pulse, Rectangular
 from qibolab._core.sequence import PulseSequence
 
@@ -55,7 +55,7 @@ def test_fixed_sequence_factory():
         ({"phi": 7.5 * np.pi}, 1.0, 1.5 * np.pi),
     ],
 )
-def test_rxy(args, amplitude, phase):
+def test_rotation(args, amplitude, phase):
     seq = PulseSequence(
         [
             (
@@ -65,8 +65,8 @@ def test_rxy(args, amplitude, phase):
         ]
     )
 
-    fseq1 = rxy(seq, **args)
-    fseq2 = rxy(seq, **args)
+    fseq1 = rotation(seq, **args)
+    fseq2 = rotation(seq, **args)
     assert fseq1 == fseq2
     np = "new/probe"
     fseq2.append((np, Pulse(duration=56, amplitude=0.43, envelope=Rectangular())))