b9831ca98e1d9fa46437f89a7f9cbad7dec6cb5b
Alessandro Candido <candido.ale@gmail.com>
2024-09-19 14:04:14 +0200

    feat!: Drop rxy factory in favor of method

--- src/qibolab/_core/native.py
+++ src/qibolab/_core/native.py
@@ -4,7 +4,7 @@ from typing import Annotated, Optional
 
 import numpy as np
 
-from .pulses import Drag, Gaussian, Pulse
+from .pulses import Pulse
 from .sequence import PulseSequence
 from .serialize import Model, replace
 
@@ -30,56 +30,18 @@ class Native(ABC, PulseSequence):
         return self.create_sequence(*args, **kwargs)
 
 
-class RxyFactory(Native):
-    """Factory for pulse sequences that generate single-qubit rotations around
-    an axis in xy plane.
+def rxy(seq: PulseSequence, theta: float = np.pi, phi: float = 0.0) -> PulseSequence:
+    """Create a sequence for single-qubit rotation.
 
-    It is assumed that the underlying sequence contains only a single pulse.
-    It is assumed that the base sequence corresponds to a calibrated pi rotation around X axis.
-    Other rotation angles are achieved by scaling the amplitude, assuming a linear transfer function.
-
-    Args:
-        sequence: The base sequence for the factory.
+    ``theta`` will be the angle of the rotation, while ``phi`` the angle that the rotation axis forms with x axis.
     """
-
-    def __init__(self, iterable):
-        super().__init__(iterable)
-        cls = type(self)
-        if len(self.channels) != 1:
-            raise ValueError(
-                f"Incompatible number of channels: {len(self.channels)}. "
-                f"{cls} expects a sequence on exactly one channel."
-            )
-
-        if len(self) != 1:
-            raise ValueError(
-                f"Incompatible number of pulses: {len(self)}. "
-                f"{cls} expects a sequence with exactly one pulse."
-            )
-
-        pulse = self[0][1]
-        assert isinstance(pulse, Pulse)
-        expected_envelopes = (Gaussian, Drag)
-        if not isinstance(pulse.envelope, expected_envelopes):
-            raise ValueError(
-                f"Incompatible pulse envelope: {pulse.envelope.__class__}. "
-                f"{cls} expects {expected_envelopes} envelope."
-            )
-
-    def create_sequence(self, theta: float = np.pi, phi: float = 0.0) -> PulseSequence:
-        """Create a sequence for single-qubit rotation.
-
-        Args:
-            theta: the angle of rotation.
-            phi: the angle that rotation axis forms with x axis.
-        """
-        theta, phi = _normalize_angles(theta, phi)
-        ch, pulse = self[0]
-        assert isinstance(pulse, Pulse)
-        new_amplitude = pulse.amplitude * theta / np.pi
-        return PulseSequence(
-            [(ch, replace(pulse, amplitude=new_amplitude, relative_phase=phi))]
-        )
+    theta, phi = _normalize_angles(theta, phi)
+    ch, pulse = seq[0]
+    assert isinstance(pulse, Pulse)
+    new_amplitude = pulse.amplitude * theta / np.pi
+    return PulseSequence(
+        [(ch, replace(pulse, amplitude=new_amplitude, relative_phase=phi))]
+    )
 
 
 class FixedSequenceFactory(Native):
@@ -108,7 +70,7 @@ class SingleQubitNatives(NativeContainer):
     """Container with the native single-qubit gates acting on a specific
     qubit."""
 
-    RX: Optional[RxyFactory] = None
+    RX: Optional[FixedSequenceFactory] = None
     """Pulse to drive the qubit from state 0 to state 1."""
     RX12: Optional[FixedSequenceFactory] = None
     """Pulse to drive to qubit from state 1 to state 2."""
@@ -116,6 +78,14 @@ class SingleQubitNatives(NativeContainer):
     """Measurement pulse."""
     CP: Optional[FixedSequenceFactory] = None
     """Pulse to activate coupler."""
+
+    def RXY(self, theta: float = np.pi, phi: float = 0.0) -> PulseSequence:
+        """Create a sequence for single-qubit rotation.
+
+        ``theta`` will be the angle of the rotation, while ``phi`` the angle that the rotation axis forms with x axis.
+        """
+        assert self.RX is not None
+        return rxy(self.RX, theta, phi)
 
 
 class TwoQubitNatives(NativeContainer):
