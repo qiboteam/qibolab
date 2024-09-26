866685775580d96333e6039be49381780eeef97f
Alessandro Candido <candido.ale@gmail.com>
2024-09-26 18:01:07 +0200

    fix: Rebase leftovers

--- flake.nix
+++ flake.nix
@@ -60,7 +60,11 @@
                   install = {
                     enable = true;
                     groups = ["dev" "analysis" "tests"];
-                    allExtras = true;
+                    extras = [
+                      (lib.strings.concatStrings
+                        (lib.strings.intersperse " -E "
+                          ["qblox" "qm" "zh" "rfsoc" "los"]))
+                    ];
                   };
                 };
               };
