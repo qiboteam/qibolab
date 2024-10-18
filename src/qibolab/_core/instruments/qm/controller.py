77ede74a030a081f783bc002b78eba900b5f2d12
Stavros Efthymiou <35475381+stavros11@users.noreply.github.com>
2024-10-18 12:58:50 +0400

    fix: handle integer duration in QM

--- src/qibolab/_core/instruments/qm/controller.py
+++ src/qibolab/_core/instruments/qm/controller.py
@@ -331,7 +331,9 @@ class QmController(Controller):
             acquisitions (dict): Map from measurement instructions to acquisition objects.
         """
         for id, pulse in sequence:
-            if hasattr(pulse, "duration") and not pulse.duration.is_integer():
+            if hasattr(pulse, "duration") and not (
+                isinstance(pulse.duration, int) or pulse.duration.is_integer()
+            ):
                 raise ValueError(
                     f"Quantum Machines cannot play pulse with duration {pulse.duration}. "
                     "Only integer duration in ns is supported."
