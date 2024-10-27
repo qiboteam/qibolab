1e613efed293e34d260680bec70878467ff1d13c
Alessandro Candido <candido.ale@gmail.com>
2024-10-27 15:09:33 +0100

    test: Split q1asn tests

--- tests/qblox/test_q1asm.py
+++ tests/qblox/test_q1asm.py
@@ -1,15 +1,28 @@
 from pathlib import Path
 
+import pytest
+
 from qibolab._core.instruments.qblox.ast_ import Program
 from qibolab._core.instruments.qblox.parse import parse
 
 PROGRAMS = Path(__file__).parent / "q1asm"
 
 
-def test_q1asm():
-    for program in PROGRAMS.glob("*.q1asm"):
-        asm = program.read_text()
-        prog = parse(asm)
-        assert isinstance(prog, Program)
-        prog_reload = parse(prog.asm())
-        assert prog_reload == prog
+@pytest.fixture(params=PROGRAMS.glob("*.q1asm"), ids=lambda p: p.stem)
+def asm(request):
+    return request.param.read_text()
+
+
+def test_load(asm):
+    prog = parse(asm)
+    assert isinstance(prog, Program)
+
+
+def test_dump(asm):
+    prog = parse(asm)
+    assert isinstance(prog.asm(), str)
+
+
+def test_roundtrip(asm):
+    prog = parse(asm)
+    assert prog == parse(prog.asm())
