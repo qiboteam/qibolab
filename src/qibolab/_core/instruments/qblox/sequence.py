07483a0564ba0d78d4ce99a61a9bd0ad47b2881f
Alessandro Candido <candido.ale@gmail.com>
2024-11-01 17:37:21 +0100

    fix: Complete sequence serialization format

--- src/qibolab/_core/instruments/qblox/sequence.py
+++ src/qibolab/_core/instruments/qblox/sequence.py
@@ -2,28 +2,29 @@ from typing import Annotated
 
 from pydantic import PlainSerializer, PlainValidator
 
-from qibolab._core.serialize import Model
+from qibolab._core.serialize import ArrayList, Model
 
 from .ast_ import Program
 from .parse import parse
 
 
-class Waveforms(Model):
-    pass
+class Waveform(Model):
+    data: ArrayList
+    index: int
 
 
-class Weights(Model):
-    pass
+Weight = Waveform
 
 
-class Acquisitions(Model):
-    pass
+class Acquisition(Model):
+    num_bins: int
+    index: int
 
 
 class Sequence(Model):
-    waveforms: Waveforms
-    weights: Weights
-    acquisitions: Acquisitions
+    waveforms: dict[str, Waveform]
+    weights: dict[str, Weight]
+    acquisitions: dict[str, Acquisition]
     program: Annotated[
         Program, PlainSerializer(lambda p: p.asm()), PlainValidator(parse)
     ]
