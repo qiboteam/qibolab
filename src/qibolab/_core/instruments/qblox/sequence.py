3d3c51193d8e67d5b3b9e11954b91481ccf3a931
Alessandro Candido <candido.ale@gmail.com>
2024-11-03 23:29:20 +0100

    fix: Scaffold address resolution and sequence processing

--- src/qibolab/_core/instruments/qblox/sequence.py
+++ src/qibolab/_core/instruments/qblox/sequence.py
@@ -2,7 +2,10 @@ from typing import Annotated
 
 from pydantic import AfterValidator, PlainSerializer, PlainValidator
 
+from qibolab._core.execution_parameters import ExecutionParameters
+from qibolab._core.sequence import PulseSequence
 from qibolab._core.serialize import ArrayList, Model
+from qibolab._core.sweeper import ParallelSweepers
 
 from .ast_ import Program
 from .parse import parse
@@ -30,3 +33,14 @@ class Sequence(Model):
     program: Annotated[
         Program, PlainSerializer(lambda p: p.asm()), PlainValidator(parse)
     ]
+
+    @classmethod
+    def from_pulses(
+        cls,
+        sequence: PulseSequence,
+        sweepers: list[ParallelSweepers],
+        options: ExecutionParameters,
+    ):
+        return cls(
+            waveforms={}, weights={}, acquisitions={}, program=Program(elements=[])
+        )
