3edfda80151836ca8c0ec4daf19bfa846da8f2c8
Alessandro Candido <candido.ale@gmail.com>
2024-11-03 15:04:43 +0100

    feat: Expose qblox drivers within the public API

---
+++ src/qibolab/instruments/qblox.py
@@ -0,0 +1,11 @@
+"""Qblox drivers.
+
+https://www.qblox.com/
+https://docs.qblox.com/
+"""
+
+from qibolab._core.instruments import qblox
+from qibolab._core.instruments.qblox import *  # noqa: F403
+
+__all__ = []
+__all__ += qblox.__all__
