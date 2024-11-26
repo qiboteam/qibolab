88d0fd10cd5cbf685997f5acd26defb38b5e6927
Elisa Stabilini <el.stabilini@gmail.com>
2024-11-26 05:33:43 +0100

    Update src/qibolab/_core/native.py
    Co-authored-by: Stavros Efthymiou <35475381+stavros11@users.noreply.github.com>

--- src/qibolab/_core/native.py
+++ src/qibolab/_core/native.py
@@ -80,8 +80,8 @@ class SingleQubitNatives(NativeContainer):
 
         ``theta`` will be the angle of the rotation, while ``phi`` the angle that the rotation axis forms with x axis.
         """
-        if self.RX90 != None:
-            return rotation(self.RX, 2 * theta, phi)
+        if self.RX90 is not None:
+            return rotation(self.RX90, 2 * theta, phi)
         assert self.RX is not None
         return rotation(self.RX, theta, phi)
 
