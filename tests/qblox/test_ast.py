5bd6a8a9d7f01a317e9e8eb70f4965abeb7818c9
Alessandro Candido <candido.ale@gmail.com>
2024-12-06 17:25:47 +0100

    test: Add failing test for immediate deserialization

--- tests/qblox/test_ast.py
+++ tests/qblox/test_ast.py
@@ -1,6 +1,12 @@
 from pydantic import TypeAdapter
 
-from qibolab._core.instruments.qblox.ast_ import Value
+from qibolab._core.instruments.qblox.ast_ import Immediate, Value
+
+
+def test_immediate_load():
+    load = TypeAdapter(Immediate).validate_python
+    im = load(42)
+    assert im == 42
 
 
 def test_value_load():
