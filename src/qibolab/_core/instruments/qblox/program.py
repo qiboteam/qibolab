2b16e03be8a07da95c5b4e2e99e8e20b2bc583a1
Alessandro Candido <candido.ale@gmail.com>
2024-12-06 22:32:06 +0100

    feat: Begin program generation

--- src/qibolab/_core/instruments/qblox/program.py
+++ src/qibolab/_core/instruments/qblox/program.py
@@ -1,5 +1,39 @@
-from .ast_ import Program
+from qibolab._core.pulses.pulse import Pulse, PulseLike
+from qibolab._core.sequence import PulseSequence
+
+from .ast_ import Line, Play, Program
 
 
-def program():
-    return Program(elements=[])
+ComponentId = tuple[str, int]
+WaveformIndices = dict[ComponentId, int]
+
+
+def pulse_uid(pulse: Pulse) -> str:
+    return str(hash(pulse))
+
+
+def instructions(pulse: PulseLike, waveforms: WaveformIndices) -> list:
+    breakpoint()
+    return (
+        [
+            Line(
+                instruction=Play(
+                    wave_0=waveforms[(pulse_uid(pulse), 0)],
+                    wave_1=waveforms[(pulse_uid(pulse), 1)],
+                    duration=0,
+                )
+            )
+        ]
+        if isinstance(pulse, Pulse)
+        else []
+    )
+
+
+def program(sequence: PulseSequence, waveforms: WaveformIndices):
+    return Program(
+        elements=[
+            el
+            for block in (instructions(pulse, waveforms) for _, pulse in sequence)
+            for el in block
+        ]
+    )
