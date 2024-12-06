f7d0662a24a30a2e5b1ba11a0c824b2abe54304c
Alessandro Candido <candido.ale@gmail.com>
2024-12-06 17:30:43 +0100

    test: Extend immediate loading test

--- tests/qblox/test_ast.py
+++ tests/qblox/test_ast.py
@@ -5,8 +5,10 @@ from qibolab._core.instruments.qblox.ast_ import Immediate, Value
 
 def test_immediate_load():
     load = TypeAdapter(Immediate).validate_python
-    im = load(42)
-    assert im == 42
+
+    for input_ in [42, "42", "0x2a", "0o52"]:
+        im = load(input_)
+        assert im == 42
 
 
 def test_value_load():
