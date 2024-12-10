2c5ed2a3c917dd25dc2b6bb50aa78346751c85e2
Alessandro Candido <candido.ale@gmail.com>
2024-12-10 18:37:59 +0100

    feat: Introduce primitive loops support

--- src/qibolab/_core/instruments/qblox/sequence.py
+++ src/qibolab/_core/instruments/qblox/sequence.py
@@ -81,5 +81,5 @@ class Sequence(Model):
             waveforms=waveforms_,
             weights={},
             acquisitions={},
-            program=program(sequence, waveform_indices(waveforms_)),
+            program=program(sequence, waveform_indices(waveforms_), options),
         )
