11a7e9ad1c70a26526a401c1d4610e43d4625fab
Alessandro Candido <candido.ale@gmail.com>
2024-12-10 13:56:54 +0100

    feat: Add configuration for logging

--- src/qibolab/_core/components/configs.py
+++ src/qibolab/_core/components/configs.py
@@ -7,6 +7,7 @@ users, but in general any user tool should try to depend only on the
 configuration defined by these classes.
 """
 
+from pathlib import Path
 from typing import Annotated, Literal, Optional, Union
 
 from pydantic import Field
@@ -120,6 +121,14 @@ class AcquisitionConfig(Config):
         )
 
 
+class LogConfig(Config):
+    """Configuration for logging."""
+
+    kind: Literal["log"] = "log"
+
+    path: Path
+
+
 ChannelConfig = Union[
-    DcConfig, IqMixerConfig, OscillatorConfig, IqConfig, AcquisitionConfig
+    DcConfig, IqMixerConfig, OscillatorConfig, IqConfig, AcquisitionConfig, LogConfig
 ]
