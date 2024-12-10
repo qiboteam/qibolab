f8fa0e31f2f153c302c911e87f0f30b1e7e27a6f
Alessandro Candido <candido.ale@gmail.com>
2024-12-10 13:17:44 +0100

    build: Stop generating LD_LIBRARY_PATH manually, trim unused definitions

--- flake.nix
+++ flake.nix
@@ -6,6 +6,10 @@
       url = "github:cachix/devenv";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nixpkgs-python = {
       url = "github:cachix/nixpkgs-python";
       inputs = {nixpkgs.follows = "nixpkgs";};
@@ -49,11 +53,14 @@
 
               env = {
                 QIBOLAB_PLATFORMS = (dirOf config.env.DEVENV_ROOT) + "/qibolab_platforms_qrc";
+                LD_LIBRARY_PATH = builtins.concatStringsSep ":" (map (p: "${p}/lib") (with pkgs; [
+                  stdenv.cc.cc.lib
+                  zlib
+                ]));
               };
 
               languages.python = {
                 enable = true;
-                libraries = with pkgs; [zlib];
                 version = "3.11";
                 poetry = {
                   enable = true;
