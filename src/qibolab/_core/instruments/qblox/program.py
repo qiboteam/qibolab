2df503001db7139fed9b056a494aa02ead1fbccc
Alessandro Candido <candido.ale@gmail.com>
2024-12-11 13:55:26 +0100

    feat: Add support for delay and virtual z ops

--- src/qibolab/_core/instruments/qblox/program.py
+++ src/qibolab/_core/instruments/qblox/program.py
@@ -1,5 +1,7 @@
+import numpy as np
+
 from qibolab._core.execution_parameters import AveragingMode, ExecutionParameters
-from qibolab._core.pulses.pulse import Pulse, PulseLike
+from qibolab._core.pulses.pulse import Delay, Pulse, PulseLike, VirtualZ
 from qibolab._core.sequence import PulseSequence
 from qibolab._core.sweeper import ParallelSweepers
 
@@ -36,11 +38,13 @@ def setup() -> list:
     return [Line(instruction=WaitSync(duration=4))]
 
 
-def execution(sequence: PulseSequence, waveforms: WaveformIndices) -> list[Line]:
+def execution(
+    sequence: PulseSequence, waveforms: WaveformIndices, sampling_rate: float
+) -> list[Line]:
     """The representation of the actual experiment to be executed."""
     return [
         el
-        for block in (play(pulse, waveforms) for _, pulse in sequence)
+        for block in (play(pulse, waveforms, sampling_rate) for _, pulse in sequence)
         for el in block
     ]
 
@@ -81,16 +85,17 @@ def finalization() -> list:
     return [Line(instruction=Stop())]
 
 
-def parameters_update(sweepers: ParallelSweepers) -> list[Instruction]:
-    return [Nop()]
+PHASE_FACTOR = 1e9 / (2 * np.pi)
 
 
-def play(pulse: PulseLike, waveforms: WaveformIndices) -> list[Line]:
+def play(
+    pulse: PulseLike, waveforms: WaveformIndices, sampling_rate: float
+) -> list[Line]:
     """Process the individual pulse in experiment."""
     return (
         [
-            Line(
-                instruction=Play(
+            Line.instr(
+                Play(
                     wave_0=waveforms[(pulse_uid(pulse), 0)],
                     wave_1=waveforms[(pulse_uid(pulse), 1)],
                     duration=0,
@@ -98,6 +103,10 @@ def play(pulse: PulseLike, waveforms: WaveformIndices) -> list[Line]:
             )
         ]
         if isinstance(pulse, Pulse)
+        else [Line.instr(Wait(duration=int(pulse.duration * sampling_rate)))]
+        if isinstance(pulse, Delay)
+        else [Line.instr(SetPhDelta(value=int(pulse.phase * PHASE_FACTOR)))]
+        if isinstance(pulse, VirtualZ)
         else []
     )
 
@@ -107,13 +116,14 @@ def program(
     waveforms: WaveformIndices,
     options: ExecutionParameters,
     sweepers: list[ParallelSweepers],
+    sampling_rate: float,
 ):
     return Program(
         elements=initialization(options.nshots)
         + setup()
         + loop(
             sweepers,
-            execution(sequence, waveforms),
+            execution(sequence, waveforms, sampling_rate),
             relaxation_time=options.relaxation_time,
             outer_shots=options.averaging_mode is not AveragingMode.SEQUENTIAL,
         )
