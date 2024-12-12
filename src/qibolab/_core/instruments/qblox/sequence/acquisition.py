3113f307150484355f9b6adbac72034b1c339635
Alessandro Candido <candido.ale@gmail.com>
2024-12-12 16:58:51 +0100

    fix: Disambiguate acquisition pulse-like event

--- src/qibolab/_core/instruments/qblox/sequence/acquisition.py
+++ src/qibolab/_core/instruments/qblox/sequence/acquisition.py
@@ -1,3 +1,4 @@
+from qibolab._core import pulses
 from qibolab._core.sequence import PulseSequence
 from qibolab._core.serialize import Model
 
@@ -13,5 +14,7 @@ Acquisitions = dict[str, Acquisition]
 def acquisitions(sequence: PulseSequence, num_bins: int) -> Acquisitions:
     return {
         str(acq.id): Acquisition(num_bins=num_bins, index=i)
-        for i, acq in enumerate([p for _, p in sequence if isinstance(p, Acquisition)])
+        for i, acq in enumerate(
+            [p for _, p in sequence if isinstance(p, pulses.Acquisition)]
+        )
     }
