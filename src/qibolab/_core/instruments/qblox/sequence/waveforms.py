dc7d6725839e1e80a5e18c20cd7bbe98d2912a31
Alessandro Candido <candido.ale@gmail.com>
2024-12-12 15:46:10 +0100

    feat: Introduce qblox acquisitions

---
+++ src/qibolab/_core/instruments/qblox/sequence/waveforms.py
@@ -0,0 +1,48 @@
+from typing import Annotated, Union
+
+from pydantic import AfterValidator
+
+from qibolab._core.pulses import Pulse, Readout
+from qibolab._core.sequence import PulseSequence
+from qibolab._core.serialize import ArrayList, Model
+
+from .program import ComponentId, pulse_uid
+
+WaveformIndices = dict[ComponentId, int]
+
+
+class Waveform(Model):
+    data: Annotated[ArrayList, AfterValidator(lambda a: a.astype(float))]
+    index: int
+
+
+Waveforms = dict[ComponentId, Waveform]
+
+
+def waveforms(sequence: PulseSequence, sampling_rate: float) -> Waveforms:
+    def waveform(pulse: Pulse, component: str) -> Waveform:
+        return Waveform(data=getattr(pulse, component)(sampling_rate), index=0)
+
+    def pulse_(event: Union[Pulse, Readout]) -> Pulse:
+        return event.probe if isinstance(event, Readout) else event
+
+    indexless = {
+        k: v
+        for d in (
+            {
+                (pulse_uid(pulse_(event)), 0): waveform(pulse_(event), "i"),
+                (pulse_uid(pulse_(event)), 1): waveform(pulse_(event), "q"),
+            }
+            for _, event in sequence
+            if isinstance(event, (Pulse, Readout))
+        )
+        for k, v in d.items()
+    }
+
+    return {
+        k: Waveform(data=v.data, index=i) for i, (k, v) in enumerate(indexless.items())
+    }
+
+
+def waveform_indices(waveforms: Waveforms) -> WaveformIndices:
+    return {k: w.index for k, w in waveforms.items()}
