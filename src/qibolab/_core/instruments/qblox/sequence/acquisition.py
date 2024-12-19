d6bc80fdf1558bba5f4076e84b6c938974913293
Alessandro Candido <candido.ale@gmail.com>
2024-12-19 21:29:40 +0100

    fix: Describe acquired data structure
    https://docs.qblox.com/en/main/api_reference/cluster.html#Cluster.get_acquisitions

--- src/qibolab/_core/instruments/qblox/sequence/acquisition.py
+++ src/qibolab/_core/instruments/qblox/sequence/acquisition.py
@@ -1,6 +1,10 @@
+from typing import TypedDict
+
 from qibolab._core import pulses
 from qibolab._core.sequence import PulseSequence
 from qibolab._core.serialize import Model
+
+__all__ = []
 
 
 class Acquisition(Model):
@@ -18,3 +22,39 @@ def acquisitions(sequence: PulseSequence, num_bins: int) -> Acquisitions:
             [p for _, p in sequence if isinstance(p, pulses.Acquisition)]
         )
     }
+
+
+IndividualScope = TypedDict(
+    "IndividualScope",
+    {"data": list[float], "out-of-range": list[bool], "avg_cnt": list[bool]},
+)
+
+
+class ScopeData(TypedDict):
+    path0: IndividualScope
+    path1: IndividualScope
+
+
+class Integration(TypedDict):
+    path0: list[float]
+    path1: list[float]
+
+
+class BinData(TypedDict):
+    integration: Integration
+    threshold: list[int]
+    valid: list
+    avg_cnt: list[int]
+
+
+class Data(TypedDict):
+    scope: ScopeData
+    bins: BinData
+
+
+class IndexedData(TypedDict):
+    index: int
+    acquisition: Data
+
+
+AcquiredData = dict[str, IndexedData]
