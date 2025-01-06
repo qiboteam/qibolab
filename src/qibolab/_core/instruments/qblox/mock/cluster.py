28749b0ce683a99ec9fe6528e2a62e9e4c983919
Alessandro Candido <candido.ale@gmail.com>
2025-01-06 20:43:32 +0100

    fix: Mock modules as well, since strictly required

--- src/qibolab/_core/instruments/qblox/mock/cluster.py
+++ src/qibolab/_core/instruments/qblox/mock/cluster.py
@@ -1,6 +1,31 @@
 __all__ = []
 
 
+class MockModule:
+    def __init__(self, slot: int) -> None:
+        self.slot_idx = slot
+
+    def present(self) -> bool:
+        return True
+
+    @property
+    def is_qrm_type(self) -> bool:
+        return True
+
+    @property
+    def sequencers(self) -> list:
+        return [None] * 20
+
+    def disconnect_outputs(self) -> None:
+        pass
+
+    def scope_acq_trigger_mode_path0(self, mode: str) -> None:
+        pass
+
+    def scope_acq_trigger_mode_path1(self, mode: str) -> None:
+        pass
+
+
 class MockCluster:
     def __init__(self, *args, **kwargs) -> None:
         self.args = args
@@ -8,8 +33,8 @@ class MockCluster:
         self.resets: int = 0
 
     @property
-    def modules(self):
-        return []
+    def modules(self) -> list:
+        return [MockModule(slot) for slot in range(21)]
 
-    def reset(self):
+    def reset(self) -> None:
         self.resets += 1
