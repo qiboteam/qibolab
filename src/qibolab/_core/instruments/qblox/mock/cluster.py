83160f63ddf12f0cff34314bac4ce093ef1e8cbd
Alessandro Candido <candido.ale@gmail.com>
2025-01-06 20:33:28 +0100

    fix: Add required cluster methods for mock one

--- src/qibolab/_core/instruments/qblox/mock/cluster.py
+++ src/qibolab/_core/instruments/qblox/mock/cluster.py
@@ -5,3 +5,11 @@ class MockCluster:
     def __init__(self, *args, **kwargs) -> None:
         self.args = args
         self.kwargs = kwargs
+        self.resets: int = 0
+
+    @property
+    def modules(self):
+        return []
+
+    def reset(self):
+        self.resets += 1
