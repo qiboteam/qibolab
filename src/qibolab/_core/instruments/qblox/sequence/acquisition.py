20ea994ec130d792f23c94801ec447363c8a4223
Alessandro Candido <candido.ale@gmail.com>
2025-01-24 17:32:35 +0100

    refactor: Move non-sequence-related acquisition code

--- src/qibolab/_core/instruments/qblox/sequence/acquisition.py
+++ src/qibolab/_core/instruments/qblox/sequence/acquisition.py
@@ -1,12 +1,7 @@
-from typing import Optional, TypedDict
-
 from qibolab._core import pulses
-from qibolab._core.identifier import ChannelId, Result
-from qibolab._core.pulses.pulse import PulseId
 from qibolab._core.sequence import PulseSequence
 from qibolab._core.serialize import Model
 
-from ..identifiers import SequencerId, SlotId
 from .waveforms import Waveform
 
 __all__ = []
@@ -31,55 +26,3 @@ def acquisitions(sequence: PulseSequence, num_bins: int) -> Acquisitions:
             p for _, p in sequence if isinstance(p, pulses.Acquisition)
         )
     }
-
-
-IndividualScope = TypedDict(
-    "IndividualScope",
-    {"data": list[float], "out-of-range": list[bool], "avg_cnt": list[bool]},
-)
-
-
-class ScopeData(TypedDict):
-    path0: IndividualScope
-    path1: IndividualScope
-
-
-class Integration(TypedDict):
-    path0: list[float]
-    path1: list[float]
-
-
-class BinData(TypedDict):
-    integration: Integration
-    threshold: list[int]
-    valid: list
-    avg_cnt: list[int]
-
-
-class Data(TypedDict):
-    scope: ScopeData
-    bins: BinData
-
-
-class IndexedData(TypedDict):
-    index: int
-    acquisition: Data
-
-
-AcquiredData = dict[MeasureId, IndexedData]
-
-
-def integration_lengths(
-    weighted: dict[MeasureId, Optional[int]],
-    defaults: dict[tuple[SlotId, SequencerId], int],
-    locations: dict[MeasureId, tuple[SlotId, SequencerId]],
-) -> dict[MeasureId, int]:
-    return {
-        k: v if v is not None else defaults[locations[k]] for k, v in weighted.items()
-    }
-
-
-def extract(
-    acquisitions: dict[ChannelId, AcquiredData], lenghts: dict[MeasureId, int]
-) -> dict[PulseId, Result]:
-    return {}
