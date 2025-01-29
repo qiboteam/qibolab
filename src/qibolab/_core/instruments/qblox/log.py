6b76d0e6756b4e06ad47f7c7ec91476ef5f3db71
Alessandro Candido <candido.ale@gmail.com>
2025-01-29 11:18:24 +0100

    fix: Fix leftover imports

--- src/qibolab/_core/instruments/qblox/log.py
+++ src/qibolab/_core/instruments/qblox/log.py
@@ -6,9 +6,9 @@ from qblox_instruments import Cluster
 from qibolab._core.components.configs import Configs, LogConfig
 from qibolab._core.identifier import ChannelId
 
-from .config import SeqeuencerMap
-from .sequence import Sequence
-from .sequence.acquisition import AcquiredData
+from .identifiers import SequencerMap
+from .results import AcquiredData
+from .sequence import Q1Sequence
 
 __all__ = []
 
@@ -32,12 +32,12 @@ class Logger:
             return lambda *args, **kwargs: None
         return super().__getattribute__(name)
 
-    def sequences(self, sequences: dict[ChannelId, Sequence]):
+    def sequences(self, sequences: dict[ChannelId, Q1Sequence]):
         assert self.path is not None
         for ch, seq in sequences.items():
             (self.path / _sanitize(f"{ch}.json")).write_text(seq.model_dump_json())
 
-    def status(self, cluster: Cluster, sequencers: SeqeuencerMap):
+    def status(self, cluster: Cluster, sequencers: SequencerMap):
         assert self.path is not None
         status = self.path / "status"
         status.mkdir(exist_ok=True)
