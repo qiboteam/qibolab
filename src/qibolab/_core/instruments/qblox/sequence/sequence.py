89ba71b2e27bfb3a94dc81bffefc8c7e4efaa3c6
Alessandro Candido <candido.ale@gmail.com>
2025-02-10 13:24:41 +0100

    fix: Consume sampling rate top-level
    Instead of passing it down many levels till instructions instantiation

--- src/qibolab/_core/instruments/qblox/sequence/sequence.py
+++ src/qibolab/_core/instruments/qblox/sequence/sequence.py
@@ -1,3 +1,4 @@
+from collections.abc import Iterable
 from typing import Annotated, Optional
 
 import numpy as np
@@ -5,10 +6,10 @@ from pydantic import PlainSerializer, PlainValidator
 
 from qibolab._core.execution_parameters import ExecutionParameters
 from qibolab._core.identifier import ChannelId
-from qibolab._core.pulses import PulseLike
+from qibolab._core.pulses import Align, PulseLike
 from qibolab._core.sequence import PulseSequence
 from qibolab._core.serialize import Model
-from qibolab._core.sweeper import ParallelSweepers
+from qibolab._core.sweeper import ParallelSweepers, Parameter
 
 from ..q1asm import Program, parse
 from .acquisition import Acquisitions, MeasureId, Weight, Weights, acquisitions
@@ -20,6 +21,49 @@ __all__ = ["Q1Sequence"]
 
 def _weight_len(w: Optional[Weight]) -> Optional[int]:
     return len(w.data) if w is not None else None
+
+
+def _apply_sampling_rate(
+    sequence: Iterable[PulseLike],
+    sweepers: list[ParallelSweepers],
+    sampling_rate: float,
+) -> tuple[list[PulseLike], list[ParallelSweepers]]:
+    sequence_ = [
+        p.model_copy(
+            update={"duration": p.duration * sampling_rate}
+            if not isinstance(p, Align)
+            # TODO: replace with `hasattr(p, "duration")`
+            # but it's not passing the type checker
+            # https://github.com/microsoft/pylance-release/issues/2237
+            else {}
+        )
+        for p in sequence
+    ]
+    sweepers_ = [
+        [
+            s.model_copy(
+                update=(
+                    {
+                        "range": tuple(t * sampling_rate for t in s.range),
+                    }
+                    if s.range is not None
+                    else {}
+                )
+                | (
+                    {
+                        "values": sampling_rate * s.values,
+                    }
+                    if s.values is not None
+                    else {}
+                )
+            )
+            if s.parameter is Parameter.duration
+            else s
+            for s in parsweep
+        ]
+        for parsweep in sweepers
+    ]
+    return (sequence_, sweepers_)
 
 
 class Q1Sequence(Model):
@@ -42,6 +86,7 @@ class Q1Sequence(Model):
         channel: ChannelId,
     ):
         waveforms_ = waveforms(sequence, sampling_rate)
+        sequence, sweepers = _apply_sampling_rate(sequence, sweepers, sampling_rate)
         acquisitions_ = acquisitions(
             sequence, np.prod(options.bins(sweepers), dtype=int)
         )
@@ -55,7 +100,6 @@ class Q1Sequence(Model):
                 acquisitions_,
                 options,
                 sweepers,
-                sampling_rate,
                 channel,
             ),
         )
