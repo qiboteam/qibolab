8f5cbb0aab18477685f111b7a0d9f4fb1adab422
Alessandro Candido <candido.ale@gmail.com>
2025-02-18 15:37:19 +0100

    refactor: Lift los retrieval
    Preparation to pass them down to sweepers

--- src/qibolab/_core/instruments/qblox/config.py
+++ src/qibolab/_core/instruments/qblox/config.py
@@ -5,7 +5,7 @@ import numpy as np
 from qblox_instruments.qcodes_drivers.module import Module
 from qblox_instruments.qcodes_drivers.sequencer import Sequencer
 
-from qibolab._core.components.channels import AcquisitionChannel, Channel, IqChannel
+from qibolab._core.components.channels import Channel, IqChannel
 from qibolab._core.components.configs import (
     AcquisitionConfig,
     Configs,
@@ -89,49 +89,10 @@ class PortAddress(Model):
         return f"{self.direction}{channels}"
 
 
-def _iqout(id_: ChannelId, channel: Channel) -> Optional[ChannelId]:
-    """Extract associated IQ output channel.
-
-    This is the identity for each IQ output channel identifier, while it retrieves the
-    associated probe channel for acquisition ones, and :obj:`None` for any other one
-    (essentially, non-RF channels).
-    """
-    return (
-        id_
-        if isinstance(channel, IqChannel)
-        else (
-            channel.probe
-            if isinstance(channel, AcquisitionChannel) and channel.probe is not None
-            else None
-        )
-    )
-
-
-def _los(
-    mod_channels: set[ChannelId], channels: dict[ChannelId, Channel]
-) -> set[tuple[ChannelId, str]]:
-    """Extract all LOs of a certain module.
-
-    The result contains the associated channel, since required to
-    address the LO through the API. While the LO identifier is used to
-    retrieve the configuration.
-    """
-    return {
-        (iq, lo)
-        for iq, lo in (
-            (iq, cast(IqChannel, channels[iq]).lo)
-            for iq in (_iqout(ch, channels[ch]) for ch in mod_channels)
-            if iq is not None
-        )
-        if lo is not None
-    }
-
-
 def module(
     mod: Module,
-    mod_channels: set[ChannelId],
     channels: dict[ChannelId, Channel],
-    configs: Configs,
+    los: dict[ChannelId, OscillatorConfig],
 ):
     """Configure module-wide settings."""
     # map sequencers to specific outputs (but first disable all sequencer connections)
@@ -144,10 +105,10 @@ def module(
         mod.scope_acq_trigger_mode_path1("sequencer")
 
     # set lo frequencies
-    for iq, lo in _los(mod_channels, channels):
+    for iq, lo in los.items():
         n = PortAddress.from_path(channels[iq].path).ports[0] - 1
         attr = f"out{n}_in{n}_lo_freq" if mod.is_qrm_type else f"out{n}_lo_freq"
-        getattr(mod, attr)(cast(OscillatorConfig, configs[lo]).frequency)
+        getattr(mod, attr)(lo.frequency)
 
 
 def _integration_length(sequence: Q1Sequence) -> Optional[int]:
@@ -188,12 +149,14 @@ def sequencer(
         if length is not None:
             seq.integration_length_acq(length)
         # discrimination
-        seq.thresholded_acq_rotation(np.degrees(config.iq_angle % (2 * np.pi)))
-        seq.thresholded_acq_threshold(config.threshold)
+        if config.iq_angle is not None:
+            seq.thresholded_acq_rotation(np.degrees(config.iq_angle % (2 * np.pi)))
+        if config.threshold is not None:
+            seq.thresholded_acq_threshold(config.threshold)
         # demodulation
         seq.demod_en_acq(acquisition is not AcquisitionType.RAW)
 
-    probe = _iqout(channel_id, channels[channel_id])
+    probe = channels[channel_id].iqout(channel_id)
     if probe is not None:
         freq = cast(IqConfig, configs[probe]).frequency
         lo = cast(IqChannel, channels[probe]).lo
