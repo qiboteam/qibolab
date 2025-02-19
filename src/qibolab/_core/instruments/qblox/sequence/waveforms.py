9db95ecaa8f2b3290c835cf8723ec7a3f9aa6e49
Alessandro Candido <candido.ale@gmail.com>
2025-02-19 14:55:47 +0100

    feat: Introduce stretched waveforms generation

--- src/qibolab/_core/instruments/qblox/sequence/waveforms.py
+++ src/qibolab/_core/instruments/qblox/sequence/waveforms.py
@@ -1,10 +1,11 @@
 from collections.abc import Iterable
-from typing import Annotated, Union
+from typing import Annotated, Optional, Union
 
 from pydantic import AfterValidator
 
 from qibolab._core.pulses import Pulse, PulseId, PulseLike, Readout
 from qibolab._core.serialize import ArrayList, Model
+from qibolab._core.sweeper import Sweeper
 
 __all__ = []
 
@@ -29,34 +30,59 @@ class WaveformSpec(Model):
 Waveforms = dict[ComponentId, Waveform]
 
 
+def _pulse(event: Union[Pulse, Readout]) -> Pulse:
+    return event.probe if isinstance(event, Readout) else event
+
+
 def waveforms(
     sequence: Iterable[PulseLike],
     sampling_rate: float,
     amplitude_swept: set[PulseId],
-    duration_swept: set[PulseId],
+    duration_swept: dict[PulseId, Sweeper],
 ) -> dict[ComponentId, WaveformSpec]:
-    def waveform(pulse: Pulse, component: str) -> WaveformSpec:
+    def _waveform(
+        pulse: Pulse, component: str, duration: Optional[float] = None
+    ) -> WaveformSpec:
         update = {"amplitude": 1.0} if pulse.id in amplitude_swept else {}
         return WaveformSpec(
             waveform=Waveform(
                 data=getattr(pulse.model_copy(update=update), component)(sampling_rate),
                 index=0,
             ),
-            duration=int(pulse.duration),
+            duration=int(pulse.duration if duration is None else duration),
         )
 
-    def pulse_(event: Union[Pulse, Readout]) -> Pulse:
-        return event.probe if isinstance(event, Readout) else event
-
+    events = {pulse.id: pulse for pulse in sequence}
     indexless = {
         k: v
         for d in (
             {
-                (pulse_uid(pulse_(event)), 0): waveform(pulse_(event), "i"),
-                (pulse_uid(pulse_(event)), 1): waveform(pulse_(event), "q"),
+                (pulse_uid(pulse), 0): _waveform(pulse, "i"),
+                (pulse_uid(pulse), 1): _waveform(pulse, "q"),
             }
-            for event in sequence
-            if isinstance(event, (Pulse, Readout))
+            for pulse in (
+                _pulse(event)
+                for event in events.values()
+                if isinstance(event, (Pulse, Readout))
+            )
+            if pulse not in duration_swept
+        )
+        for k, v in d.items()
+    } | {
+        k: v
+        for d in (
+            {
+                (pulse_uid(pulse), 2 * i): _waveform(pulse, "i", duration),
+                (pulse_uid(pulse), 2 * i + 1): _waveform(pulse, "q", duration),
+            }
+            for pulse, sweep in (
+                (_pulse(event), duration_swept[id_])
+                for id_, event in ((id_, events[id_]) for id_ in duration_swept)
+                if isinstance(event, (Pulse, Readout))
+            )
+            for i, duration in (
+                (i, sweep.irange[0] + sweep.irange[2] * i) for i in range(len(sweep))
+            )
         )
         for k, v in d.items()
     }
