fa1b22e26d91b4d341220c3a71c5c3020d87128b
Stavros Efthymiou <35475381+stavros11@users.noreply.github.com>
2025-02-24 17:49:39 +0400

    refactor: use fixture and monkeypatch for dummy hardware

--- tests/dummy_hardware/platform.py
+++ tests/dummy_hardware/platform.py
@@ -1,16 +1,8 @@
 """Dummy platform to test loading via ``Hardware`` object."""
 
-from pathlib import Path
-
-from qibolab import Hardware, initialize_parameters
+from qibolab import Hardware
 from qibolab._core.dummy.platform import create_dummy_hardware
-from qibolab._core.platform.platform import PARAMETERS
 
 
 def create() -> Hardware:
-    hardware = create_dummy_hardware()
-    parameters = initialize_parameters(hardware=hardware)
-    (Path(__file__).parent / PARAMETERS).write_text(
-        parameters.model_dump_json(indent=4)
-    )
-    return hardware
+    return create_dummy_hardware()
