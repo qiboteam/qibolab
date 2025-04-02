834a9afc9e7c97bbb1fd755b143f6a3c5cf15adf
Andrea Pasquale <andreapasquale97@gmail.com>
2025-04-02 17:28:30 +0200

    test: Basic tests on integration mode and single sweeper

--- src/qibolab/_core/instruments/emulator/emulator.py
+++ src/qibolab/_core/instruments/emulator/emulator.py
@@ -35,7 +35,6 @@ from qibolab._core.sweeper import ParallelSweepers
 from .hamiltonians import (
     HamiltonianConfig,
     Modulated,
-    ModulatedVirtualZ,
     channel_operator,
     waveform,
 )
@@ -125,7 +124,7 @@ class EmulatorController(Controller):
                 if isinstance(ev, (Acquisition, Readout)):
                     acq[ev.id] = time
                 if isinstance(ev, Align):
-                    raise ValueError("Align not support in emulator.")
+                    raise ValueError("Align not supported in emulator.")
                 time += ev.duration
         return acq
 
@@ -295,12 +294,10 @@ def channel_time(waveforms: Iterable[Modulated]) -> Callable[[float], float]:
             if cumulative_time <= t < cumulative_time + pulse_duration:
                 relative_time = t - cumulative_time
                 index = int(relative_time)  # TODO: pass sampling rate
-                if isinstance(pulse, ModulatedVirtualZ):
-                    continue
                 return pulse(t, index, cumulative_phase)
             cumulative_time += pulse_duration
             cumulative_phase += pulse_phase
-        return 0
+        return 0  # pragma: no cover
 
     return time
 
---
+++ tests/instruments/emulator/test_emulator.py
@@ -0,0 +1,55 @@
+"""Testing emulator basic functionalities."""
+
+import numpy as np
+import pytest
+
+from qibolab._core.execution_parameters import AcquisitionType, AveragingMode
+from qibolab._core.pulses.pulse import Align
+from qibolab._core.sweeper import Parameter, Sweeper
+
+NSHOTS = 1000
+"""Number of shots to be used for tests."""
+
+
+def test_integration_mode(platform):
+    seq = platform.natives.single_qubit[0].MZ()
+    acq_handle = list(seq.channel(platform.qubits[0].acquisition))[-1].id
+    result = platform.execute(
+        [seq],
+        nshots=1000,
+        acquisition_type=AcquisitionType.INTEGRATION,
+        averaging_mode=AveragingMode.SINGLESHOT,
+    )
+
+    assert result[acq_handle].shape == (1000, 2)
+    np.testing.assert_allclose(result[acq_handle][:, 1].mean(), 0, atol=1e-2)
+
+    result = platform.execute(
+        [seq],
+        nshots=1000,
+        acquisition_type=AcquisitionType.INTEGRATION,
+        averaging_mode=AveragingMode.CYCLIC,
+    )
+
+    assert result[acq_handle].shape == (2,)
+    np.testing.assert_allclose(result[acq_handle][1], 0, atol=1e-2)
+
+
+def test_align_fail(platform):
+    seq = platform.natives.single_qubit[0].MZ()
+    seq += (platform.qubits[0].drive, Align())
+
+    with pytest.raises(ValueError):
+        platform.execute([seq])
+
+
+def test_sweepers(platform):
+    q0 = platform.natives.single_qubit[0]
+    seq = q0.RX() | q0.MZ()
+    ch, pulse = seq[0]
+    sweeper = Sweeper(
+        parameter=Parameter.amplitude, values=np.array([0, 1]), pulses=[pulse]
+    )
+    acq_handle = list(seq.channel(platform.qubits[0].acquisition))[-1].id
+    res = platform.execute([seq], [[sweeper]], nshots=100)
+    assert res[acq_handle].shape == (100, 2)
