0868fe33c94a33290e03634524f8c4194c0a451f
Andrea Pasquale <andreapasquale97@gmail.com>
2025-04-02 12:24:10 +0200

    test: More tests and adding qubit platform

--- tests/instruments/emulator/test_sequence.py
+++ tests/instruments/emulator/test_sequence.py
@@ -1,3 +1,5 @@
+"""Testing basic pulse sequences with emulator platforms."""
+
 import pytest
 
 from qibolab import Platform
@@ -9,3 +11,29 @@ def test_ground_state(platform: Platform):
     acq_handle = list(seq.channel(platform.qubits[0].acquisition))[-1].id
     res = platform.execute([seq], nshots=1e4)[acq_handle]
     assert pytest.approx(res.mean(), abs=1e-1) == 0
+
+
+def test_superposition_state(platform: Platform):
+    q0 = platform.natives.single_qubit[0]
+    seq = q0.RX90() | q0.MZ()
+    acq_handle = list(seq.channel(platform.qubits[0].acquisition))[-1].id
+    res = platform.execute([seq], nshots=1e4)[acq_handle]
+    assert pytest.approx(res.mean(), abs=1e-1) == 0.5
+
+
+def test_excited_state(platform: Platform):
+    q0 = platform.natives.single_qubit[0]
+    seq = q0.RX() | q0.MZ()
+    acq_handle = list(seq.channel(platform.qubits[0].acquisition))[-1].id
+    res = platform.execute([seq], nshots=1e4)[acq_handle]
+    assert pytest.approx(res.mean(), abs=1e-1) == 1
+
+
+def test_second_excited_state(platform: Platform):
+    q0 = platform.natives.single_qubit[0]
+    if q0.RX12 is None:
+        pytest.skip(f"Skipping due to missing RX12 for platform {platform}.")
+    seq = q0.RX() | q0.RX12() | q0.MZ()
+    acq_handle = list(seq.channel(platform.qubits[0].acquisition))[-1].id
+    res = platform.execute([seq], nshots=1e4)[acq_handle]
+    assert pytest.approx(res.mean(), abs=1e-1) == 2
