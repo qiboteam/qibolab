0b94a0ba587912acf3a6de018e0147294dc0caab
Stavros Efthymiou <35475381+stavros11@users.noreply.github.com>
2025-05-29 16:19:51 +0400

    fix: add mixer calibration path

--- src/qibolab/_core/instruments/qm/controller.py
+++ src/qibolab/_core/instruments/qm/controller.py
@@ -259,6 +259,7 @@ class QmController(Controller):
             port=int(port),
             credentials=credentials,
             cluster_name=self.cluster_name,
+            octave_calibration_db_path=self._calibration_path,
         )
 
     def disconnect(self):
