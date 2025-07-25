5cdbb3f44cc8a171b46d4687f31de9b77c1fcbac
Andrea Pasquale <andreapasquale97@gmail.com>
2025-07-25 10:07:38 +0200

    fix: Avoid shadowing

--- src/qibolab/_core/instruments/emulator/emulator.py
+++ src/qibolab/_core/instruments/emulator/emulator.py
@@ -62,7 +62,7 @@ class EmulatorController(Controller):
         sweepers: list[ParallelSweepers],
     ) -> dict[int, Result]:
         # convert align to delays
-        sequences = (seq.align_to_delays() for seq in sequences)
+        sequences_ = (seq.align_to_delays() for seq in sequences)
         # just merge the results of multiple executions in a single dictionary
         return reduce(
             or_,
@@ -74,7 +74,7 @@ class EmulatorController(Controller):
                     cast(HamiltonianConfig, configs["hamiltonian"]),
                     options,
                 )
-                for sequence in sequences
+                for sequence in sequences_
             ),
         )
 
