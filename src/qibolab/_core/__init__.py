dd5e42bea63a89729310a8ee27055259c7b35e9b
Alessandro Candido <candido.ale@gmail.com>
2025-11-20 11:57:57 +0900

    fix: Drop qibo logger usage in dummy
    Still to cut the link

--- src/qibolab/_core/__init__.py
+++ src/qibolab/_core/__init__.py
@@ -1,6 +1,5 @@
 from . import (
     components,
-    dummy,
     execution_parameters,
     parameters,
     platform,
@@ -10,7 +9,6 @@ from . import (
     sweeper,
 )
 from .components import *
-from .dummy import *
 from .execution_parameters import *
 from .parameters import *
 from .platform import *
@@ -21,7 +19,6 @@ from .sweeper import *
 
 __all__ = []
 __all__ += components.__all__
-__all__ += dummy.__all__
 __all__ += execution_parameters.__all__
 __all__ += parameters.__all__
 __all__ += platform.__all__
