09e8625d76308d33bf8be26e1aa98fe25d913803
jykhoo1987 <jyogaurora@gmail.com>
2026-01-25 02:48:43 +0800

    cudaq engine update, dissipation disabled due to kernel crash bug; added test platform split-transmons-cudaq; slight refactor of base emulator; added qutip-cuquantum controller

--- src/qibolab/_core/instruments/emulator/engine/qutip.py
+++ src/qibolab/_core/instruments/emulator/engine/qutip.py
@@ -1,20 +1,22 @@
 from functools import cached_property
 from typing import Union
+from numpy.typing import NDArray
 
-from .abstract import Operator, OperatorEvolution, SimulationEngine
+from .abstract import Operator, OperatorEvolution, SimulationEngine, EvolutionResult
 
-__all__ = ["QutipEngine"]
+__all__ = ["QutipEngine", "QutipCuquantumEngine"]
 
 
 class QutipEngine(SimulationEngine):
     """Qutip simulation engine."""
+    has_flipped_index: bool = False
 
     @cached_property
     def engine(self):
         """Return the qutip engine."""
         # TODO: maybe it can be improved
         import qutip as qt
-
+        
         return qt
 
     def evolve(
@@ -24,6 +26,7 @@ class QutipEngine(SimulationEngine):
         time: list[float],
         time_hamiltonian: OperatorEvolution = None,
         collapse_operators: list[Operator] = None,
+        dimensions: dict = None,
         **kwargs,
     ):
         """Evolve the system."""
@@ -33,15 +36,15 @@ class QutipEngine(SimulationEngine):
             hamiltonian, initial_state, time, collapse_operators, **kwargs
         )
 
-    def create(self, n: int) -> Operator:
+    def create(self, n: int, **kwargs) -> Operator:
         """Create operator for n levels system."""
         return self.engine.create(n)
 
-    def destroy(self, n: int) -> Operator:
+    def destroy(self, n: int, **kwargs) -> Operator:
         """Destroy operator for n levels system."""
         return self.engine.destroy(n)
 
-    def identity(self, n: int) -> Operator:
+    def identity(self, n: int, **kwargs) -> Operator:
         """Identity operator for n levels system."""
         return self.engine.qeye(n)
 
@@ -53,6 +56,36 @@ class QutipEngine(SimulationEngine):
         """Expand operator in larger Hilbert space."""
         return self.engine.expand_operator(op, targets, dims)
 
-    def basis(self, dim: int, state: int) -> Operator:
+    def basis(self, dim: list, state: list) -> Operator:
         """Basis operator for n levels system."""
         return self.engine.basis(dimensions=dim, n=state)
+
+    def get_state_dm(self, state: Operator, **kwargs) -> NDArray:
+        if state.type=='ket':
+            state = self.engine.ket2dm(state)
+        return state.full()
+
+    def get_evolution_states(self, results: EvolutionResult) -> list:
+        return results.states
+
+    def relaxation_op(self, transition: list, dim: int, **kwargs) -> Operator:
+        return self.engine.basis(dim, transition[0]) * self.engine.basis(dim, transition[1]).dag()
+
+    def dephasing_op(self, pair: list, dim: int, **kwargs) -> Operator:
+        return self.engine.basis(dim, pair[0]) * self.engine.basis(dim, pair[0]).dag() - self.engine.basis(dim, pair[1]) * self.engine.basis(dim, pair[1]).dag()
+
+
+class QutipCuquantumEngine(QutipEngine):
+    """Qutip simulation engine using cuquantum."""
+    
+    @cached_property
+    def engine(self):
+        """Return the qutip engine using cuquantum."""
+        import qutip as qt
+
+        import qutip_cuquantum
+        from cuquantum.densitymat import WorkStream
+        stream = WorkStream()
+        qutip_cuquantum.set_as_default(stream)
+        
+        return qt
