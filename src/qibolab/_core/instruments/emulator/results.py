6056a5c74f2ce8270992987fbe4e93a8d9debd3c
lballerio <lorenzo@ballerio.com>
2026-02-02 06:49:57 +0000

    restoring results in emulator

--- src/qibolab/_core/instruments/emulator/results.py
+++ src/qibolab/_core/instruments/emulator/results.py
@@ -51,19 +51,33 @@ def shots(probabilities: NDArray, nshots: int) -> NDArray:
     return np.moveaxis(shots, -1, 0)
 
 
+# def calculate_probabilities_from_density_matrix(
+#     states: NDArray, subsystems: Iterable[QubitId], nsubsystems: int, d: int
+# ) -> NDArray:
+#     """Compute probabilities from density matrix."""
+#     states_ = np.reshape(states, states.shape[:-2] + 2 * nsubsystems * (d,))
+#     marginal = np.einsum(
+#         states_,
+#         # TODO: the `np.array()` wrapping call is only needed because of NumPy's type
+#         # annotation - in practice, it also works without
+#         np.array([...] + list(range(nsubsystems)) * 2),
+#         np.array([...] + list(subsystems)),
+#     )
+#     return np.abs(marginal).reshape((*states.shape[:-2], -1))
+
+
 def calculate_probabilities_from_density_matrix(
-    states: NDArray, subsystems: Iterable[QubitId], nsubsystems: int, d: int
+    states: NDArray
 ) -> NDArray:
     """Compute probabilities from density matrix."""
-    states_ = np.reshape(states, states.shape[:-2] + 2 * nsubsystems * (d,))
-    marginal = np.einsum(
-        states_,
+    diag = np.einsum(
+        states,
         # TODO: the `np.array()` wrapping call is only needed because of NumPy's type
         # annotation - in practice, it also works without
-        np.array([...] + list(range(nsubsystems)) * 2),
-        np.array([...] + list(subsystems)),
+        np.array([...] + [0, 0]),
+        np.array([...] + [0]),
     )
-    return np.abs(marginal).reshape((*states.shape[:-2], -1))
+    return np.abs(diag)
 
 
 def acquisitions(sequence: PulseSequence) -> dict[PulseId, float]:
@@ -116,34 +130,98 @@ def results(
     result for the execution of this single sequence, thus suitable
     to be returned as is.
     """
+
     probabilities = calculate_probabilities_from_density_matrix(
         states,
-        range(hamiltonian.nqubits),
-        hamiltonian.nqubits,
-        hamiltonian.transmon_levels,
     )
+    # HERE I COULD RECOVER THE CORRECT SOLUTION ONLY FOR 'fixed-frequency-qutrits' PLATFORM
+    # STILL NOT WORKING FOR 'qutrits' PLATFORM
+    # t = datetime.datetime.now().strftime("%H:%M:%S")
+    # np.savez(f'{t}_raw_qutip_evolution.npz', np.stack(results))
+
     assert options.nshots is not None
     sampled = shots(np.moveaxis(probabilities, -2, 0), options.nshots)
     # move measurements dimension to the front, getting ready for extraction
     measurements = np.moveaxis(sampled, 1, 0)
 
+    probabilities = np.moveaxis(probabilities,0,-1)
+    states_idx = [*range(probabilities.shape[1])]
     results = {}
+    qutip_res = []
     # introduce cached measurements to avoid losing correlations
     cache_measurements = {}
-    for (ro_id, sample), meas in zip(acquisitions(sequence).items(), measurements):
+    # for (ro_id, sample), meas in zip(acquisitions(sequence).items(), measurements):
+    for (ro_id, sample), probs in zip(acquisitions(sequence).items(), probabilities):
         i = index(sequence.pulse_channels(ro_id)[0], hamiltonian)
-        cache_measurements.setdefault(sample, meas)
-        res = np.stack(np.unravel_index(cache_measurements[sample], hamiltonian.dims))[
-            i
-        ]
+        # cache_measurements.setdefault(sample, meas)
 
-        if options.acquisition_type is AcquisitionType.INTEGRATION:
-            res = np.stack((res, np.zeros_like(res)), axis=-1)
-            res = np.random.normal(res, scale=0.001)
+        # res = np.stack(
+        #     np.unravel_index(cache_measurements[sample], hamiltonian.reduced_dims)
+        # )[i]
 
-        if options.averaging_mode == AveragingMode.CYCLIC:
-            res = np.mean(res, axis=0)
+        states_computational_idx = np.stack(
+            np.unravel_index(states_idx, hamiltonian.reduced_dims)
+        )
+        
+        res = np.sum(probabilities[i,states_computational_idx[i]==1,:],axis=0)
+        # if options.acquisition_type is AcquisitionType.INTEGRATION:
+        #     res = np.stack((res, np.zeros_like(res)), axis=-1)
+        #     res = np.random.normal(res, scale=0.001)
+
+        # if options.averaging_mode == AveragingMode.CYCLIC:
+        #     res = np.mean(res, axis=0)
 
         results[ro_id] = res
+        #qutip_res.append(res)
 
+    # HERE I COULD RECOVER THE CORRECT SOLUTION ONLY FOR 'fixed-frequency-qutrits' PLATFORM
+    # STILL NOT WORKING FOR 'qutrits' PLATFORM
+    # t = datetime.datetime.now().strftime("%H:%M:%S")
+    # np.savez(f'{t}_probs_qutip_evolution.npz', np.stack(qutip_res))
+        
     return results
+
+
+# def results(
+#     states: NDArray,
+#     sequence: PulseSequence,
+#     hamiltonian: HamiltonianConfig,
+#     options: ExecutionParameters,
+# ) -> dict[int, Result]:
+#     """Collect results for a single pulse sequence.
+
+#     The dictionary returned is already compliant with the expected
+#     result for the execution of this single sequence, thus suitable
+#     to be returned as is.
+#     """
+#     probabilities = calculate_probabilities_from_density_matrix(
+#         states,
+#         range(hamiltonian.nqubits),
+#         hamiltonian.nqubits,
+#         hamiltonian.transmon_levels,
+#     )
+#     assert options.nshots is not None
+#     sampled = shots(np.moveaxis(probabilities, -2, 0), options.nshots)
+#     # move measurements dimension to the front, getting ready for extraction
+#     measurements = np.moveaxis(sampled, 1, 0)
+
+#     results = {}
+#     # introduce cached measurements to avoid losing correlations
+#     cache_measurements = {}
+#     for (ro_id, sample), meas in zip(acquisitions(sequence).items(), measurements):
+#         i = index(sequence.pulse_channels(ro_id)[0], hamiltonian)
+#         cache_measurements.setdefault(sample, meas)
+#         res = np.stack(np.unravel_index(cache_measurements[sample], hamiltonian.dims))[
+#             i
+#         ]
+
+#         if options.acquisition_type is AcquisitionType.INTEGRATION:
+#             res = np.stack((res, np.zeros_like(res)), axis=-1)
+#             res = np.random.normal(res, scale=0.001)
+
+#         if options.averaging_mode == AveragingMode.CYCLIC:
+#             res = np.mean(res, axis=0)
+
+#         results[ro_id] = res
+
+#     return results
