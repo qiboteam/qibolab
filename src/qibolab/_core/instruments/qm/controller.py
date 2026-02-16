1674ad6ff659ef139c2cffe0fa4074a878f5d0c4
lballerio <lorenzo@ballerio.com>
2026-02-16 12:16:51 +0000

    adding debug mode for qm controller - used for solving QM error (together with QM people)

--- src/qibolab/_core/instruments/qm/controller.py
+++ src/qibolab/_core/instruments/qm/controller.py
@@ -260,6 +260,7 @@ class QmController(Controller):
             credentials=credentials,
             cluster_name=self.cluster_name,
             octave_calibration_db_path=self._calibration_path,
+            log_level="DEBUG"
         )
 
     def disconnect(self):
