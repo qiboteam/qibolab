87ca9a5b22e0ee7bcc2f28494781ba18d85b2c09
lballerio <lorenzo@ballerio.com>
2026-03-09 15:04:00 +0000

    Adding first draft for emulator documentation
    Removing gaussian noise for SINGLESHOT averaging mode because it does not work.

--- src/qibolab/_core/instruments/emulator/emulator.py
+++ src/qibolab/_core/instruments/emulator/emulator.py
@@ -43,11 +43,6 @@ SAMPLING_INTERVAL = 1 / (2 * NYQUIST_FREQUENCY)
 
 
 
-# DEBUG
-import datetime
-import rich
-
-
 __all__ = ["EmulatorController"]
 
 
@@ -159,11 +154,6 @@ class EmulatorController(Controller):
 
             # append new slice for the current parallel value
             results.append(self._sweep(sequence, configs, sweepers[1:], updates))
-
-        # HERE I COULD RECOVER THE CORRECT SOLUTION ONLY FOR 'fixed-frequency-qutrits' PLATFORM
-        # STILL NOT WORKING FOR 'qutrits' PLATFORM
-        # t = datetime.datetime.now().strftime("%H:%M:%S")
-        # np.savez(f'{t}_raw_qutip_evolution.npz', np.stack(results))
 
         # stack all slices in a single array, along the current outermost dimension
         return np.stack(results)
