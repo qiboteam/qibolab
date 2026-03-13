fec1b03e329c2b927ff25afe3fc330ad3d8e3add
RoyStegeman <roystegeman@live.nl>
2026-03-13 12:56:57 +0000

    remove unused batch() function

--- tests/test_unrolling.py
+++ tests/test_unrolling.py
@@ -1,12 +1,10 @@
 """Tests ``unrolling.py``."""
 
-import pytest
-
 from qibolab._core.platform import Platform
 from qibolab._core.pulses import Delay, Drag, Pulse, Rectangular
 from qibolab._core.pulses.pulse import Acquisition
 from qibolab._core.sequence import PulseSequence
-from qibolab._core.unrolling import Bounds, batch, unroll_sequences
+from qibolab._core.unrolling import Bounds, unroll_sequences
 
 
 def test_bounds_update():
@@ -67,44 +65,6 @@ def test_bounds_comparison():
 
     assert bounds1 > bounds2
     assert not bounds2 < bounds1
-
-
-@pytest.mark.parametrize(
-    "bounds",
-    [
-        Bounds(waveforms=150, readout=int(10e6), instructions=int(10e6)),
-        Bounds(waveforms=int(10e6), readout=10, instructions=int(10e6)),
-        Bounds(waveforms=int(10e6), readout=int(10e6), instructions=20),
-    ],
-)
-def test_batch(bounds):
-    ps = PulseSequence.load(
-        [
-            (
-                "ch3/drive",
-                Pulse(duration=40, amplitude=0.9, envelope=Drag(rel_sigma=0.2, beta=1)),
-            ),
-            (
-                "ch2/drive",
-                Pulse(duration=40, amplitude=0.9, envelope=Drag(rel_sigma=0.2, beta=1)),
-            ),
-            (
-                "ch1/drive",
-                Pulse(duration=40, amplitude=0.9, envelope=Drag(rel_sigma=0.2, beta=1)),
-            ),
-            ("ch3/probe", Pulse(duration=1000, amplitude=0.9, envelope=Rectangular())),
-            ("ch3/acquisition", Acquisition(duration=1000)),
-            ("ch2/probe", Pulse(duration=1000, amplitude=0.9, envelope=Rectangular())),
-            ("ch2/acquisition", Acquisition(duration=1000)),
-            ("ch1/probe", Pulse(duration=1000, amplitude=0.9, envelope=Rectangular())),
-            ("ch1/acquisition", Acquisition(duration=1000)),
-        ]
-    )
-
-    sequences = 10 * [ps]
-
-    batches = list(batch(sequences, bounds))
-    assert len(batches) > 1
 
 
 def test_unroll_sequences(platform: Platform):
