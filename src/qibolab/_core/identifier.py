2786c50ae54f1333d366213351ca9ec41182f723
RoyStegeman <roystegeman@live.nl>
2026-03-27 12:40:47 +0000

    add docstrings for _join and _split

--- src/qibolab/_core/identifier.py
+++ src/qibolab/_core/identifier.py
@@ -1,4 +1,4 @@
-from typing import Annotated, TypeAlias, Union
+from typing import Annotated, Union
 
 import numpy as np
 import numpy.typing as npt
@@ -9,10 +9,6 @@ __all__ = ["Result"]
 QubitId = Annotated[Union[int, str], Field(union_mode="left_to_right")]
 """Qubit name."""
 
-QubitPairKey: TypeAlias = tuple[QubitId, QubitId]
-"""Two-qubit pair key for static typing."""
-
-
 ChannelId = str
 """Unique identifier for a channel."""
 
@@ -21,14 +17,28 @@ StateId = int
 """State identifier."""
 
 
-def _split(pair: Union[str, tuple]) -> tuple[str, str]:
-    tupled_pair = pair if isinstance(pair, tuple) else tuple(pair.split("-"))
-    assert len(tupled_pair) == 2
-    return tupled_pair
-
-
 def _join(pair: tuple[str, str]) -> str:
+    """ "Join a tuple of qubits into a string identifier. The reason for doing this is
+    that keys in JSON have to be strings."""
     return f"{pair[0]}-{pair[1]}"
+
+
+def _split(pair: Union[str, tuple]) -> tuple[str, str]:
+    """ "This function reverts the _join operation in case TransitionId or QubitPairId
+    is loaded from a JSON file where the key is the string constructed in _join.
+
+    .. note::
+
+        Since it is used as a BeforeValidator, the type of pair may be neither str nor
+        tuple. In that case, the function will pass on pair without modification, and
+        the validation will fail later on when the type is checked against
+        tuple[StateId, StateId] for TransitionId or tuple[QubitId, QubitId] for
+        QubitPairId.
+    """
+    if isinstance(pair, str):
+        a, b = pair.split("-")
+        return a, b
+    return pair
 
 
 TransitionId = Annotated[
@@ -37,7 +47,9 @@ TransitionId = Annotated[
 """Identifier for a state transition."""
 
 
-QubitPairId = Annotated[QubitPairKey, BeforeValidator(_split), PlainSerializer(_join)]
+QubitPairId = Annotated[
+    tuple[QubitId, QubitId], BeforeValidator(_split), PlainSerializer(_join)
+]
 """Two-qubit active interaction identifier."""
 
 
