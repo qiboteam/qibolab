1ebd896b9192fd6dee90281c30e2a7aa94f17029
Nandan Patel <118518753+NandanPatel24@users.noreply.github.com>
2026-06-06 10:27:37 +0800

    Update src/qibolab/_core/instruments/emulator/engine/abstract.py
    Co-authored-by: Lorenzo Ballerio <130075247+lballerio@users.noreply.github.com>

--- src/qibolab/_core/instruments/emulator/engine/abstract.py
+++ src/qibolab/_core/instruments/emulator/engine/abstract.py
@@ -54,7 +54,7 @@ class OperatorEvolution:
     """Abstract operator evolution interface."""
 
     operators: list[Operator | TimeDependentOperator] = field(default_factory=list)
-    coefficients: "NDArray | None" = None
+    coefficients: NDArray | None = None
 
 
 class SimulationEngine(Model, ABC):
