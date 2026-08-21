014c24b19b37593905f7460eeac86485f6a6bf03
Alessandro Candido <candido.ale@gmail.com>
2026-08-21 19:09:17 +0400

    fix: Comment out the update nop rule

--- src/qibolab/_core/instruments/qblox/sequence/finalize/__init__.py
+++ src/qibolab/_core/instruments/qblox/sequence/finalize/__init__.py
@@ -1,17 +1,20 @@
 from . import move, register, wait
 from .components import Pipeline
+from .process import transform, traverse
 
 __all__ = [
     "DEFAULT_PIPELINE",
     "Pipeline",
     "move",
     "register",
+    "transform",
+    "traverse",
     "wait",
 ]
 
 DEFAULT_PIPELINE = (
     wait.merge_wait,
     (wait.long_wait, move.negative_immediate),
-    register.update_nop,
+    # register.update_nop,
 )
 """Pipeline used by default to finalize the Q1ASM code compiled by this driver."""
