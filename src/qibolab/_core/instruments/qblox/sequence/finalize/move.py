dbccb06c63dfa5e1f4bf5a3d00fe5e558b4c993c
Alessandro Candido <candido.ale@gmail.com>
2026-08-21 16:28:44 +0400

    fix: Migrate tansformation for negative immediate in move instruction

--- src/qibolab/_core/instruments/qblox/sequence/finalize/move.py
+++ src/qibolab/_core/instruments/qblox/sequence/finalize/move.py
@@ -1,19 +1,43 @@
-from ...q1asm.ast_ import Block, Line
+from typing import cast
+
+from ...q1asm.ast_ import Block, Line, Move, Sub
 from .components import LineRule
 
 __all__ = ["negative_immediate"]
 
 
 def _match_negative_immediate(line: Line, state: None) -> tuple[bool, None]:
-    # TODO:
-    return not isinstance(line, Line), state
+    instr = line.instr
+    match = (
+        isinstance(instr, Move) and isinstance(instr.source, int) and instr.source < 0
+    )
+    return match, state
 
 
 def _map_negative_immediate(line: Line, state: None) -> tuple[Block, None]:
-    # TODO:
-    return [line], state
+    instr = line.instr
+    assert isinstance(instr, Move)
+    src = cast(int, instr.source)
+    dest = instr.destination
+    return [
+        Move(source=0, destination=dest),
+        Sub(a=dest, b=abs(src), destination=dest),
+    ], None
 
 
 negative_immediate = LineRule[None](
     match=_match_negative_immediate, map=_map_negative_immediate
 )
+"""Compile negative value sets.
+
+Apparently, the only place where negative numbers are not allowed are registers,
+otherwise they are handled by the internal compiler.
+
+https://docs.qblox.com/en/main/tutorials/q1asm_tutorials/intermediate/nco_control_adv.html#:~:text=Internally,%20the%20processor%20stores
+
+Thus, we compile instructions setting negative values as suggested: first setting them
+to 0, than subtracting the desired amount. This is more reliable than manually
+complementing the number, since it makes no assumption about the registers size.
+
+https://docs.qblox.com/en/main/cluster/troubleshooting.html#:~:text=How%20do%20I%20set%20negative%20numbers
+"""
