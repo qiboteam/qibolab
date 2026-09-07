9cffd8e1b54fce45d1d98ba5aba3125f014bdbde
Roy Stegeman <roystegeman@live.nl>
2026-09-07 14:14:59 +0400

    remove recommonmark dependency
    docs are purely rst, without any md, so there is no need for recommonmark

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -12,7 +12,6 @@
 #
 from pathlib import Path
 
-from recommonmark.transform import AutoStructify
 from sphinx.ext import apidoc
 
 import qibolab
@@ -44,7 +43,6 @@ extensions = [
     "sphinx.ext.coverage",
     "sphinx.ext.napoleon",
     "sphinx.ext.intersphinx",
-    "recommonmark",
     "sphinx_copybutton",
     "sphinx.ext.todo",
     "sphinx.ext.viewcode",
@@ -114,8 +112,6 @@ intersphinx_mapping = {"python": ("https://docs.python.org/3", None)}
 autodoc_member_order = "bysource"
 
 
-# Adapted this from
-# https://github.com/readthedocs/recommonmark/blob/ddd56e7717e9745f11300059e4268e204138a6b1/docs/conf.py
 # app setup hook
 
 
@@ -128,8 +124,6 @@ def run_apidoc(_):
 
 
 def setup(app):
-    app.add_config_value("recommonmark_config", {"enable_eval_rst": True}, True)
-    app.add_transform(AutoStructify)
     app.add_css_file("css/style.css")
 
     app.connect("builder-inited", run_apidoc)
